c9cb6faec149c47a4d598527786d757bfbbd4c14
xoto10 <buylow001@gmail.com>
2017-10-18 19:32:10 +0100

    make time pressure adjustment smaller

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -881,7 +881,7 @@ namespace {
 
     v /= int(PHASE_MIDGAME);
 
-    v += (me->game_phase() - 64) * Time.pressure() * 3 / 200;   // max +/-61
+    v += (me->game_phase() - 64) * Time.pressure() * 3 / 400;   // max +/-30
 
     // In case of tracing add all remaining individual evaluation terms
     if (T)

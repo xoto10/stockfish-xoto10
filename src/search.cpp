051bb241e71e605dfbfb29859de463b3ed74f7ac
xoto10 <buylow001@gmail.com>
2017-10-30 18:08:18 +0000

    use contempt to make black more cautious

--- src/search.cpp
+++ src/search.cpp
@@ -240,8 +240,10 @@ void MainThread::search() {
   TT.new_search();
 
   int contempt = Options["Contempt"] * PawnValueEg / 100; // From centipawns
+  int blackCaution = 20;
   DrawValue[ us] = VALUE_DRAW - Value(contempt);
   DrawValue[~us] = VALUE_DRAW + Value(contempt);
+  DrawValue[BLACK] += blackCaution;
 
   if (rootMoves.empty())
   {

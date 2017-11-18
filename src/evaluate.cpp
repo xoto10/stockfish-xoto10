87259d2e61e02b847ee04886cfd6a9331e5806b3
xoto10 <buylow001@gmail.com>
2017-11-18 22:20:12 +0000

    lower scale factor

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -788,11 +788,11 @@ namespace {
             // is almost a draw, in case of KBP vs KB, it is even more a draw.
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                return more_than_one(pos.pieces(PAWN)) ? ScaleFactor(31) : ScaleFactor(9);
+                return more_than_one(pos.pieces(PAWN)) ? ScaleFactor(26) : ScaleFactor(9);
 
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
-            return ScaleFactor(46);
+            return ScaleFactor(38);
         }
         // Endings where weaker side can place his king in front of the opponent's
         // pawns are drawish.

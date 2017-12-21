c1c91b8e47f8cab3c4b5e4db73a8de963c97e70c
xoto10 <buylow001@gmail.com>
2017-12-21 15:25:23 +0000

    xray through b+q+k or r+q+k. bench is 5808488

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -310,8 +310,8 @@ namespace {
     while ((s = *pl++) != SQ_NONE)
     {
         // Find attacked squares, including x-ray attacks for bishops and rooks
-        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(BISHOP, QUEEN))
-          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(ROOK, QUEEN))
+        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(BISHOP, QUEEN) ^ pos.pieces(KING))
+          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(ROOK, QUEEN) ^ pos.pieces(KING))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.pinned_pieces(Us) & s)

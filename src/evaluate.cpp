064c219f4757c964f6bda51f6a0797e45743e6c1
xoto10 <buylow001@gmail.com>
2018-04-04 17:17:36 +0100

    only give bonus if relative_rank < 4. bench 4806473

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -355,7 +355,7 @@ namespace {
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s);
 
                 // Bonus if bishop can see squares on far side of board
-                if (b & FarSideRanks)
+                if (relative_rank(Us, s) < RANK_4 && (b & FarSideRanks))
                     score += BishopFarSide;
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares

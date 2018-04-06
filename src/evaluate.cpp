5e9fad9f86c8006c1e19e6c2238b505b32f16f8b
xoto10 <buylow001@gmail.com>
2018-04-06 15:20:24 +0100

    only use endgame bonus. bench 5287447

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -174,7 +174,7 @@ namespace {
   constexpr Score PawnlessFlank      = S( 20, 80);
   constexpr Score RookOnPawn         = S(  8, 24);
   constexpr Score SliderOnQueen      = S( 42, 21);
-  constexpr Score QueenAttack        = S( 20, 10);
+  constexpr Score QueenAttack        = S(  0, 16);
   constexpr Score ThreatByPawnPush   = S( 47, 26);
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(175,168);

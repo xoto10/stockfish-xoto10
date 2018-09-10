3a693b409e189bd2bcfbf19dadee4d11470d22c5
xoto10 <buylow001@gmail.com>
2018-09-10 13:44:49 +0100

    use viz ideas combined. bench 4452637

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -173,6 +173,7 @@ namespace {
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(173,102);
   constexpr Score TrappedRook        = S( 92,  0);
+  constexpr Score TrappedRookMult    = S(  7,  7);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 29);
 
@@ -381,11 +382,11 @@ namespace {
                 score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
-            else if (mob <= 3)
+            else if (mob <= 4)
             {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
-                    score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
+                    score -= (TrappedRook - TrappedRookMult * mob * mob) * (1 + !pos.can_castle(Us));
             }
         }
 

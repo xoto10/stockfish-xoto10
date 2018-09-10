d86ca98ac914aee62c1ca670b5b7f22f289b7344
xoto10 <buylow001@gmail.com>
2018-09-10 22:08:43 +0100

    trappedrook = 86. bench 4144898

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,7 +172,7 @@ namespace {
   constexpr Score ThreatByPawnPush   = S( 45, 40);
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(173,102);
-  constexpr Score TrappedRook        = S( 89,  0);
+  constexpr Score TrappedRook        = S( 86,  0);
   constexpr Score TrappedRookMult    = S( 22,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 29);

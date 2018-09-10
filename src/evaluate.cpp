47c9a95af57d515b5bcd6cdfcd6d737d87756280
xoto10 <buylow001@gmail.com>
2018-09-10 14:53:07 +0100

    increase TrappedRook to 122 to avoid giving a bonus. bench 4267082

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,7 +172,7 @@ namespace {
   constexpr Score ThreatByPawnPush   = S( 45, 40);
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(173,102);
-  constexpr Score TrappedRook        = S( 92,  0);
+  constexpr Score TrappedRook        = S(122,  0);
   constexpr Score TrappedRookMult    = S(  7,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 29);

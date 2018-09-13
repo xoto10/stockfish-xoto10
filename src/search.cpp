f7dffc74099abd331e90f3669de8aa070bf8befa
xoto10 <buylow001@gmail.com>
2018-09-13 21:09:06 +0100

    prefer draws that are further away. bench 4440729

--- src/search.cpp
+++ src/search.cpp
@@ -535,7 +535,7 @@ namespace {
         && !rootNode
         && pos.has_game_cycle(ss->ply))
     {
-        alpha = VALUE_DRAW;
+        alpha = VALUE_DRAW + ss->ply/16;
         if (alpha >= beta)
             return alpha;
     }
@@ -585,7 +585,7 @@ namespace {
             || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
             return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos) - 10 * ((ss-1)->statScore > 0)
-                                                    : VALUE_DRAW;
+                                                    : VALUE_DRAW + ss->ply/16;
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply+1), but if alpha is already bigger because
@@ -1158,7 +1158,7 @@ moves_loop: // When in check, search starts from here
 
     if (!moveCount)
         bestValue = excludedMove ? alpha
-                   :     inCheck ? mated_in(ss->ply) : VALUE_DRAW;
+                   :     inCheck ? mated_in(ss->ply) : VALUE_DRAW + ss->ply/16;
     else if (bestMove)
     {
         // Quiet best move: update move sorting heuristics
@@ -1232,7 +1232,7 @@ moves_loop: // When in check, search starts from here
     // Check for an immediate draw or maximum ply reached
     if (   pos.is_draw(ss->ply)
         || ss->ply >= MAX_PLY)
-        return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos) : VALUE_DRAW;
+        return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos) : VALUE_DRAW + ss->ply/16;
 
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
 

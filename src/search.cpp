ca1b00cb9c77f6760343e1fb63d38a2b4188af2d
xoto10 <buylow001@gmail.com>
2018-09-18 08:39:43 +0100

    rootdepth / 4. bench 4291772

--- src/search.cpp
+++ src/search.cpp
@@ -621,8 +621,8 @@ namespace {
     posKey = pos.key() ^ Key(excludedMove << 16); // Isn't a very good hash
     tte = TT.probe(posKey, ttHit);
     if (ttHit &&
-          (   thisThread->nodes.load(std::memory_order_relaxed) & 512
-           || depth < thisThread->rootDepth / 2
+          (   thisThread->nodes.load(std::memory_order_relaxed) & 31
+           || depth < thisThread->rootDepth / 4
            || value_from_tt(tte->value(), ss->ply) != VALUE_DRAW
           )
        )

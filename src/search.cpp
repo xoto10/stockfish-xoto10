c1fc5f29a97c6c39b0a715de0c6fb6e25621e06b
xoto10 <buylow001@gmail.com>
2018-09-19 15:59:59 +0100

    value_draw - (depth+4)/8. bench 4110843

--- src/search.cpp
+++ src/search.cpp
@@ -535,7 +535,7 @@ namespace {
         && !rootNode
         && pos.has_game_cycle(ss->ply))
     {
-        alpha = VALUE_DRAW - depth/4;
+        alpha = VALUE_DRAW - (depth+4) / 8;
         if (alpha >= beta)
             return alpha;
     }
@@ -585,7 +585,7 @@ namespace {
             || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
             return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos) - 10 * ((ss-1)->statScore > 0)
-                                                    : VALUE_DRAW - depth/4;
+                                                    : VALUE_DRAW - (depth+4) / 8;
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply+1), but if alpha is already bigger because

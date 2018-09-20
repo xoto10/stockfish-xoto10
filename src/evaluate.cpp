6a683b0eadc8badad832b1c6c6abcb745b4af296
xoto10 <buylow001@gmail.com>
2018-09-20 21:17:50 +0100

    score 160, bonus 32. bench 4199526

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -777,7 +777,7 @@ namespace {
     Score ret;
 
     if (oppositeCastling)
-        mg = m > 160 ? 16 : m < -160 ? -16 : 0;
+        mg = m > 160 ? 32 : m < -160 ? -32 : 0;
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
     // that the endgame score will never change sign after the bonus.

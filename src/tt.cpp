39e67a5a95a0b4b6dd240372d9f34cea58752102
xoto10 <buylow001@gmail.com>
2018-09-21 18:29:44 +0100

    only save some value draws, depth < 6. bench 4059477

--- src/tt.cpp
+++ src/tt.cpp
@@ -41,7 +41,7 @@ void TTEntry::save(Key k, Value v, Bound b, Depth d, Move m, Value ev) {
   // Overwrite less valuable entries
   if (  (k >> 48) != key16
       || (v != VALUE_DRAW && d / ONE_PLY > depth8 - 4)
-      || (v == VALUE_DRAW && d / ONE_PLY < depth8 + 4)
+      || (v == VALUE_DRAW && d / ONE_PLY < 6)
       || b == BOUND_EXACT)
   {
       key16     = (uint16_t)(k >> 48);

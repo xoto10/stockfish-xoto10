653c19063a30b40de4cd13002bf74aeba839e4ae
xoto10 <buylow001@gmail.com>
2018-09-26 11:48:04 +0100

    fix typo. bench 4835504

--- src/search.cpp
+++ src/search.cpp
@@ -74,7 +74,7 @@ namespace {
   // Futility and reductions lookup tables, initialized at startup
   int Reductions[2][2][64][64];  // [pv][improving][depth][moveNumber]
   int FutilityMoveCounts[2][16] = { // [improving][depth]
-                                    { 2, 3, 5, 8, 12, 16, 20, 25, 31, 38, 46, 53, 62, 71, 91, 92 },
+                                    { 2, 3, 5, 8, 12, 16, 20, 25, 31, 38, 46, 53, 62, 71, 81, 92 },
                                     { 5, 6, 9, 15, 23, 30, 41, 53, 66, 83, 101, 119, 142, 170, 196, 215 } };
 
   template <bool PvNode> Depth reduction(bool i, Depth d, int mn) {

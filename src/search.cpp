6b6eaa69af69a4cad39f7cfb8527fa8b539b9609
xoto10 <buylow001@gmail.com>
2018-09-26 13:21:33 +0100

    increase futility move counts array to size 18. bench 4248710

--- src/search.cpp
+++ src/search.cpp
@@ -72,7 +72,7 @@ namespace {
   }
 
   // Futility and reductions lookup tables, initialized at startup
-  int FutilityMoveCounts[2][16]; // [improving][depth]
+  int FutilityMoveCounts[2][18]; // [improving][depth]
   int Reductions[2][2][64][64];  // [pv][improving][depth][moveNumber]
 
   template <bool PvNode> Depth reduction(bool i, Depth d, int mn) {
@@ -163,7 +163,7 @@ void Search::init() {
                 Reductions[NonPV][imp][d][mc]++;
           }
 
-  for (int d = 0; d < 16; ++d)
+  for (int d = 0; d < 18; ++d)
   {
       FutilityMoveCounts[0][d] = int(2.4 + 0.74 * pow(d, 1.78));
       FutilityMoveCounts[1][d] = int(5.0 + 1.00 * pow(d, 2.00));
@@ -898,7 +898,7 @@ moves_loop: // When in check, search starts from here
       movedPiece = pos.moved_piece(move);
       givesCheck = gives_check(pos, move);
 
-      moveCountPruning =   depth < 16 * ONE_PLY
+      moveCountPruning =   depth < 18 * ONE_PLY
                         && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
 
       // Step 13. Extensions (~70 Elo)

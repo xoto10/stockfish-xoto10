7d9233b7674302b7d8c170666c9b7dd2ebba68b7
xoto10 <buylow001@gmail.com>
2018-09-30 00:02:27 +0100

    4xpawns, 16xfiles, +18. bench 4275853

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -761,7 +761,7 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    +  2
+                    + 18
                     - 16 * pe->open_files()
                     ;
 

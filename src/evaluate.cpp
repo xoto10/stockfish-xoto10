19ae178cb5267a2e70b060200c371987786c3d7c
xoto10 <buylow001@gmail.com>
2018-09-30 09:51:56 +0100

    11xoutflanking (from tune). bench 4545793

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -756,14 +756,11 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =       // 8 * pe->pawn_asymmetry()
-                    +  4 * pos.count<PAWN>()
-                    + 12 * outflanking
-                    + 16 * pawnsOnBothFlanks
+    int complexity =   4 * pos.count<PAWN>()
+                    + 16 * (pawnsOnBothFlanks - pe->open_files())
+                    + 11 * outflanking
                     + 48 * !pos.non_pawn_material()
-                    + 18
-                    - 16 * pe->open_files()
-                    ;
+                    + 10;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

09246436f1a38ab84336c66bf7061ac55d41a465
xoto10 <buylow001@gmail.com>
2018-10-04 09:48:54 +0100

    no 0 from eval(), don't store 0 in tt. bench 4512634

--- src/tt.cpp
+++ src/tt.cpp
@@ -33,6 +33,7 @@ TranspositionTable TT; // Our global transposition table
 void TTEntry::save(Key k, Value v, Bound b, Depth d, Move m, Value ev) {
 
   assert(d / ONE_PLY * ONE_PLY == d);
+  if (v == VALUE_DRAW) return;
 
   // Preserve any existing move for the same position
   if (m || (k >> 48) != key16)

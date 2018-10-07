52c5df81c42daa52865da081d618029b849ced45
Miguel Lahoz <miguel_lahoz@protonmail.com>
2018-10-07 22:13:03 +0800

    take 2. Bench: 4475313

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,7 +32,7 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
-  constexpr Score Backward[] = { S( 9, 24), S(6, 18) };
+  constexpr Score Backward[] = { S( 10, 30), S(7, 21) };
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
 

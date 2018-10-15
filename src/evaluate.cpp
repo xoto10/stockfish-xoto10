2604ca4e7842f8cce8a6f617da5f1d6a763a1925
xoto10 <buylow001@gmail.com>
2018-10-15 20:26:31 +0100

    larger penalty to replace trappedrook. bench 4893433

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -163,7 +163,7 @@ namespace {
   constexpr Score KnightOnQueen      = S( 21, 11);
   constexpr Score LongDiagonalBishop = S( 46,  0);
   constexpr Score MinorBehindPawn    = S( 16,  0);
-  constexpr Score NoCastle           = S( 92,  0);
+  constexpr Score NoCastle           = S(148,  0);
   constexpr Score Overload           = S( 13,  6);
   constexpr Score PawnlessFlank      = S( 19, 84);
   constexpr Score RookOnPawn         = S( 10, 30);

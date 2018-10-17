15e6a5ffb0c43a99c69d0f88017c683b58d56620
xoto10 <buylow001@gmail.com>
2018-10-17 07:31:14 +0100

    prev < -32. bench 4885450

--- src/search.cpp
+++ src/search.cpp
@@ -387,7 +387,7 @@ void Thread::search() {
           if (rootDepth >= 5 * ONE_PLY)
           {
               Value previousScore = rootMoves[pvIdx].previousScore;
-              delta = previousScore < -28 ? Value(27) : Value(18);
+              delta = previousScore < -32 ? Value(27) : Value(18);
               alpha = std::max(previousScore - delta,-VALUE_INFINITE);
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 

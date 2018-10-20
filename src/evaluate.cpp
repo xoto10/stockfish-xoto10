71152fe9841e70dfa41f848cbad64f0f3426ebf3
xoto10 <buylow001@gmail.com>
2018-10-20 06:57:30 +0100

    bonus 10,10 only their pieces block. bench 4707832

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -158,7 +158,7 @@ namespace {
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CloseEnemies       = S(  6,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
-  constexpr Score FlankLever         = S( 14, 14);
+  constexpr Score FlankLever         = S( 10, 10);
   constexpr Score Hanging            = S( 57, 32);
   constexpr Score KingProtector      = S(  6,  6);
   constexpr Score KnightOnQueen      = S( 21, 11);
@@ -605,7 +605,7 @@ namespace {
 
     // Bonus for having unblocked levers on both flanks if center is blocked
     b =   (pos.pieces(Us, PAWN) & attackedBy[Them][PAWN])
-        | (pe->lever_pawns(Us) & ~shift<Down>(pos.pieces()));  // or pos.pieces(Them) ??
+        | (pe->lever_pawns(Us) & ~shift<Down>(pos.pieces(Them)));  // or pos.pieces() ??
     blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces(Them)) & (FileDBB | FileEBB);
     if (more_than_one(blocked))
         score += FlankLever * (bool(b & QueenSide) + bool(b & KingSide));

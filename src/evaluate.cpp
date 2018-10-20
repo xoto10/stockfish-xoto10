856c13a24c6841750ca4606516fd2b268cf5e527
xoto10 <buylow001@gmail.com>
2018-10-20 05:13:49 +0100

    increase bonus to 14,14  bench 4747150

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -158,7 +158,7 @@ namespace {
   constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CloseEnemies       = S(  6,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
-  constexpr Score FlankLever         = S( 14,  7);
+  constexpr Score FlankLever         = S( 14, 14);
   constexpr Score Hanging            = S( 57, 32);
   constexpr Score KingProtector      = S(  6,  6);
   constexpr Score KnightOnQueen      = S( 21, 11);

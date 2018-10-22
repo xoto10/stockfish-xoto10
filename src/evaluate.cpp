31701f917b098929a1a70da0a87d9c4e5833fba4
xoto10 <buylow001@gmail.com>
2018-10-22 13:53:35 +0100

    is mob[knight][2] too low? bench 4451597

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -102,7 +102,7 @@ namespace {
   // MobilityBonus[PieceType-2][attacked] contains bonuses for middle and end game,
   // indexed by piece type and number of attacked squares in the mobility area.
   constexpr Score MobilityBonus[][32] = {
-    { S(-75,-76), S(-57,-54), S( -9,-28), S( -2,-10), S(  6,  5), S( 14, 12), // Knights
+    { S(-75,-76), S(-57,-54), S(-18,-28), S( -2,-10), S(  6,  5), S( 14, 12), // Knights
       S( 22, 26), S( 29, 29), S( 36, 29) },
     { S(-48,-59), S(-20,-23), S( 16, -3), S( 26, 13), S( 38, 24), S( 51, 42), // Bishops
       S( 55, 54), S( 63, 57), S( 63, 65), S( 68, 73), S( 81, 78), S( 81, 86),

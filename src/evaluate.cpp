3bdb0e35b771567c1ad4e1c1ab51b2b3b40369a6
xoto10 <buylow001@gmail.com>
2018-10-25 16:58:35 +0100

    remove bonus, keep zero if no live pawns. bench 4522426

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -765,7 +765,6 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    + 96 * livePawns
                     -118 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

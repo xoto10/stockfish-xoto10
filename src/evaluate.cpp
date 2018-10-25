7b5aa98391aad347d9d6a11ac407b24cdaa692b2
xoto10 <buylow001@gmail.com>
2018-10-25 12:54:40 +0100

    livePawns 64. bench 4438854

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -763,7 +763,7 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    + 32 * livePawns
+                    + 64 * livePawns
                     -118 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

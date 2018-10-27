1b27252b4733a00bb6f7a9394078ce31e941bafa
xoto10 <buylow001@gmail.com>
2018-10-27 19:14:43 +0100

    weakunopposed 10,24. bench 3725792

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -173,7 +173,7 @@ namespace {
   constexpr Score ThreatBySafePawn   = S(173,102);
   constexpr Score TrappedRook        = S( 92,  0);
   constexpr Score WeakQueen          = S( 50, 10);
-  constexpr Score WeakUnopposedPawn  = S( 15, 19);
+  constexpr Score WeakUnopposedPawn  = S( 10, 24);
 
 #undef S
 

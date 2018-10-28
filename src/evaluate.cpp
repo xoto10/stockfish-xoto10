2b7160e1e6d9ead1ee6bc4afdbc74cad15e7d735
xoto10 <buylow001@gmail.com>
2018-10-28 21:56:45 +0000

    combo with Kur/morekf8. bench 3440422

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -171,7 +171,7 @@ namespace {
   constexpr Score ThreatByPawnPush   = S( 45, 40);
   constexpr Score ThreatByRank       = S( 16,  3);
   constexpr Score ThreatBySafePawn   = S(173,102);
-  constexpr Score TrappedRook        = S( 92,  0);
+  constexpr Score TrappedRook        = S(100,  0);
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S( 15, 19);
 

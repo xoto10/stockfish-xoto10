c9cd87a52e214f8ab555a3e777edcc8976ea4438
xoto10 <buylow001@gmail.com>
2018-10-29 21:26:45 +0000

    penalty 16. bench 3572431

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -156,7 +156,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score CastlingStopped    = S( 32,  0);
+  constexpr Score CastlingStopped    = S( 16,  0);
   constexpr Score CloseEnemies       = S(  6,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 57, 32);

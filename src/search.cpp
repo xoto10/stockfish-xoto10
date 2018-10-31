6e322ca0ae75dee7286e2c9b1e509510d58787f3
xoto10 <buylow001@gmail.com>
2018-10-31 11:34:16 +0000

    c0. bench 3757339

--- src/search.cpp
+++ src/search.cpp
@@ -334,7 +334,7 @@ void Thread::search() {
 
   // No static contempt during opening phase, at most 2 bishops off or 3 pawns
   if (rootPos.count<ALL_PIECES>() > 27)
-      ct = 10;
+      ct = 0;
 
   // In analysis mode, adjust contempt in accordance with user preference
   if (Limits.infinite || Options["UCI_AnalyseMode"])

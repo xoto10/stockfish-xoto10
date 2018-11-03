66f459bf65fc4b955b32b5ce89f4041f8fef310b
xoto10 <buylow001@gmail.com>
2018-11-03 21:19:16 +0000

    change doubled to 32,35. bench 3646260

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward = S( 9, 24);
-  constexpr Score Doubled  = S(26, 41);
+  constexpr Score Doubled  = S(32, 35);
   constexpr Score Isolated = S( 5, 15);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank

3d2b2e3369709937dbf156d3b4f664927dda8435
xoto10 <buylow001@gmail.com>
2018-11-15 19:18:00 +0000

    restrict time saving on stable moves, 0.5  bench 3913185

--- src/search.cpp
+++ src/search.cpp
@@ -505,7 +505,7 @@ void Thread::search() {
               // Use part of the gained time from a previous stable move for the current move
               double bestMoveInstability = 1.0 + mainThread->bestMoveChanges;
               bestMoveInstability *= std::pow(mainThread->previousTimeReduction, 0.528) / timeReduction;
-              bestMoveInstability = std::max(0.7, bestMoveInstability * improvingFactor / 581);
+              bestMoveInstability = std::max(0.5, bestMoveInstability * improvingFactor / 581);
 
               // Stop the search if we have only one legal move, or if available time elapsed
               if (   rootMoves.size() == 1

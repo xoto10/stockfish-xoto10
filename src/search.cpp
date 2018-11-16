8eda2a6dfd8b8cba32f706fad8573d8049e16acc
xoto10 <buylow001@gmail.com>
2018-11-16 18:43:25 +0000

    use more time after a quick move, remove pow. bench 3913185

--- src/search.cpp
+++ src/search.cpp
@@ -504,7 +504,7 @@ void Thread::search() {
 
               // Use part of the gained time from a previous stable move for the current move
               double bestMoveInstability = 1.0 + mainThread->bestMoveChanges;
-              bestMoveInstability *= std::pow(mainThread->previousTimeReduction, 0.528) / timeReduction;
+              bestMoveInstability *= mainThread->previousTimeReduction / timeReduction;
 
               // Stop the search if we have only one legal move, or if available time elapsed
               if (   rootMoves.size() == 1

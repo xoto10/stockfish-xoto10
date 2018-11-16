aa16aa799d5cc7f656e7d1cea651969fbcc1b54e
xoto10 <buylow001@gmail.com>
2018-11-16 21:19:48 +0000

    use short ema for previousTimeReduction. bench 3913185

--- src/search.cpp
+++ src/search.cpp
@@ -504,7 +504,7 @@ void Thread::search() {
 
               // Use part of the gained time from a previous stable move for the current move
               double bestMoveInstability = 1.0 + mainThread->bestMoveChanges;
-              bestMoveInstability *= mainThread->previousTimeReduction / timeReduction;
+              bestMoveInstability *= std::pow(mainThread->previousTimeReduction, 0.528) / timeReduction;
 
               // Stop the search if we have only one legal move, or if available time elapsed
               if (   rootMoves.size() == 1
@@ -523,7 +523,7 @@ void Thread::search() {
   if (!mainThread)
       return;
 
-  mainThread->previousTimeReduction = timeReduction;
+  mainThread->previousTimeReduction += (timeReduction - mainThread->previousTimeReduction) / 2;
 
   // If skill level is enabled, swap best PV line with the sub-optimal one
   if (skill.enabled())
--- src/thread.cpp
+++ src/thread.cpp
@@ -151,7 +151,7 @@ void ThreadPool::clear() {
 
   main()->callsCnt = 0;
   main()->previousScore = VALUE_INFINITE;
-  main()->previousTimeReduction = 1.0;
+  main()->previousTimeReduction = 1.5;
 }
 
 /// ThreadPool::start_thinking() wakes up main thread waiting in idle_loop() and

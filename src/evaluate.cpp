96ae8580f5f4df6e5973e69f08a69687c12f51c8
xoto10 <buylow001@gmail.com>
2018-11-17 20:15:01 +0000

    subtract larger constant, -48. bench 4270545

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -478,7 +478,7 @@ namespace {
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
                      +       mg_value(mobility[Them] - mobility[Us])
-                     -   30;
+                     -  48;
 
         // Transform the kingDanger units into a Score, and subtract it from the evaluation
         if (kingDanger > 0)

f274c519751261772820963c3c031a0114c19044
xoto10 <buylow001@gmail.com>
2018-11-20 07:59:40 +0000

    piece 8,7 bench 3712348

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -162,7 +162,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 12,  6);
   constexpr Score PawnlessFlank      = S( 18, 94);
-  constexpr Score RestrictedPiece    = S(  7,  6);
+  constexpr Score RestrictedPiece    = S(  8,  7);
   constexpr Score RookOnPawn         = S( 10, 28);
   constexpr Score SliderOnQueen      = S( 49, 21);
   constexpr Score ThreatByKing       = S( 21, 84);

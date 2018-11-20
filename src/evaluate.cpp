e56542657406af81a5b59308c9ba2dda69a9250a
xoto10 <buylow001@gmail.com>
2018-11-20 15:37:23 +0000

    pawn 5,6 bench 3522403

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -162,7 +162,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 12,  6);
   constexpr Score PawnlessFlank      = S( 18, 94);
-  constexpr Score RestrictedPawn     = S(  7,  6);
+  constexpr Score RestrictedPawn     = S(  5,  6);
   constexpr Score RestrictedPiece    = S(  7,  6);
   constexpr Score RookOnPawn         = S( 10, 28);
   constexpr Score SliderOnQueen      = S( 49, 21);

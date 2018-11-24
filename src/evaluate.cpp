f9c86e160c9bfb6816e9b600291d714655e39756
xoto10 <buylow001@gmail.com>
2018-11-24 17:37:26 +0000

    bonus 20,0 for castling. bench 4117631

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -153,7 +153,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  8);
-  constexpr Score Castled            = S( 40,  0);
+  constexpr Score Castled            = S( 20,  0);
   constexpr Score CloseEnemies       = S(  7,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 62, 34);

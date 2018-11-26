25fedf27103a8347268ca659fc3b1947d5b2b411
xoto10 <buylow001@gmail.com>
2018-11-26 08:17:46 +0000

    pawnTarget bonus only if we have q or rook. bench 3893130

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -162,7 +162,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 16,  0);
   constexpr Score Overload           = S( 12,  6);
   constexpr Score PawnlessFlank      = S( 18, 94);
-  constexpr Score PawnTargets        = S( 10, 10);
+  constexpr Score PawnTargets        = S( 20, 20);
   constexpr Score RestrictedPiece    = S(  7,  6);
   constexpr Score RookOnPawn         = S( 10, 28);
   constexpr Score SliderOnQueen      = S( 49, 21);
@@ -567,13 +567,14 @@ namespace {
                 &  attackedBy[Us][ALL_PIECES];
     score += RestrictedPiece * popcount(restricted);
 
-    // Bonus for attacking pawnTargets
-    score += PawnTargets * (  popcount(attackedBy[Us][ALL_PIECES] & pe->pawn_targets(Them)) );
-//                          + popcount(attackedBy2[Us] & pe->pawn_targets(Them)));
-
-    // Bonus for enemy unopposed weak pawns
     if (pos.pieces(Us, ROOK, QUEEN))
+    {
+        // Bonus for enemy unopposed weak pawns
         score += WeakUnopposedPawn * pe->weak_unopposed(Them);
+
+        // Bonus for attacking pawnTargets
+        score += PawnTargets * popcount(attackedBy2[Us] & pe->pawn_targets(Them));
+    }
 
     // Find squares where our pawns can push on the next move
     b  = shift<Up>(pos.pieces(Us, PAWN)) & ~pos.pieces();

268966e3570d8b7bcf4845f5ec222232edef0489
xoto10 <buylow001@gmail.com>
2018-11-30 19:51:12 +0000

    bonus for opponents majors on our bishop's color squares, 6,6. bench 3454680

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -153,7 +153,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  8);
-  constexpr Score BishopMajors       = S( 10, 10);
+  constexpr Score BishopMajors       = S(  6,  6);
   constexpr Score CloseEnemies       = S(  7,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 62, 34);

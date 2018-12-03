9d84ab1d1ff1d065fcc9d19addf2a48786acc241
xoto10 <buylow001@gmail.com>
2018-12-03 19:56:56 +0000

    pawnsonsamecolor -1. bench 3500111

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -343,8 +343,8 @@ namespace {
                 // bishop, bigger when the center files are blocked with pawns.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
-                score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
-                                     * (popcount(blocked & CenterFiles));
+                score -= BishopPawns * (pe->pawns_on_same_color_squares(Us, s) - 1)
+                                     * (1 + popcount(blocked & CenterFiles));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))

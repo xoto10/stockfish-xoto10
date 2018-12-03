b11e847048d65c799c7c685fee043048453102c2
xoto10 <buylow001@gmail.com>
2018-12-03 19:54:19 +0000

    no bishoppawn if center not blocked. bench 3334345

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -344,7 +344,7 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles));
+                                     * (popcount(blocked & CenterFiles));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))

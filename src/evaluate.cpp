b1dc7406bb56732d56b93420c235c45a2e914950
xoto10 <buylow001@gmail.com>
2018-12-05 19:38:55 +0000

    4xblocked - 86. bench 3457726

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -758,8 +758,8 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    -  2 * backblock
-                    - 70 ;
+                    -  4 * backblock
+                    - 86 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

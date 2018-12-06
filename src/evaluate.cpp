5e9309be3f0c3792b57231af11deefa0ec237111
xoto10 <buylow001@gmail.com>
2018-12-06 13:27:04 +0000

    connectivity from npm, 0,2. bench 3775219

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -154,6 +154,7 @@ namespace {
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  8);
   constexpr Score CloseEnemies       = S(  7,  0);
+  constexpr Score Connectivity       = S(  0,  2);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 62, 34);
   constexpr Score KingProtector      = S(  6,  7);
@@ -602,6 +603,9 @@ namespace {
 
         score += SliderOnQueen * popcount(b & safe & attackedBy2[Us]);
     }
+
+    b = pos.pieces(Us) & attackedBy[Us][NON_PAWN];
+    score += Connectivity * popcount(b);
 
     if (T)
         Trace::add(THREAT, Us, score);

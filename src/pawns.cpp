579794cc1064a611eb9e0e272e14fd812ea2c474
xoto10 <buylow001@gmail.com>
2018-12-07 10:13:44 +0000

    does extra shelter evaluation help or hinder? bench 3062296

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -246,13 +246,6 @@ Score Entry::do_king_safety(const Position& pos) {
 
   Value bonus = evaluate_shelter<Us>(pos, ksq);
 
-  // If we can castle use the bonus after the castling if it is bigger
-  if (pos.can_castle(Us | KING_SIDE))
-      bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_G1)));
-
-  if (pos.can_castle(Us | QUEEN_SIDE))
-      bonus = std::max(bonus, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)));
-
   return make_score(bonus, -16 * minKingPawnDistance);
 }
 

472b50616e751067510e8e148982f3dfc492ff1a
xoto10 <buylow001@gmail.com>
2018-12-15 20:38:02 +0000

    scale outpost bonus by distance to their king, 9-,80*. bench 3539886

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -326,7 +326,7 @@ namespace {
             bb = OutpostRanks & ~pe->pawn_attacks_span(Them);
             if (bb & s)
                 score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & s)]
-                         * (9 - distance(s, pos.square<KING>(Them))) * 88 / 256;
+                         * (9 - distance(s, pos.square<KING>(Them))) * 80 / 256;
 
             else if (bb &= b & ~pos.pieces(Us))
                 score += Outpost[Pt == BISHOP][bool(attackedBy[Us][PAWN] & bb)];

0cee2601cd4c7cc9f00da5f2da8ce160c345edb4
xoto10 <buylow001@gmail.com>
2018-12-18 21:04:48 +0000

    connectivity between npm, (6,26)/8. bench 3527742

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -154,7 +154,7 @@ namespace {
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  8);
   constexpr Score CloseEnemies       = S(  7,  0);
-  constexpr Score Connectivity       = S( 10, 22);
+  constexpr Score Connectivity       = S(  6, 26);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 62, 34);
   constexpr Score KingProtector      = S(  6,  7);

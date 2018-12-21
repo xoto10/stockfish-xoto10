aa41074ee8fd16982ae929fbee5487d8d4cdb37a
xoto10 <buylow001@gmail.com>
2018-12-21 14:54:09 +0000

    vote calculation: use maxScore-100 instead of minScore. no functional change for 1 thread

--- src/search.cpp
+++ src/search.cpp
@@ -267,7 +267,7 @@ void MainThread::search() {
       // Vote according to score and depth
       auto square = [](int64_t x) { return x * x; };
       for (Thread* th : Threads)
-          votes[th->rootMoves[0].pv[0]] += 200 + (square(std::max(1, int(th->rootMoves[0].score - (maxScore - 24))))
+          votes[th->rootMoves[0].pv[0]] += 200 + (square(std::max(1, int(th->rootMoves[0].score - (maxScore - 100))))
                                                   * int64_t(th->completedDepth));
 
       // Select best thread

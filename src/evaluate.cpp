f54cfaf392cf2fd6a5dffab6d1940218a627bb01
xoto10 <you@example.com>
2018-12-26 19:53:37 +0000

    consider their pieces in kingdanger. bench 3635529

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -418,7 +418,7 @@ namespace {
     // Find the squares that opponent attacks in our king flank, and the squares
     // which are attacked twice in that flank.
     kingFlank = KingFlank[file_of(ksq)];
-    b1 = attackedBy[Them][ALL_PIECES] & kingFlank & Camp;
+    b1 =(pos.pieces(Them) | attackedBy[Them][ALL_PIECES]) & kingFlank & Camp;
     b2 = b1 & attackedBy2[Them];
 
     int tropism = popcount(b1) + popcount(b2);

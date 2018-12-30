dc0040d949955fd853d7eb45cb5be4699fe03ef7
xoto10 <buylow001@gmail.com>
2018-12-30 19:54:21 +0000

    use mob total in initiative. x48/32. bench 3176307

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,7 +743,7 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
-    int mobDiff = (mobTotal[WHITE] - mobTotal[BLACK]) * 20 / 32;
+    int mobDiff = (mobTotal[WHITE] - mobTotal[BLACK]) * 48 / 32;
 
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * pe->pawn_asymmetry()

adfca0862ba487e8df69f11c34f229dd2ba0ca97
xoto10 <buylow001@gmail.com>
2018-12-31 01:16:02 +0000

    initiative bonus for material diff, 24. bench 3857745

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -753,7 +753,7 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    + 10 * materialDiff
+                    + 24 * materialDiff
                     -118 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

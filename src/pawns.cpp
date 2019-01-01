2e7400a8e8611e1a8aced9cde86a459980d10aee
xoto10 <buylow001@gmail.com>
2019-01-01 13:59:19 +0000

    add 1 to asymm if msb > lsb+1. bench 3580486

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -189,7 +189,7 @@ Entry* probe(const Position& pos) {
 
   int realSemiopen = e->semiopenFiles[WHITE] ^ e->semiopenFiles[BLACK];
   e->asymmetry = popcount(realSemiopen | e->passedPawns[WHITE] | e->passedPawns[BLACK])
-                + (realSemiopen > 0) * (msb(realSemiopen) - lsb(realSemiopen)) / 2;
+                + (realSemiopen > 0 && msb(realSemiopen) > lsb(realSemiopen) + 1);
 //sync_cout << "info string semi: " << realSemiopen << " msb " << (realSemiopen > 0) * msb(realSemiopen)
 //          << " lsb " << (realSemiopen > 0) * lsb(realSemiopen) << sync_endl;
   return e;

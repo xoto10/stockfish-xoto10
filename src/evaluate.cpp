e89741c42d63c72205e26ac6365ea06e3d89f635
xoto10 <buylow001@gmail.com>
2019-01-02 23:16:21 +0000

    penalty for kingring attacks by pawns, 42. bench 3049822

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -467,7 +467,7 @@ namespace {
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
-                 +  50 * popcount(kingRing[Us] & attackedBy[Them][PAWN])
+                 +  42 * popcount(kingRing[Us] & attackedBy[Them][PAWN])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  +       tropism * tropism / 4
                  - 873 * !pos.count<QUEEN>(Them)

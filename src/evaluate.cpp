ecaffc82136c82936539014b2160b32029391899
xoto10 <buylow001@gmail.com>
2019-01-05 22:28:40 +0000

    add span/2 into pawnasymmetry, 8x,-147. bench 3373229

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,12 +743,12 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   6 * pe->pawn_asymmetry()
+    int complexity =   8 * pe->pawn_asymmetry()
                     + 12 * pos.count<PAWN>()
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    -118 ;
+                    -147 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

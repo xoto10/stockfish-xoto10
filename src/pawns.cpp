ec0acab5fc4bc3d48cacdb08c70eb0a6c59b3d0d
xoto10 <buylow001@gmail.com>
2019-01-09 21:54:57 +0000

    small reduction in ks for advancing pawn, (3,3)*(5-theirRank), 0<npm<7400. bench 3426827

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
-  constexpr Score ToRank6  = S( 0,  4);
+  constexpr Score ToRank6  = S( 3,  3);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];

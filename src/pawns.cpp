1da55f0e6434ba278d8bdea51874b0a6ee377e8d
xoto10 <buylow001@gmail.com>
2019-01-12 20:28:40 +0000

    10,15 for rank 6 thorns, extra 10,15 if in their kingflank. bench 3511873

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
-  constexpr Score Thorn    = S(30, 30);
+  constexpr Score Thorn    = S(10, 15);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];

333a49eaefb0f514edb25bb7f8c1c7a08d4bc9f5
xoto10 <buylow001@gmail.com>
2019-01-12 19:41:00 +0000

    any rank 6 thorn, 32,32. bench 4027549

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
-  constexpr Score Thorn    = S(50, 50);
+  constexpr Score Thorn    = S(32, 32);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];

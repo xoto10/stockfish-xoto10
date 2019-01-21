50103071b3588a510d55d22c7556e04e13388373
xoto10 <buylow001@gmail.com>
2019-01-21 10:47:32 +0000

    tuned pawn arrays. bench 3979690

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -42,20 +42,20 @@ namespace {
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
   constexpr Value ShelterStrength[int(FILE_NB) / 2][RANK_NB] = {
-    { V( -5), V( 82), V( 94), V( 58), V( 38), V( 19), V(  24) },
-    { V(-46), V( 61), V( 37), V(-48), V(-31), V(-11), V( -62) },
-    { V(-11), V( 75), V( 25), V( -2), V( 30), V(  2), V( -43) },
-    { V(-37), V(-12), V(-29), V(-52), V(-49), V(-67), V(-165) }
+    { V( -5), V( 81), V( 93), V( 57), V( 38), V( 18), V(  26) },
+    { V(-45), V( 62), V( 35), V(-49), V(-31), V(-11), V( -62) },
+    { V(-12), V( 77), V( 25), V( -2), V( 31), V(  2), V( -43) },
+    { V(-37), V(-14), V(-31), V(-52), V(-50), V(-68), V(-164) }
   };
 
   // Danger of enemy pawns moving toward our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where the enemy has no pawn, or their pawn
   // is behind our king.
   constexpr Value UnblockedStorm[int(FILE_NB) / 2][RANK_NB] = {
-    { V( 88), V(108), V(122), V(94), V(57), V( 44), V( 50) },
-    { V( 47), V(-19), V(123), V(48), V(38), V( -9), V( 23) },
-    { V(  5), V( 50), V(163), V(39), V( 7), V(-16), V( -2) },
-    { V(-12), V(-15), V( 90), V(16), V( 2), V( -7), V(-17) }
+    { V( 88), V(106), V(123), V(94), V(58), V( 45), V( 51) },
+    { V( 46), V(-19), V(123), V(48), V(39), V( -7), V( 22) },
+    { V(  6), V( 50), V(163), V(38), V( 7), V(-16), V( -4) },
+    { V(-12), V(-16), V( 91), V(17), V( 2), V( -5), V(-15) }
   };
 
   #undef S

997eb654e8d8f142e84fcb78e0879818538ef718
xoto10 <buylow001@gmail.com>
2019-02-02 16:52:33 +0000

    don't reduce time use if score rising. bench 3611854

--- src/search.cpp
+++ src/search.cpp
@@ -498,7 +498,8 @@ void Thread::search() {
           && !Threads.stop
           && !mainThread->stopOnPonderhit)
       {
-          double fallingEval = (306 + 119 * failedLow + 6 * (mainThread->previousScore - bestValue)) / 581.0;
+          double fallingEval = (306 + 119 * failedLow + 6 * std::max(0, int(mainThread->previousScore - bestValue)))
+                               / 581.0;
           fallingEval        = std::max(0.5, std::min(1.5, fallingEval));
 
           // If the bestMove is stable over several iterations, reduce time accordingly

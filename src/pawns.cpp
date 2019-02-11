d0386cfa5be69e062b326fcb681c7897048729cb
xoto10 <buylow001@gmail.com>
2019-02-11 10:11:21 +0000

    undo unblocked storm change. bench 3517690

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -52,7 +52,7 @@ namespace {
   // RANK_1 = 0 is used for files where the enemy has no pawn, or their pawn
   // is behind our king.
   constexpr Value UnblockedStorm[int(FILE_NB) / 2][RANK_NB] = {
-    { V( 89), V(107), V(123), V(93), V(77), V( 45), V( 51) },
+    { V( 89), V(107), V(123), V(93), V(57), V( 45), V( 51) },
     { V( 44), V(-18), V(123), V(46), V(39), V( -7), V( 23) },
     { V(  4), V( 52), V(162), V(37), V( 7), V(-14), V( -2) },
     { V(-10), V(-14), V( 90), V(15), V( 2), V( -7), V(-16) }

5ff66ef8ac893d9a03f01ee986ffd1641f0bbea9
xoto10 <buylow001@gmail.com>
2019-02-21 22:04:54 +0000

    use time saved 5,10 if we have more than them. bench 3310239

--- src/search.cpp
+++ src/search.cpp
@@ -506,7 +506,7 @@ void Thread::search() {
           // Stop the search if we have only one legal move, or if available time elapsed
           if (   rootMoves.size() == 1
               || Time.elapsed() > Time.optimum() * bestMoveInstability * fallingEval
-                                  * (Limits.time[us] > Limits.time[~us] ? Time.get_timeToUse(5, 5) : 1.0))
+                                  * (Limits.time[us] > Limits.time[~us] ? Time.get_timeToUse(5, 10) : 1.0))
           {
               Time.save_timeFactor(rootMoves.size() == 1 ? 0.1 : bestMoveInstability * fallingEval);
 

5f9db0c9e87ad660956918ef86236202aa89256a
xoto10 <buylow001@gmail.com>
2019-02-22 20:42:29 +0000

    initiative bonus if better passers, 22x, -12 compensation. bench 3506219

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -759,8 +759,8 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    + 11 * ((eg>0) == (passedDiff>0))
-                    -127 ;
+                    + 22 * ((eg>0) == (passedDiff>0))
+                    -133 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

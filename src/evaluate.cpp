6933f54a0ebefa089baf0bb50a65ecfc06317b98
xoto10 <buylow001@gmail.com>
2019-02-22 19:19:49 +0000

    initiative bonus if still have queen, 20x, -11 compensation. bench 3628084

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -760,7 +760,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     + 20 * ((eg>0 && pos.count<QUEEN>(WHITE)>0) || (eg<0 && pos.count<QUEEN>(BLACK)>0))
-                    -121 ;
+                    -132 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

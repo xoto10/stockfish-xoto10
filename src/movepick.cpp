3e896f31eba3c5fdd800d9aecc754caf3e4c050b
xoto10 <buylow001@gmail.com>
2019-03-07 20:27:28 +0000

    mainhist * 2. bench 3111077

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -119,8 +119,8 @@ void MovePicker::score() {
           // dbg_mean_of((*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]), // -1824.66
           // dbg_mean_of((*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]), // -2120.51
           // dbg_mean_of((*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]), // -2330.96
-          m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)] * 2
+          m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)] * 2
+                   + (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2;

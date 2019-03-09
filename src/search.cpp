bfe7e59b697645aff2b95a95823a447b04824681
xoto10 <buylow001@gmail.com>
2019-03-09 22:18:13 +0000

    adjust SeeDepthMult, 192. bench 3267584

--- src/search.cpp
+++ src/search.cpp
@@ -65,7 +65,7 @@ namespace {
   constexpr int SkipSize[]  = { 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4 };
   constexpr int SkipPhase[] = { 0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6, 7 };
 
-  constexpr Value SeeDepthMult = Value(240);
+  constexpr Value SeeDepthMult = Value(192);
 
   // Razor and futility margins
   constexpr int RazorMargin = 600;

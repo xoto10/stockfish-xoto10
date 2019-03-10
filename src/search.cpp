654b75b71f15ffc0ed47ae930cfab879cf26143a
xoto10 <buylow001@gmail.com>
2019-03-10 18:27:29 +0000

    adjust see depth test, 96+176*d. bench 3598315

--- src/search.cpp
+++ src/search.cpp
@@ -65,7 +65,8 @@ namespace {
   constexpr int SkipSize[]  = { 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4 };
   constexpr int SkipPhase[] = { 0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6, 7 };
 
-  constexpr Value SeeDepthMult = Value(160);
+  constexpr Value SeeDepthBase = Value(96);
+  constexpr Value SeeDepthMult = Value(176);
 
   // Razor and futility margins
   constexpr int RazorMargin = 600;
@@ -985,7 +986,7 @@ moves_loop: // When in check, search starts from here
                   continue;
           }
           else if (   !extension // (~20 Elo)
-                   && !pos.see_ge(move, -SeeDepthMult * (depth / ONE_PLY)))
+                   && !pos.see_ge(move, -SeeDepthBase - SeeDepthMult * (depth / ONE_PLY)))
                   continue;
       }
 

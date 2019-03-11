e7839581cad9bcd38e715af0383de386824b81d5
xoto10 <buylow001@gmail.com>
2019-03-11 09:32:33 +0000

    replace nested min() and max(). no functional change

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -209,7 +209,7 @@ Value Entry::evaluate_shelter(const Position& pos, Square ksq) {
   Value safety = (shift<Down>(theirPawns) & (FileABB | FileHBB) & BlockRanks & ksq) ?
                  Value(374) : Value(5);
 
-  File center = std::max(FILE_B, std::min(FILE_G, file_of(ksq)));
+  File center = file_of(ksq) >= FILE_G ? FILE_G : file_of(ksq) <= FILE_B ? FILE_B : file_of(ksq);
   for (File f = File(center - 1); f <= File(center + 1); ++f)
   {
       b = ourPawns & file_bb(f);

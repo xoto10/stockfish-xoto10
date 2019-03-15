25d6a2d6c7a860d661943088bd0c1340e730fe7c
xoto10 <buylow001@gmail.com>
2019-03-15 09:45:52 +0000

    more time on quick moves, 82/18, 0.7. bench 3318033

--- src/search.cpp
+++ src/search.cpp
@@ -527,7 +527,7 @@ void Thread::search() {
   }
 
   // Use slow moving average to use extra time when regularly moving quickly
-  timeEMA = 0.86 * timeEMA + 0.14 * timeAdjustment;
+  timeEMA = 0.82 * timeEMA + 0.18 * timeAdjustment;
 //if (timeEMA < 0.7)
 //    sdbg_mean_of(1000*timeEMA);  0.58
 //sdbg_mean_of(1000*timeEMA);  0.78 (maybe 0.7?)

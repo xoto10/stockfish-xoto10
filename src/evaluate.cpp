070e878495f399758d15737a54440d60f76f32fe
xoto10 <buylow001@gmail.com>
2019-04-03 23:54:47 +0100

    asymmetry span/2 in passedCount, -89 in initiative(). bench 3170473

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -747,7 +747,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    - 91 ;
+                    - 89 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

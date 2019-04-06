deff4b1bdb7e53916efddc06df362458040222e0
xoto10 <buylow001@gmail.com>
2019-04-06 20:19:25 +0100

    remove pawn count from initiative, and constant. bench 3329101

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -746,11 +746,12 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    + 11 * pos.count<PAWN>()
+//                  + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                         ;
+//dbg_mean_of(pos.count<PAWN>());  9.33
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

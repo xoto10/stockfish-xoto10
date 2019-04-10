89065ad3f2f7cd95b8c5290151a72694afc9b389
xoto10 <buylow001@gmail.com>
2019-04-10 19:08:26 +0100

    include seldepth and nodes in voting, 6 * log10(n). Bench 3666092

--- src/search.cpp
+++ src/search.cpp
@@ -244,7 +244,7 @@ void MainThread::search() {
           int64_t s = th->rootMoves[0].score - minScore + 1;
           votes[th->rootMoves[0].pv[0]] +=  200
                                           + s * s * (th->completedDepth + th->selDepth) / 2
-                                          + 10 * log10(double(th->nodes));
+                                          + 6 * log10(double(th->nodes));
       }
 
       // Select best thread

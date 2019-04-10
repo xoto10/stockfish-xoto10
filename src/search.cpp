c088e5070706179f9b12df736abf3e184487c0fb
xoto10 <buylow001@gmail.com>
2019-04-10 16:40:07 +0100

    include seldepth in thread voting. bench 3243738

--- src/search.cpp
+++ src/search.cpp
@@ -242,7 +242,7 @@ void MainThread::search() {
       for (Thread* th : Threads)
       {
           int64_t s = th->rootMoves[0].score - minScore + 1;
-          votes[th->rootMoves[0].pv[0]] += 200 + s * s * (th->completedDepth + th->selDepth) / 2;
+          votes[th->rootMoves[0].pv[0]] += 200 + s * s * (int(th->completedDepth) + th->selDepth);
       }
 
       // Select best thread

28bcfddcd5f1602e9aefd201418a674a50eb4739
xoto10 <buylow001@gmail.com>
2019-04-16 07:27:17 +0100

    Bonus in initiative for blockedPawns, 10x. Bench 3463821

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -752,8 +752,8 @@ namespace {
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
+                    + 10 * pawnsBlocked
                     + 49 * !pos.non_pawn_material()
-                    - 20 * (pawnsBlocked > 6)
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

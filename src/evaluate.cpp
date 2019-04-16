b0c3a3bbe26692b8941b60f87c4bff30cd46c81d
xoto10 <buylow001@gmail.com>
2019-04-16 06:54:49 +0100

    Penalty in initiative if pawnsBlocked>6, -20. Bench 3594710

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -753,7 +753,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    - 20 * (pawnsBlocked > 8)
+                    - 20 * (pawnsBlocked > 6)
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

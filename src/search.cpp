5e323e5ee29c49c2d190ab464811bbbf6e1c0db4
xoto10 <buylow001@gmail.com>
2019-04-20 20:14:36 +0100

    Remove constant from voting. Bench 3402947

--- src/search.cpp
+++ src/search.cpp
@@ -242,7 +242,7 @@ void MainThread::search() {
       for (Thread* th : Threads)
       {
           int64_t s = th->rootMoves[0].score - minScore + 1;
-          votes[th->rootMoves[0].pv[0]] += 220 + s * s * int(th->completedDepth);
+          votes[th->rootMoves[0].pv[0]] += s * s * int(th->completedDepth);
       }
 
       // Select best thread

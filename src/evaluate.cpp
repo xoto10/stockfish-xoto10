b4297a806033de006ccbd3d78adb79a924789fca
xoto10 <buylow001@gmail.com>
2019-04-22 11:50:14 +0100

    Less weight on pawn count, 9. Bench 3554430

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -746,7 +746,7 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    + 11 * pos.count<PAWN>()
+                    +  9 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()

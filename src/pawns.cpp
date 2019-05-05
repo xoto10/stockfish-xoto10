37af75f07558c5d65ab1a4a169e756a560e8331b
xoto10 <buylow001@gmail.com>
2019-05-05 13:46:59 +0100

    Simplify candidate passers derivation. Bench 3137968

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -110,7 +110,7 @@ namespace {
         // full attack info to evaluate them. Include also not passed pawns
         // which could become passed after one or two pawn pushes when are
         // not attacked more times than defended.
-        if (   !(stoppers ^ lever ^ leverPush)
+        if (   !opposed
             && (support || !more_than_one(lever))
             && popcount(phalanx) >= popcount(leverPush))
             e->passedPawns[Us] |= s;

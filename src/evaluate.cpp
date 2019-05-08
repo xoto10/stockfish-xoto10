3e2e48e90281eaa165db279b7c007467ccb9d074
xoto10 <buylow001@gmail.com>
2019-05-08 01:34:12 +0100

    Also initialise kingattackscount with pawn attacks. Bench 3280771

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -254,8 +254,9 @@ namespace {
     else if (file_of(ksq) == FILE_A)
         kingRing[Us] |= shift<EAST>(kingRing[Us]);
 
-    kingAttacksCount[Them] = popcount(attackedBy[Us][KING] & pe->pawn_attacks(Them));
-    kingAttackersCount[Them] = kingAttackersWeight[Them] = 0;
+    kingAttacksCount[Them] = kingAttackersCount[Them] =
+        popcount(attackedBy[Us][KING] & pe->pawn_attacks(Them));
+    kingAttackersWeight[Them] = 0;
 
     // Remove from kingRing[] the squares defended by two pawns
     kingRing[Us] &= ~dblAttackByPawn;

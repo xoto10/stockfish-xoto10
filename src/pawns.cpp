7b71753fea1189383c0a65978a946bea9e8cb855
xoto10 <buylow001@gmail.com>
2019-05-16 18:48:13 +0100

    Consider all pawns on file for Doubled penalty, 9,42. Bench 3359815

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward = S( 9, 24);
-  constexpr Score Doubled  = S(11, 56);
+  constexpr Score Doubled  = S( 9, 42);
   constexpr Score Isolated = S( 5, 15);
 
   // Connected pawn bonus

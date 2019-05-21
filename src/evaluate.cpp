625b57597dd2517950ecda6596db212880e1be3b
xoto10 <buylow001@gmail.com>
2019-05-21 19:46:24 +0100

    bugfix. Define AheadOfPawns to try to get minors into better positions, bishop,3,0. Bench 3518454

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -320,6 +320,7 @@ namespace {
 
             // Knight and Bishop bonus for being right behind a pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
+                score += MinorBehindPawn;
 
             // Penalty if the piece is far from the king
             score -= KingProtector * distance(s, pos.square<KING>(Us));

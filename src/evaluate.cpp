3bb3c339eb72ad400770b29076c8cca0cd7d0528
xoto10 <buylow001@gmail.com>
2019-05-25 20:23:35 +0100

    Bonus for attacking weak pawns, 3,3. Bench 3560688

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -151,7 +151,7 @@ namespace {
   constexpr Score ThreatByRank       = S( 13,  0);
   constexpr Score ThreatBySafePawn   = S(173, 94);
   constexpr Score TrappedRook        = S( 47,  4);
-  constexpr Score WeakPawnAttacks    = S(  5,  5);
+  constexpr Score WeakPawnAttacks    = S(  3,  3);
   constexpr Score WeakQueen          = S( 49, 15);
   constexpr Score WeakUnopposedPawn  = S( 12, 23);
 

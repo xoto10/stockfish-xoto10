825c3588b59901fb17a8d367255ac3279545262d
xoto10 <buylow001@gmail.com>
2019-05-26 02:27:39 +0100

    Initiative bonus for attacking more pieces, 3*,-108. Bench 3403718

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -751,8 +751,8 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    +  5 * attacks
-                    -112 ;
+                    +  3 * attacks
+                    -108 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

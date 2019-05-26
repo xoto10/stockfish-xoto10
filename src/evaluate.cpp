2c62f657475ec3d1e1ab048e0158a5a198d31bc9
xoto10 <buylow001@gmail.com>
2019-05-26 02:01:10 +0100

    Initiative bonus for restricting opponent's moves. Bench 3498802

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -742,13 +742,19 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
+    int restrict =  (eg > 0) * popcount((attackedBy[WHITE][ALL_PIECES] & ~attackedBy2[BLACK])
+                                        | attackedBy2[WHITE])
+                  + (eg < 0) * popcount((attackedBy[BLACK][ALL_PIECES] & ~attackedBy2[WHITE])
+                                        | attackedBy2[BLACK]);
+
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                    +      restrict
+                    -132 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

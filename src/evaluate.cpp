2289dd5604e0eaa3b769a51e1ca62b96f6fa10f8
xoto10 <buylow001@gmail.com>
2019-05-27 13:52:48 +0100

    Bishop kd bonus only if kd > 200. Bench 3606624

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -466,7 +466,6 @@ namespace {
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
-                 -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
@@ -476,7 +475,12 @@ namespace {
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
+    {
+        if (kingDanger > 200)
+            kingDanger -= 35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING]);
+
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
+    }
 
     // Penalty when our king is on a pawnless flank
     if (!(pos.pieces(PAWN) & KingFlank[file_of(ksq)]))

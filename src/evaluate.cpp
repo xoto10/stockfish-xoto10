87dd399483acb78baed30d4c3d42421206eb44d8
xoto10 <buylow001@gmail.com>
2019-05-29 08:52:39 +0100

    Initiative bonus for attacking more squares in their side of board, 1x,-113. Bench 3580664

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -754,8 +754,8 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    +  3 * attacks
-                    -133 ;
+                    +      attacks
+                    -113 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

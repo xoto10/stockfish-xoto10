615656bc0e39e6d12b0c7eef9cfc70cefafa5352
xoto10 <buylow001@gmail.com>
2019-05-30 13:12:17 +0100

    Initiative bonus for number of center file (d/e) pawns, 5x,-110. Bench 3964423

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -745,11 +745,11 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
-                    +  9 * pe->center_pawns()
+                    +  5 * pe->center_pawns()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -116 ;
+                    -110 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

f9ea1cdb78b2d7bd433f4a02077279ae71f9d23c
xoto10 <buylow001@gmail.com>
2019-06-01 22:20:38 +0100

    pawnsOnBothFlanks as int (Viz suggestion), 10*pawns,6*both. Bench 3538891

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -739,14 +739,14 @@ namespace {
     int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                      - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
-    bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
-                            && (pos.pieces(PAWN) & KingSide);
+    int kingsidePawns = popcount(pos.pieces(PAWN) & KingSide);
+    int pawnsOnBothFlanks = std::min(kingsidePawns, pos.count<PAWN>() - kingsidePawns);
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    + 11 * pos.count<PAWN>()
+                    + 10 * pos.count<PAWN>()
                     +  9 * outflanking
-                    + 18 * pawnsOnBothFlanks
+                    +  6 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     -103 ;
 

aa57388b7bb5b46820c01ec14872440715d6242b
xoto10 <buylow001@gmail.com>
2019-06-01 19:32:46 +0100

    pawnsOnBothFlanks as an int, remove pawn bonus, 27*. Bench 3811631

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -739,14 +739,13 @@ namespace {
     int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                      - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
-    bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
-                            && (pos.pieces(PAWN) & KingSide);
+    int pawnsOnBothFlanks = std::min(popcount(pos.pieces(PAWN) & QueenSide),
+                                     popcount(pos.pieces(PAWN) & KingSide));
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    + 11 * pos.count<PAWN>()
                     +  9 * outflanking
-                    + 18 * pawnsOnBothFlanks
+                    + 27 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     -103 ;
 

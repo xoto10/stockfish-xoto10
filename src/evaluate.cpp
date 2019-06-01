e4f2c255b49b0658bbdb43f392a61db9e273f5ca
xoto10 <buylow001@gmail.com>
2019-06-01 21:36:29 +0100

    pawnsOnBothFlanks as an int, 10*pawns,7*both. Bench 3173105

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -746,7 +746,7 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 10 * pos.count<PAWN>()
                     +  9 * outflanking
-                    +  6 * pawnsOnBothFlanks
+                    +  7 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     -103 ;
 

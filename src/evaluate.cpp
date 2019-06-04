bccc9687e39146771bc96e9d8db12b1ee353b010
xoto10 <buylow001@gmail.com>
2019-06-04 18:55:07 +0100

    Floor for complexity at -50. Bench 3803635

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -756,7 +756,7 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
     // that the endgame score will never change sign after the bonus.
-    int v = ((eg > 0) - (eg < 0)) * std::max( {complexity, -abs(eg), -40} );
+    int v = ((eg > 0) - (eg < 0)) * std::max( {complexity, -abs(eg), -50} );
 
     if (T)
         Trace::add(INITIATIVE, make_score(0, v));

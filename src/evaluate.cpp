73ea3345bc137b1d5af37e1473c42b13a655b600
xoto10 <buylow001@gmail.com>
2019-06-04 13:30:19 +0100

    pawnsOnBothFlanks as an int, 7*pawns,8*both. Bench 3674279

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -744,9 +744,9 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    +  9 * pos.count<PAWN>()
+                    +  7 * pos.count<PAWN>()
                     +  9 * outflanking
-                    +  6 * pawnsOnBothFlanks
+                    +  8 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     -103 ;
 

ddbd1548403ef2787ca9318450f82cbaa0de9a61
xoto10 <buylow001@gmail.com>
2019-06-04 18:48:23 +0100

    Initiative bonus for attacking pieces not defended twice, 5*,-110. Bench 3431062

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -742,13 +742,19 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
+    int attacks =  (eg > 0) * popcount(  pos.pieces(BLACK) & attackedBy[WHITE][ALL_PIECES]
+                                       & ~attackedBy2[BLACK])
+                 + (eg < 0) * popcount(  pos.pieces(WHITE) & attackedBy[BLACK][ALL_PIECES]
+                                       & ~attackedBy2[WHITE]);
+
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                    +  5 * attacks
+                    -110 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

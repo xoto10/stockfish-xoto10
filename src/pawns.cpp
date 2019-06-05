e7f69909d08e1680635036be00424d145be090ba
xoto10 <buylow001@gmail.com>
2019-06-05 21:39:59 +0100

    Bonus if unopposed pawn has 0/1 opponent neighbours, 7,7. Bench 3604533

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,7 @@ namespace {
   constexpr Score Backward = S( 9, 24);
   constexpr Score Doubled  = S(11, 56);
   constexpr Score Isolated = S( 5, 15);
-  constexpr Score Unopposed = S( 3,  3);
+  constexpr Score Unopposed = S( 7,  7);
 
   // Connected pawn bonus
   constexpr int Connected[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };

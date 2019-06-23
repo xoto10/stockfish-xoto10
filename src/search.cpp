7f39bd36f8ab314cb8abd781f1ce9aa56e084a9f
xoto10 <buylow001@gmail.com>
2019-06-23 16:27:23 +0100

    Use TT on PvNodes if ply > 32 (remove debug). Bench 3398333

--- src/search.cpp
+++ src/search.cpp
@@ -144,25 +144,6 @@ namespace {
 } // namespace
 
 
-/// Debug functions used mainly to collect run-time statistics
-static int64_t shits[2], smeans[2];
-
-void sdbg_hit_on(bool b) { ++shits[0]; if (b) ++shits[1]; }
-void sdbg_hit_on(bool c, bool b) { if (c) sdbg_hit_on(b); }
-void sdbg_mean_of(int v) { ++smeans[0]; smeans[1] += v; }
-
-void sdbg_print() {
-
-  if (shits[0])
-      sync_cout << "info string Total " << shits[0] << " Hits " << shits[1]
-           << " hit rate (%) " << 100 * shits[1] / shits[0] << sync_endl;
-
-  if (smeans[0])
-      sync_cout << "info string Total " << smeans[0] << " Mean "
-           << (double)smeans[1] / smeans[0] << sync_endl;
-}
-
-
 /// Search::init() is called at startup to initialize various lookup tables
 
 void Search::init() {
@@ -498,7 +479,6 @@ void Thread::search() {
           }
       }
   }
-sdbg_print();
 
   if (!mainThread)
       return;

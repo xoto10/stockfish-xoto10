12b15af3713ac44f45c8b221dd3ab228fa48d4a2
xoto10 <buylow001@gmail.com>
2019-07-01 22:03:45 +0100

    Less LMR at root node if marked, 3*. Bench 3596270

--- src/search.cpp
+++ src/search.cpp
@@ -1062,7 +1062,7 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3 * ONE_PLY
-          &&  moveCount > 1 + (3 + 2 * th.marked()) * rootNode
+          &&  moveCount > 1 + (3 + 3 * th.marked()) * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha))

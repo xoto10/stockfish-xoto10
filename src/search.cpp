ed8409d12081eaf39b560c031999e250ed111644
xoto10 <buylow001@gmail.com>
2019-07-04 19:42:29 +0100

    Undo second reduction if marked. Bench 3596270

--- src/search.cpp
+++ src/search.cpp
@@ -1099,7 +1099,7 @@ moves_loop: // When in check, search starts from here
               // hence break make_move(). (~5 Elo)
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(make_move(to_sq(move), from_sq(move))))
-                  r -= 2 * ONE_PLY;
+                  r -= (2 - th.marked()) * ONE_PLY;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]

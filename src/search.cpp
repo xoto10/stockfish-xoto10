958da428edd766799e0df35f4d92f9f5be1b9824
xoto10 <buylow001@gmail.com>
2019-07-04 20:23:44 +0100

    Max history reduction of 1 if marked. Bench 3596270

--- src/search.cpp
+++ src/search.cpp
@@ -1115,7 +1115,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r -= ss->statScore / 20000 * ONE_PLY;
+              r -= std::min(ss->statScore / 20000, th.marked() ? 1 : MAX_MOVES) * ONE_PLY;
           }
 
           Depth d = clamp(newDepth - r, ONE_PLY, newDepth);

4714d0ceb1e497912395e0febb4f4d85533f6308
xoto10 <buylow001@gmail.com>
2019-07-05 20:32:51 +0100

    Statscore reduction, /8096. Bench 2723275

--- src/search.cpp
+++ src/search.cpp
@@ -1115,7 +1115,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r -= ss->statScore / 32768 * ONE_PLY;
+              r -= ss->statScore / 8192 * ONE_PLY;
           }
 
           Depth d = clamp(newDepth - r, ONE_PLY, newDepth);

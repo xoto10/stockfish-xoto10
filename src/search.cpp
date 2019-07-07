c49e2cf4cac00c46bac0cc39d45810f360ea6d4e
xoto10 <buylow001@gmail.com>
2019-07-07 18:27:25 +0100

    Divide by 256 instead of 200. Bench 3939264

--- src/search.cpp
+++ src/search.cpp
@@ -792,7 +792,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min(int(eval - beta) / 200, 3)) * ONE_PLY;
+        Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min(int(eval - beta) / 256, 3)) * ONE_PLY;
 
         ss->currentMove = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[NO_PIECE][0];

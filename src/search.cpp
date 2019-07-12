de4874e59bae977bb479bbfc3a7327cba15e1dbd
xoto10 <buylow001@gmail.com>
2019-07-12 19:58:57 +0100

    Extra ply on stat_bonus for quiet ttMove that fails low. Bench 3089977

--- src/search.cpp
+++ src/search.cpp
@@ -671,7 +671,7 @@ namespace {
             // Penalty for a quiet ttMove that fails low
             else if (!pos.capture_or_promotion(ttMove))
             {
-                int penalty = -stat_bonus(depth);
+                int penalty = -stat_bonus(depth + ONE_PLY);
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
             }

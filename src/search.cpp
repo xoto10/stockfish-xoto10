06640bf32f8412910a48bcc420a025a1757e1626
xoto10 <buylow001@gmail.com>
2019-07-20 21:08:52 +0100

    movecount adjustment in probcut. Bench 3608695

--- src/search.cpp
+++ src/search.cpp
@@ -842,7 +842,7 @@ namespace {
         &&  depth >= 5 * ONE_PLY
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
-        Value raisedBeta = std::min(beta + 216 - 48 * improving, VALUE_INFINITE);
+        Value raisedBeta = std::min(beta + 216 - 48 * improving - (ss-2)->moveCount, VALUE_INFINITE);
         MovePicker mp(pos, ttMove, raisedBeta - ss->staticEval, &thisThread->captureHistory);
         int probCutCount = 0;
 

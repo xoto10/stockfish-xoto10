1fd49466a9d530b3b1900917357ecc1d0e5118f2
xoto10 <buylow001@gmail.com>
2019-07-24 15:28:37 +0100

    Max +4400 for lmr seege test (intended -4400 but try it). Bench 309935

--- src/search.cpp
+++ src/search.cpp
@@ -1041,7 +1041,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)
-              if (!pos.see_ge(move, Value(-29 * lmrDepth * lmrDepth)))
+              if (!pos.see_ge(move, Value(std::max(4400, -29 * lmrDepth * lmrDepth))))
                   continue;
           }
           else if (  (!givesCheck || !extension)

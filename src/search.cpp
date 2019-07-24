5a0047c72e5969e217d6837f6965d814cba82577
xoto10 <buylow001@gmail.com>
2019-07-24 19:28:06 +0100

    Lmr seege test, 21*, max -2400. Bench 3298516

--- src/search.cpp
+++ src/search.cpp
@@ -1041,7 +1041,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)
-              if (!pos.see_ge(move, Value(std::max(-2400, -29 * lmrDepth * lmrDepth))))
+              if (!pos.see_ge(move, Value(std::max(-2400, -21 * lmrDepth * lmrDepth))))
                   continue;
           }
           else if (  (!givesCheck || !extension)

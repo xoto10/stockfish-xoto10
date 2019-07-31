3c4c5ad37469db6b9b27c55364653712876391af
xoto10 <buylow001@gmail.com>
2019-07-31 11:23:17 +0100

    Change delta back to 20. Bench 3690246

--- src/search.cpp
+++ src/search.cpp
@@ -412,7 +412,7 @@ void Thread::search() {
           if (rootDepth >= 4 * ONE_PLY)
           {
               Value previousScore = rootMoves[pvIdx].previousScore;
-              delta = Value(23);
+              delta = Value(20);
               alpha = std::max(previousScore - delta,-VALUE_INFINITE);
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 

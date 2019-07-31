b02a3fc6922d617181bc1c90aea4bdadfaa81010
xoto10 <buylow001@gmail.com>
2019-07-31 04:51:18 +0100

    Derive avgDepth. No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -300,6 +300,7 @@ void MainThread::search() {
   }
 
   previousScore = bestThread->rootMoves[0].score;
+  avgDepth = (3 * avgDepth + rootDepth) / 4;
 
   // Send again PV info if we have a new best thread
   if (bestThread != this)

697dcb00f6c1334398c80afd79ceb5bc6fdfce3a
xoto10 <buylow001@gmail.com>
2019-08-01 21:07:06 +0100

    Remove depth scaling from nmp test, -229. Bench 3428586

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= beta - 33 * depth / ONE_PLY + 299
+        &&  ss->staticEval >= beta - 229
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

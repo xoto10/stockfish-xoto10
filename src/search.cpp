113eb838f56a0c4772cb7bce442782a75bf2550d
xoto10 <buylow001@gmail.com>
2019-08-06 12:37:53 +0100

    Nmp test, set depth multiplier according to rootdepth, 44-rd/2. Bench 3912420

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= beta - (45 * depth - 3 * thisThread->rootDepth) / ONE_PLY + 299
+        &&  ss->staticEval >= beta - (44 - thisThread->rootDepth / 2) * depth / ONE_PLY + 299
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

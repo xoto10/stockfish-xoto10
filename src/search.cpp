7a5913367c0301d8914410b3107b28da2e3079e3
xoto10 <buylow001@gmail.com>
2019-08-07 12:50:47 +0100

    Nmp test, -53*d + 5*rd + 299. Bench 3874301

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 22661
         &&  eval >= beta
-        &&  ss->staticEval >= beta - (33 * depth - 11 * thisThread->rootDepth) / ONE_PLY + 57
+        &&  ss->staticEval >= beta - (53 * depth - 5 * thisThread->rootDepth) / ONE_PLY + 299
         && !excludedMove
         &&  pos.non_pawn_material(us)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))

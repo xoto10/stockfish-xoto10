df3e3651a525fe6213b6560d54f76630be4c725e
xoto10 <buylow001@gmail.com>
2019-08-19 04:51:35 +0100

    Inverse scale ct with opponent's npm if losing. Bench 3990226

--- src/search.cpp
+++ src/search.cpp
@@ -418,7 +418,8 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + 86 * previousScore / (abs(previousScore) + 176);
+              int dct =  (previousScore < 0 ? ct * (8331 - rootPos.non_pawn_material(~us)) / 8331 : ct)
+                       + 86 * previousScore / (abs(previousScore) + 176);
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));

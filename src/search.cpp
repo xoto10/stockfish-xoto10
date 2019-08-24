60fc18d60200b6bae949801fee0de1237345161c
xoto10 <buylow001@gmail.com>
2019-08-24 20:02:20 +0100

    Tuned delta increase. Bench 3860795

--- src/search.cpp
+++ src/search.cpp
@@ -474,7 +474,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 4 + 5;
+              delta += (delta * 61 + 1256) / 256;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }

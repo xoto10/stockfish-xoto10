1a15841d60ec15ea705df52e19b024ea37c61649
xoto10 <buylow001@gmail.com>
2019-08-26 20:58:37 +0100

    Initiative bonus for wider attacks, 6*. Bench 3854022

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,7 +743,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    + 10 * ((eg > 0) * attackWidth[WHITE] + (eg < 0) * attackWidth[BLACK])
+                    +  6 * ((eg > 0) * attackWidth[WHITE] + (eg < 0) * attackWidth[BLACK])
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

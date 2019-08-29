1713b9ae7935a3930e429af4373216b8057019a7
xoto10 <buylow001@gmail.com>
2019-08-29 17:01:05 +0100

    Increase time use for more blocked pawns and more possible moves. Bench 3568210

--- src/search.cpp
+++ src/search.cpp
@@ -378,7 +378,7 @@ void Thread::search() {
   contempt = (us == WHITE ?  make_score(ct, ct / 2)
                           : -make_score(ct, ct / 2));
 
-  // Increase time use according to number of blocked pawns
+  // Count blocked pawns (used to increase time use in complex positions)
   double rams = (std::min(4, popcount(shift<NORTH>(rootPos.pieces(WHITE, PAWN)) & rootPos.pieces(BLACK, PAWN))) - 1)
                 / 30.0 + 1.0;
 
@@ -535,9 +535,12 @@ void Thread::search() {
           }
           double bestMoveInstability = 1 + totBestMoveChanges / Threads.size();
 
+          // Increase time use in complex positions
+          double positional = rams * clamp(rootMoves.size() / 124.0 + 0.75, 0.9, 1.1);
+
           // Stop the search if we have only one legal move, or if available time elapsed
           if (   rootMoves.size() == 1
-              || Time.elapsed() > Time.optimum() * fallingEval * reduction * bestMoveInstability * rams)
+              || Time.elapsed() > Time.optimum() * fallingEval * reduction * bestMoveInstability * positional)
           {
               // If we are allowed to ponder do not stop the search now but
               // keep pondering until the GUI sends "ponderhit" or "stop".

abd9c0e3911ff1bd0929a3bec0c9922bda160be2
Vizvezdenec <Vizvezdenec@gmail.com>
2019-09-05 02:06:29 +0300

    twiikg. bench 3884996

--- src/search.cpp
+++ src/search.cpp
@@ -1075,8 +1075,8 @@ moves_loop: // When in check, search starts from here
       // Step 16. Reduced depth search (LMR). If the move fails high it will be
       // re-searched at full depth.
       if (    depth >= 3 * ONE_PLY
-          &&  moveCount > 1 + 2 * rootNode
-          && (!rootNode || thisThread->best_move_count(move) == 0 || moveCount > 2 * depth / ONE_PLY)
+          &&  moveCount > 1 + 2 * rootNode - (ss->staticEval > beta + futility_margin(depth, false))
+          && (!rootNode || thisThread->best_move_count(move) == 0)
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha

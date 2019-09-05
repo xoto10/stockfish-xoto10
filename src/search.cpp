d1934d67d1aad9194614effdf53d46c7c00d539c
xoto10 <buylow001@gmail.com>
2019-09-05 02:03:57 +0100

    Only on PV nodes, -1000. Bench 3903706

--- src/search.cpp
+++ src/search.cpp
@@ -1283,8 +1283,10 @@ moves_loop: // When in check, search starts from here
             && !pos.captured_piece())
                 update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + ONE_PLY));
 
-        if (bestValue > beta)
-            (ss-1)->statScore -= 10000;
+        if (PvNode && bestValue > beta)
+        {
+            (ss-1)->statScore -= 1000;
+        }
 
     }
     // Bonus for prior countermove that caused the fail low

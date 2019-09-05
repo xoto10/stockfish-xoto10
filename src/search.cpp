a7570963e7fb34d79740b7c5b8dba2ee75895fed
xoto10 <buylow001@gmail.com>
2019-09-05 01:50:56 +0100

    try to break it, -10000. bench 3998658

--- src/search.cpp
+++ src/search.cpp
@@ -1284,7 +1284,7 @@ moves_loop: // When in check, search starts from here
                 update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + ONE_PLY));
 
         if (bestValue > beta)
-            (ss-1)->statScore -= 1000;
+            (ss-1)->statScore -= 10000;
 
     }
     // Bonus for prior countermove that caused the fail low

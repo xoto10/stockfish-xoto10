1b785d68529137fef61d924d03a3781bd561db01
xoto10 <buylow001@gmail.com>
2019-09-07 16:04:41 +0100

    Initiative bonus for differing npm, +20. Bench 3579466

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -726,7 +726,8 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -103 ;
+                    + 20 * (pos.non_pawn_material(WHITE) != pos.non_pawn_material(BLACK))
+                    -117 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

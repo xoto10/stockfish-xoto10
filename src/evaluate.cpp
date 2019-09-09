c75401f5613cb452dc9d25ef6489e1880988e9cd
xoto10 <buylow001@gmail.com>
2019-09-09 12:45:56 +0100

    Initiative bonus for not having opposite bishops, 18,-121. Bench 3922513

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -726,8 +726,8 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    + 10 * !pos.opposite_bishops()
-                    -114 ;
+                    + 18 * !pos.opposite_bishops()
+                    -121 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

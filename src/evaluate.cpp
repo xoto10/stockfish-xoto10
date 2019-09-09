ca28c92886837186f2e48c0d4dbbce6dbc224677
xoto10 <buylow001@gmail.com>
2019-09-09 11:39:27 +0100

    Initiative bonus for different material, 4. Bench 3723678

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -726,7 +726,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    +  6 * (pos.non_pawn_material(WHITE) != pos.non_pawn_material(BLACK))
+                    +  4 * (pos.non_pawn_material(WHITE) != pos.non_pawn_material(BLACK))
                     -106 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting

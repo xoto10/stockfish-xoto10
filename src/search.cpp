8650165666f01bbe23f94f2447effc19fe4b8657
xoto10 <buylow001@gmail.com>
2019-09-11 12:50:50 +0100

    Revert stat_bonus change since -ve at depth 1. Bench 5197666

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d > 17 ? -7 : 19 * d * d + 111 * d - 151;
+    return d > 17 ? -8 : 22 * d * d + 151 * d - 140;
   }
 
   // Add a small random component to draw evaluations to avoid 3fold-blindness

4644c4d5a80cd52b5aff220ea515c1bc39b1aa0a
xoto10 <buylow001@gmail.com>
2019-09-12 23:04:04 +0100

    Increase kingdanger if only 0 or 1 free pawns in kingflank, 50*(2-fp)^2. Bench 3612339

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -474,7 +474,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 + 100 * (2 - freePawns)
+                 +  50 * (2 - freePawns) * (2 - freePawns)
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation

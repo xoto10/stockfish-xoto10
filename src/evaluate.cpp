37853879852e1b02707477a5fb672f6f56227f64
xoto10 <buylow001@gmail.com>
2019-09-13 12:39:19 +0100

    Remove subtraction from complexity so it is always positive. Bench 3547796

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -735,8 +735,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    + 36 * winnable
-                    -103 ;
+                    + 36 * winnable;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so

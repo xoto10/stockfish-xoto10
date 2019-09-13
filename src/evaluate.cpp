8e67d580825452a1c1cb2d8353cde86f68767ed8
xoto10 <buylow001@gmail.com>
2019-09-13 09:14:10 +0100

    Increase scale factor if not main OCB case and pawnsonbothflanks, +4. Bench 3869737

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -765,14 +765,17 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 16 + 4 * pe->passed_count();
         else
-            sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
+        {
+            bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
+                                    && (pos.pieces(PAWN) & KingSide);
+
+            sf = std::min(  40
+                          + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide)
+                          + 4 * pawnsOnBothFlanks
+                         , sf);
+        }
 
     }
-
-    bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
-                            && (pos.pieces(PAWN) & KingSide);
-    if (pawnsOnBothFlanks)
-        sf += 4;
 
     return ScaleFactor(sf);
   }

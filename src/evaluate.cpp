6a809920683a2283588c96473af09d373098bd47
xoto10 <buylow001@gmail.com>
2019-09-13 18:39:43 +0100

    Reduce complexity if mg*eg < 0 and abs(mg) > 200. Bench 3893714

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -737,7 +737,7 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -      (mg * eg < 0 && abs(mg) > 150) * (abs(mg) - abs(eg) / 2) / 8
+                    -      (mg * eg < 0 && abs(mg) > 200) * (abs(mg) - abs(eg) / 2) / 8
                     - 36 * almostUnwinnable
                     -103 ;
 

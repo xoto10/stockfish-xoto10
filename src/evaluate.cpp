8c6884cc09899972e2442f1e1b7ee59822903b3d
xoto10 <buylow001@gmail.com>
2019-09-14 13:03:33 +0100

    Reduce rookonfile, 16/7,40/20. Bench 3715745

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -108,7 +108,7 @@ namespace {
 
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is
   // no (friendly) pawn on the rook file.
-  constexpr Score RookOnFile[] = { S(18, 7), S(44, 20) };
+  constexpr Score RookOnFile[] = { S(16, 7), S(40, 20) };
 
   // ThreatByMinor/ByRook[attacked PieceType] contains bonuses according to
   // which piece type attacks which one. Attacks on lesser pieces which are

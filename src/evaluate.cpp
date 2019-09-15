1fa269f31ab1c9c5f6093897844b5402425c76a6
xoto10 <buylow001@gmail.com>
2019-09-15 22:09:39 +0100

    snicolet's scale_factor8 patch. Bench 4272173

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -769,8 +769,7 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 16 + 4 * pe->passed_count();
         else
-            sf = std::min(43 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide)
-                             - 4 * (pos.count<ROOK>(strongSide) + pos.count<QUEEN>(strongSide)), sf);
+            sf = std::min(36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4  );
     }

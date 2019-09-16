4362c82d694ee2541123654756896c0aa833d3c8
xoto10 <buylow001@gmail.com>
2019-09-16 11:19:15 +0100

    Initiative: 5*outflanking, 16*pawnsonbothflanks. Bench 3681950

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -735,8 +735,8 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
-                    +  3 * outflanking
-                    + 18 * pawnsOnBothFlanks
+                    +  5 * outflanking
+                    + 16 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
                     -103 ;

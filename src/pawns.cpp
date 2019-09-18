5768a5e4910bd4a74b10a1be4691b054723ecbdd
xoto10 <buylow001@gmail.com>
2019-09-18 19:29:34 +0100

    Bonus if unopposed pawn has adjacent file(s) with no pawn, 2,10,wu+1,5. Bench 3751207

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -36,9 +36,9 @@ namespace {
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
-  constexpr Score UnopposedAdj  = S(10,  2);
+  constexpr Score UnopposedAdj  = S( 2, 10);
   constexpr Score WeakLever     = S( 0, 56);
-  constexpr Score WeakUnopposed = S(13, 27);
+  constexpr Score WeakUnopposed = S(14, 32);
 
   // Connected pawn bonus
   constexpr int Connected[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };

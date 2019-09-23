d44f4a8042e257504126d3e7f0566eb858866dfc
xoto10 <buylow001@gmail.com>
2019-09-23 09:00:39 +0100

    Tuned values for separate delta values. bench 3872478

--- src/search.cpp
+++ src/search.cpp
@@ -461,7 +461,7 @@ void Thread::search() {
               {
                   beta = (alpha + beta) / 2;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
-                  delta += (delta * 10 + 180) / 32;
+                  delta += (delta * 81 + 1384) / 256;
 
                   failedHighCnt = 0;
                   if (mainThread)
@@ -470,7 +470,7 @@ void Thread::search() {
               else if (bestValue >= beta)
               {
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
-                  delta += (delta * 6 + 140) / 32;
+                  delta += (delta * 56 + 1143) / 256;
                   ++failedHighCnt;
               }
               else

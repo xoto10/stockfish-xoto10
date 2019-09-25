9b37fc2e69e7a4cb766032fa53a2b914149776c4
xoto10 <buylow001@gmail.com>
2019-09-26 00:55:06 +0100

    rooksafecheck, 1050. Bench 3642939

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -81,8 +81,8 @@ namespace {
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 77, 55, 44, 10 };
 
   // Penalties for enemy's safe checks
-  constexpr int QueenSafeCheck  = 760;
-  constexpr int RookSafeCheck   = 1080;
+  constexpr int QueenSafeCheck  = 780;
+  constexpr int RookSafeCheck   = 1050;
   constexpr int BishopSafeCheck = 635;
   constexpr int KnightSafeCheck = 790;
 

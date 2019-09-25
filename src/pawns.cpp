56bf9f4979c36d6341282e6a32a83615c90382af
xoto10 <buylow001@gmail.com>
2019-09-25 17:28:06 +0100

    Tuned values for more detailed connected calc. Bench 4158732

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -31,9 +31,6 @@ namespace {
   #define V Value
   #define S(mg, eg) make_score(mg, eg)
 
-int A=512,B=256,C=256,D=210,E=512,F=256,G=256,H=210,I=192,J=192;
-//TUNE(SetRange(0,1024),A,B,C,D,E,F,G,H,I,J);
-
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
   constexpr Score BlockedStorm  = S(82, 82);
@@ -43,8 +40,8 @@ int A=512,B=256,C=256,D=210,E=512,F=256,G=256,H=210,I=192,J=192;
   constexpr Score WeakUnopposed = S(13, 27);
 
   // Connected pawn bonus
-  constexpr int CM[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };
-  constexpr int CE[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };
+  constexpr int ConnectedMg[RANK_NB] = { 0, 7, 8, 11, 28, 48, 85 };
+  constexpr int ConnectedEg[RANK_NB] = { 0, 7, 7, 12, 27, 49, 89 };
 
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
@@ -134,10 +131,10 @@ int A=512,B=256,C=256,D=210,E=512,F=256,G=256,H=210,I=192,J=192;
         // Score this pawn
         if (support | phalanx)
         {
-            int u =  CM[r] * (A + B*bool(phalanx) - C*opposed)/256
-                   + D * popcount(support) / 10 + I * (r - 2) / 64;
-            int v =  CE[r] * (E + F*bool(phalanx) - G*opposed)/256
-                   + H * popcount(support) / 10 + J * (r - 2) / 64;
+            int u =  ConnectedMg[r] * (472 + 275 * bool(phalanx) - 254 * opposed) / 256
+                   + 183 * popcount(support) / 10 + 199 * (r - 2) / 64;
+            int v =  ConnectedEg[r] * (421 + 232 * bool(phalanx) - 283 * opposed) / 256
+                   + 178 * popcount(support) / 10 + 180 * (r - 2) / 64;
 
             score += make_score(u, v);
         }

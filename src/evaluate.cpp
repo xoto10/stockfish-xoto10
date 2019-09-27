63b4c4dde5c33da362c4f46b45a5ac29aed22fb5
xoto10 <buylow001@gmail.com>
2019-09-27 17:47:56 +0100

    Add pawn attack into kingflankattacks, 132,100zone. Bench 4150539

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -469,7 +469,7 @@ namespace {
         if (blocked)
         {
             kingZone = pos.pieces(Them, PAWN) & (KingFlank[file_of(ksq)] & ~CentFiles) & Camp;
-            pawnAttack = 68 + 100 * bool(kingZone);
+            pawnAttack = 132 + 100 * bool(kingZone);
 //          kingFlankAttacks += popcount(pos.pieces(Them, PAWN) & kingZone);
         }
     }

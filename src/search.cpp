a7ee9e9088d3fd6f61f4236ceb928a6c388d961b
xoto10 <buylow001@gmail.com>
2019-10-01 15:49:37 +0100

    Vary cmp threshold for conthist 0 and 1. Bench 3792094

--- src/search.cpp
+++ src/search.cpp
@@ -1038,8 +1038,8 @@ moves_loop: // When in check, search starts from here
 
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
-                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold - 30
+                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold + 30)
                   continue;
 
               // Futility pruning: parent node (~2 Elo)

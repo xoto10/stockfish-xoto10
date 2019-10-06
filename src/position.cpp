600455290b66a1c6cebb525fb7043238ca3c95fb
xoto10 <buylow001@gmail.com>
2019-10-06 20:32:42 +0100

    Lower see_ge threshold if capture is near their king, <3,PawnEg. Bench 4229295

--- src/position.cpp
+++ src/position.cpp
@@ -1060,8 +1060,8 @@ bool Position::see_ge(Move m, Value threshold) const {
   Value balance;   // Values of the pieces taken by us minus opponent's ones
   Square ksq = square<KING>(stm);
 
-  if (distance<File>(to, ksq) + distance<Rank>(to, ksq) < 4)
-      threshold -= 2 * PawnValueEg;
+  if (distance<File>(to, ksq) + distance<Rank>(to, ksq) < 3)
+      threshold -= PawnValueEg;
 
   // The opponent may be able to recapture so this is the best result
   // we can hope for.

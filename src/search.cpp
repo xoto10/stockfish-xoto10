5377bfc40114fb24d23c4590b71ff6d706c6bd8c
xoto10 <buylow001@gmail.com>
2019-10-07 15:03:12 +0100

    Use previousScore and previous delta to initialise delta, 21+(prv+8d)/256. Bench 4008770

--- src/search.cpp
+++ src/search.cpp
@@ -413,7 +413,9 @@ void Thread::search() {
           if (rootDepth >= 4)
           {
               Value previousScore = rootMoves[pvIdx].previousScore;
-              delta = Value((147 + delta) / 8);
+//sync_cout << "info string xo " << (147+delta)/8 << " ala " << 21+abs(previousScore)/128
+//          << " new " << 21 + (abs(previousScore) + 8 * delta) / 256 << sync_endl;
+              delta = Value(21 + (abs(previousScore) + 8 * delta) / 256);
               alpha = std::max(previousScore - delta,-VALUE_INFINITE);
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 

ef7bf7ea414a8795b0f7b141e9229252d6c7c8c4
xoto10 <buylow001@gmail.com>
2019-10-13 12:29:03 +0100

    Initiative penalty for rams, 10*(ramcount-1)^2. Bench 3870188

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,7 +723,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
-                    - 20 * abs(pe->ram_count() - 2)
+                    - 10 * (pe->ram_count() - 1) * (pe->ram_count() - 1)
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the

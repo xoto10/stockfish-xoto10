099c4edbda178a48c78b5b1cca7376b55aa99141
xoto10 <buylow001@gmail.com>
2019-10-13 20:53:15 +0100

    Reduce sf for blocked pawns, 4*blocked. Bench 3743320

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -753,7 +753,13 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 16 + 4 * pe->passed_count();
         else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
+        {
+            Bitboard blocked = pos.pieces(WHITE, PAWN) & shift<SOUTH>(pos.pieces(BLACK, PAWN));
+
+            sf = std::min(sf,  38
+                             + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide)
+                             - 4 * popcount(blocked));
+        }
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4  );
     }

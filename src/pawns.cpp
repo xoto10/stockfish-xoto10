d73a3a268004a8826568fa0d31703179d2cb244c
xoto10 <buylow001@gmail.com>
2019-10-23 20:26:56 +0100

    KS bonus for stopping their edge pawn, 4,4. Bench 4916360

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
-  constexpr Score BlockedEdge   = S(10, 10);
+  constexpr Score BlockedEdge   = S( 4,  4);
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);

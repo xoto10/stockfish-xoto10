9e271fe92b0175ece9a040d0175a5717475ed52f
xoto10 <buylow001@gmail.com>
2019-10-28 20:19:10 +0000

    N/B reductions in kd only if kd > 0. Bench 5115841

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -455,10 +455,12 @@ namespace {
                  +   3 * kingFlankAttacks * kingFlankAttacks / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
-                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
-                 -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   7;
+
+    if (kingDanger > 0)
+        kingDanger -=  100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
+                     +  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING]);
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)

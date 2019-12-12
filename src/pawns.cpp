97afbc48734da6fa11eb76b723ed8710e9ecb7b3
xoto10 <buylow001@gmail.com>
2019-12-12 17:27:17 +0000

    Only part Isolated penalty if no stoppers, 3,8. Bench 5369633

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -35,7 +35,8 @@ namespace {
   constexpr Score Backward      = S( 9, 24);
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
-  constexpr Score Isolated      = S( 5, 15);
+  constexpr Score IsolatedBase  = S( 3,  8);
+  constexpr Score Isolated      = S( 2,  7);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 
@@ -139,7 +140,8 @@ namespace {
         }
 
         else if (!neighbours)
-            score -=   Isolated      * bool(stoppers)
+            score -=   IsolatedBase
+                     + Isolated      * bool(stoppers)
                      + WeakUnopposed * !opposed;
 
         else if (backward)

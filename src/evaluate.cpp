585677ad384f0d062235e5b2861858a5e2ed7945
xoto10 <buylow001@gmail.com>
2019-12-17 17:19:17 +0000

    Pawnsonbothflanks uses files abc and fgh, 25*. Bench 5643098

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -719,7 +719,7 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
-                    + 21 * pawnsOnBothFlanks
+                    + 25 * pawnsOnBothFlanks
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
                     - 95 ;

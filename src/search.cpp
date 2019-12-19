7cbb50361aef7821ecef0cfbbed581b29d215531
xoto10 <buylow001@gmail.com>
2019-12-19 17:27:20 +0000

    Increase search width if often hitting draw eval without fail high/low, 32,48. Bench 5316876

--- src/search.cpp
+++ src/search.cpp
@@ -426,7 +426,7 @@ void Thread::search() {
           selDepth = 0;
 
           // quickDrawAverage can be used to approximate the running average of (iteration gives draw with no fails high/low)
-          quickDrawAverage =  (16 - 1) * quickDrawAverage / 16
+          quickDrawAverage =  (32 - 1) * quickDrawAverage / 32
                             + ttHitAverageResolution * (delta == initialDelta && abs(bestValue < 2));
 
           // Reset aspiration window starting size
@@ -992,7 +992,7 @@ moves_loop: // When in check, search starts from here
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
           moveCountPruning = moveCount >=  futility_move_count(improving, depth)
-                                         + (thisThread->quickDrawAverage >  88 * ttHitAverageResolution * 16
+                                         + (thisThread->quickDrawAverage >  48 * ttHitAverageResolution * 32
                                                                           / 1024);
 
           if (   !captureOrPromotion

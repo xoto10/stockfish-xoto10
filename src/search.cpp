ccabae9f11a1beddd7b48cb6d03ada8b198ab10d
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-02 06:13:41 +0100

    Additional condition for quiet futility pruning: hist+cmh0+cmh1+cmh3 < 30000. Bench: 5215200

--- src/search.cpp
+++ src/search.cpp
@@ -1007,7 +1007,7 @@ moves_loop: // When in check, search starts from here
                   &&  thisThread->mainHistory[us][from_to(move)]
                     + (*contHist[0])[movedPiece][to_sq(move)]
                     + (*contHist[1])[movedPiece][to_sq(move)]
-                    + (*contHist[3])[movedPiece][to_sq(move)] < 40000)
+                    + (*contHist[3])[movedPiece][to_sq(move)] < 30000)
                   continue;
 
               // Prune moves with negative SEE (~10 Elo)

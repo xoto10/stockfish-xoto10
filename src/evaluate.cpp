7bdcc93b35424457ce1c4b772bbe6657ef03191c
xoto10 <buylow001@gmail.com>
2020-01-03 17:36:05 +0000

    Decide who has initiative using rootMoves[0].score. Bench 4923098

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -168,7 +168,7 @@ namespace {
     template<Color Us> Score passed() const;
     template<Color Us> Score space() const;
     ScaleFactor scale_factor(Value eg) const;
-    Score initiative(Score score) const;
+    Score initiative() const;
 
     const Position& pos;
     Material::Entry* me;
@@ -697,10 +697,7 @@ namespace {
   // known attacking/defending status of the players.
 
   template<Tracing T>
-  Score Evaluation<T>::initiative(Score score) const {
-
-    Value mg = mg_value(score);
-    Value eg = eg_value(score);
+  Score Evaluation<T>::initiative() const {
 
     int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                      - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
@@ -724,8 +721,9 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
-    int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
+    Value rsc = pos.this_thread()->rootMoves[0].score;
+    int u = ((rsc > 0) - (rsc < 0)) * std::min(complexity + 50, 0);
+    int v = ((rsc > 0) - (rsc < 0)) * complexity;
 
     if (T)
         Trace::add(INITIATIVE, make_score(u, v));
@@ -807,7 +805,7 @@ namespace {
             + passed< WHITE>() - passed< BLACK>()
             + space<  WHITE>() - space<  BLACK>();
 
-    score += initiative(score);
+    score += initiative();
 
     // Interpolate between a middlegame and a (scaled by 'sf') endgame score
     ScaleFactor sf = scale_factor(eg_value(score));

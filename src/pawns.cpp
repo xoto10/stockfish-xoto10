4950a08d55c09d21733b1ef3bd8d98703cdaa0ba
xoto10 <buylow001@gmail.com>
2020-01-05 21:31:36 +0000

    Penalty for unconnected pawns, 2,8. Bench 4998772

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -36,7 +36,7 @@ namespace {
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
-  constexpr Score Unconnected   = S( 4, 12);
+  constexpr Score Unconnected   = S( 2,  8);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 

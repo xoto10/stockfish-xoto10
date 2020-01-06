fbe84a855e5bf0749ee7e7fb74a09d0edef47ebd
xoto10 <buylow001@gmail.com>
2020-01-06 17:56:59 +0000

    Give more piece preference to different threads, 240*. Bench 5180012

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -120,7 +120,7 @@ void MovePicker::score() {
                    + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   +80 * (type_of(pos.moved_piece(m)) == preferPieceType);
+                   + 240 * (type_of(pos.moved_piece(m)) == preferPieceType);
 
       else // Type == EVASIONS
       {

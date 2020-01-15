9f44e0659a584808e1cd0e3379670fa501f59b46
xoto10 <buylow001@gmail.com>
2020-01-15 20:47:53 +0000

    Use avg of bestvalue and min bestvalue across threads for tm. Bench 5156767

--- src/search.cpp
+++ src/search.cpp
@@ -547,7 +547,7 @@ void Thread::search() {
           }
           double bestMoveInstability = 1 + totBestMoveChanges / Threads.size();
 
-          double fallingEval = (332 +  6 * (mainThread->previousScore - minBestValue)
+          double fallingEval = (332 +  3 * (2 * mainThread->previousScore - minBestValue - bestValue)
                                     +  6 * (mainThread->iterValue[iterIdx] - minBestValue)) / 704.0;
           fallingEval = clamp(fallingEval, 0.5, 1.5);
 

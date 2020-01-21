323de29156a7c30eb2ff89e02628cef1dd0fa4e0
xoto10 <buylow001@gmail.com>
2020-01-21 13:44:49 +0000

    Supported pawn penalty in kd, 18*fpr. Bench 5427668

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -453,12 +453,12 @@ namespace {
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
-                 +  22 * frontPawnRank * frontPawnRank * frontPawnRank / 16
+                 +  18 * frontPawnRank
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 +   7;
+                 +  37;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)

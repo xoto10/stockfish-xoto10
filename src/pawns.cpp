a860f2f3c449adb9284a18a09abaea8aa64a9a75
xoto10 <buylow001@gmail.com>
2020-01-25 17:03:17 +0000

    Part doubled penalty even if supported, 8,18. Bench 5247523

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -34,7 +34,8 @@ namespace {
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
   constexpr Score BlockedStorm  = S(82, 82);
-  constexpr Score Doubled       = S(11, 56);
+  constexpr Score Doubled       = S( 3, 38);
+  constexpr Score DoubledSupp   = S( 8, 18);
   constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
@@ -149,6 +150,8 @@ namespace {
         if (!support)
             score -=   Doubled * doubled
                      + WeakLever * more_than_one(lever);
+
+        score -= DoubledSupp * doubled;
     }
 
     return score;

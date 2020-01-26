e4a533afa4ce956b31791368c531153afca07684
xoto10 <buylow001@gmail.com>
2020-01-26 13:02:45 +0000

    Some Doubled penalty even if supported, 9/33, 16/15. Bench 4998905

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -34,8 +34,8 @@ namespace {
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
   constexpr Score BlockedStorm  = S(82, 82);
-  constexpr Score Doubled       = S( 3, 28);
-  constexpr Score DoubledSupp   = S( 8, 28);
+  constexpr Score Doubled       = S( 9, 33);
+  constexpr Score DoubledSupp   = S(16, 15);
   constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);

6492a07d4d3e4a47a362051af07a627d8cb374fa
xoto10 <buylow001@gmail.com>
2020-01-28 13:51:05 +0000

    No futility pruning if pos has been in pv. Bench 5029950

--- src/search.cpp
+++ src/search.cpp
@@ -829,7 +829,7 @@ namespace {
               || (ss-4)->staticEval == VALUE_NONE) : ss->staticEval > (ss-2)->staticEval;
 
     // Step 8. Futility pruning: child node (~50 Elo)
-    if (   !PvNode
+    if (   !ttPv
         &&  depth < 6
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins

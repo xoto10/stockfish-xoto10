17b6ff8da18115beb1d5bed1258e32f2bf36e99f
xoto10 <buylow001@gmail.com>
2020-01-30 20:39:47 +0000

    Increase pawn score if large, >88/1,>54/2. Bench 4810342

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -789,10 +789,10 @@ namespace {
     pe = Pawns::probe(pos);
     score += pe->pawn_score(WHITE) - pe->pawn_score(BLACK);
     Value diff = eg_value(pe->pawn_score(WHITE)) - eg_value(pe->pawn_score(BLACK));
-    if (abs(diff) > 118)
-        score += make_score(0, (diff > 0 ? 1 : -1) * (abs(diff) - 101));
-    else if (abs(diff) > 84)
-        score += make_score(0, (diff > 0 ? 1 : -1) * (abs(diff) - 84) / 2);
+    if (abs(diff) > 88)
+        score += make_score(0, (diff > 0 ? 1 : -1) * (abs(diff) - 71));
+    else if (abs(diff) > 54)
+        score += make_score(0, (diff > 0 ? 1 : -1) * (abs(diff) - 54) / 2);
 //dbg_mean_of(abs(diff)); // mean 43, next 84, next 118
 
     // Early exit if score is high

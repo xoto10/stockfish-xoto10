2de14d42beb0a629fcbadc65f705904c9a66445e
xoto10 <buylow001@gmail.com>
2020-02-05 20:36:30 +0000

    Higher support bonus if pawn rank > 4, 21+2*. Bench 4550968

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -133,7 +133,7 @@ namespace {
         if (support | phalanx)
         {
             int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
-                   + (20 + 7 * (r > RANK_4)) * popcount(support);
+                   + (21 + 2 * (r > RANK_4)) * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
         }

9850efdd3166ca1ed982748955133979a68632e5
xoto10 <buylow001@gmail.com>
2020-02-05 15:05:06 +0000

    Higher Connected bonus if pawn passed 4th rank. Bench 4767914

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -135,7 +135,7 @@ namespace {
             int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
                    + 21 * popcount(support);
 
-            score += make_score(v, v * (r - 2) / 4);
+            score += make_score(v, v * (r + (r > RANK_4) - 2) / 4);
         }
 
         else if (!neighbours)

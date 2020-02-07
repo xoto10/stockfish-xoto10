4e1ac133c52852c163e3bf0b45f61f4f5cad0b9e
xoto10 <buylow001@gmail.com>
2020-02-07 21:42:45 +0000

    Only update prevScore if rootDepth < 10 or not a draw. Bench 5087478

--- src/search.cpp
+++ src/search.cpp
@@ -407,7 +407,7 @@ void Thread::search() {
       // Save the last iteration's scores before first PV line is searched and
       // all the move scores except the (new) PV are set to -VALUE_INFINITE.
       for (RootMove& rm : rootMoves)
-          if (abs(rm.score) > 1)
+          if (abs(rm.score) > 1 || rootDepth < 10)
               rm.previousScore = rm.score;
 
       size_t pvFirst = 0;

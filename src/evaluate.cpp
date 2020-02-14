e541797eb6f36910a8a7e7cdb898623912cc0bb3
xoto10 <buylow001@gmail.com>
2020-02-14 22:27:26 +0000

    RBQ adjust scale factor by pawns. No debug. Bench 5117498

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -23,7 +23,6 @@
 #include <cstring>   // For std::memset
 #include <iomanip>
 #include <sstream>
-#include <iostream>
 
 #include "bitboard.h"
 #include "evaluate.h"
@@ -168,7 +167,7 @@ namespace {
     template<Color Us> Score threats() const;
     template<Color Us> Score passed() const;
     template<Color Us> Score space() const;
-    ScaleFactor scale_factor(Value eg, Score& sc) const;
+    ScaleFactor scale_factor(Value eg) const;
     Score initiative(Score score) const;
 
     const Position& pos;
@@ -740,16 +739,10 @@ namespace {
   // Evaluation::scale_factor() computes the scale factor for the winning side
 
   template<Tracing T>
-  ScaleFactor Evaluation<T>::scale_factor(Value eg, Score& sc) const {
+  ScaleFactor Evaluation<T>::scale_factor(Value eg) const {
 
     Color strongSide = eg > VALUE_DRAW ? WHITE : BLACK;
     int sf = me->scale_factor(pos, strongSide);
-
-//if (   pos.non_pawn_material(strongSide) == QueenValueMg
-//    && pos.non_pawn_material(~strongSide) == RookValueMg + BishopValueMg)
-//{
-//    sync_cout << "info string qrb, sf = " << sf << " pos\n" << pos << sync_endl;
-//}
 
     // If scale is not already specific, scale down the endgame via general heuristics
     if (sf == SCALE_FACTOR_NORMAL)
@@ -760,22 +753,8 @@ namespace {
         else if (   pos.non_pawn_material(strongSide) == QueenValueMg
                  && pos.non_pawn_material(~strongSide) == RookValueMg + BishopValueMg)
         {
-// Other possible factors:
-// - defended 7th rank pawn for weak side
-            int rank7pawns = popcount(  pos.pieces(~strongSide, PAWN)
-                                      & (strongSide == WHITE ? Rank2BB : Rank7BB)
-                                      & attackedBy[~strongSide][ALL_PIECES]);
-            if (rank7pawns)
-            {
-                sc = -sc; // RB may be winning!
-                //sf = 96;
-            }
-            else
-            {
-                Bitboard b = pos.pieces(~strongSide, BISHOP);
-                sf += (pos.count<PAWN>(~strongSide) - pos.pawns_on_same_color_squares(~strongSide, pop_lsb(&b)) - 8);
-            }
-sync_cout << "info string qrb adj, sf = " << sf << " pos\n" << pos << sync_endl;
+            Bitboard b = pos.pieces(~strongSide, BISHOP);
+            sf += (pos.count<PAWN>(~strongSide) - pos.pawns_on_same_color_squares(~strongSide, pop_lsb(&b)) - 8);
         }
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
@@ -839,7 +818,7 @@ sync_cout << "info string qrb adj, sf = " << sf << " pos\n" << pos << sync_endl;
     score += initiative(score);
 
     // Interpolate between a middlegame and a (scaled by 'sf') endgame score
-    ScaleFactor sf = scale_factor(eg_value(score), score);
+    ScaleFactor sf = scale_factor(eg_value(score));
     v =  mg_value(score) * int(me->game_phase())
        + eg_value(score) * int(PHASE_MIDGAME - me->game_phase()) * sf / SCALE_FACTOR_NORMAL;
 

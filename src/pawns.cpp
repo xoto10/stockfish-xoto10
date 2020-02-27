af32fb83f5ee6728373ed99fac66ace945afaf95
xoto10 <buylow001@gmail.com>
2020-02-27 15:27:24 +0000

    Use B1/B8 as ksq for qs castle in do_king_safety(). Bench 4667400

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -234,7 +234,7 @@ Score Entry::do_king_safety(const Position& pos) {
       shelter = std::max(shelter, evaluate_shelter<Us>(pos, relative_square(Us, SQ_G1)), compare);
 
   if (pos.can_castle(Us & QUEEN_SIDE))
-      shelter = std::max(shelter, evaluate_shelter<Us>(pos, relative_square(Us, SQ_C1)), compare);
+      shelter = std::max(shelter, evaluate_shelter<Us>(pos, relative_square(Us, SQ_B1)), compare);
 
   // In endgame we like to bring our king near our closest pawn
   Bitboard pawns = pos.pieces(Us, PAWN);

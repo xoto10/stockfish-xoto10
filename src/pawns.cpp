d7630eaafc2d3718abbd15eb4b5fc52e91cba169
xoto10 <buylow001@gmail.com>
2020-03-20 12:31:10 +0000

    Bonus for outpost pawns, 8/8. Bench 4968475

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -36,7 +36,7 @@ namespace {
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
-  constexpr Score OutpostPawn   = S( 6,  6);
+  constexpr Score OutpostPawn   = S( 8,  8);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 

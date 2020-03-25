0c3bebd1127db3288a23c7c9111e244e1e8cac55
xoto10 <buylow001@gmail.com>
2020-03-25 05:54:56 +0000

    Lower sf for 4 rook endgames, 48. Bench 4934944

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -753,6 +753,8 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22 ;
+        else if (pos.non_pawn_material() == 4 * RookValueMg)
+            sf = 48;
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 

df5b1a9a7bcc3b3ceff1daf50f6fbde4047c0061
xoto10 <buylow001@gmail.com>
2020-03-25 08:59:38 +0000

    Reduce mg initiative if pawn structure is simple and open, mg-20. Bench 4899504

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -722,7 +722,6 @@ namespace {
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    - 20 * pe->simple_pawns()
                     -110 ;
 
     Value mg = mg_value(score);
@@ -731,7 +730,8 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
+    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0) - 20 * pe->simple_pawns(),
+                                             -abs(mg));
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     if (T)

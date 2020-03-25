45447a45beecda3100add7d997b7231d54a83f24
xoto10 <buylow001@gmail.com>
2020-03-25 17:23:34 +0000

    Variant of Rocky's idea, last 4 ranks as ActiveBishopRanks. Bench 4815290

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -256,6 +256,9 @@ namespace {
     constexpr Direction Down = -pawn_push(Us);
     constexpr Bitboard OutpostRanks = (Us == WHITE ? Rank4BB | Rank5BB | Rank6BB
                                                    : Rank5BB | Rank4BB | Rank3BB);
+    constexpr Bitboard ActiveBishopRanks = (Us == WHITE ? Rank5BB | Rank6BB | Rank7BB | Rank8BB
+                                                        : Rank4BB | Rank3BB | Rank2BB | Rank1BB);
+
     const Square* pl = pos.squares<Pt>(Us);
 
     Bitboard b, bb;
@@ -313,7 +316,7 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (!bool(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
+                                     * (!(b & ActiveBishopRanks) + popcount(blocked & CenterFiles));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))

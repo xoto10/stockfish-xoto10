02c5406b3d067ff88a6ce8d7b790f3a01b2a2d95
xoto10 <buylow001@gmail.com>
2020-03-27 12:44:27 +0000

    Higher start for delta if >=2 increases on previous iteration. Bench 4942891

--- src/search.cpp
+++ src/search.cpp
@@ -434,7 +434,7 @@ void Thread::search() {
           if (rootDepth >= 4)
           {
               Value previousScore = rootMoves[pvIdx].previousScore;
-              delta = Value(21 + abs(previousScore) / 256 + 10 * (delta > 21));
+              delta = Value(21 + abs(previousScore) / 256 + 10 * (delta > 31));
               alpha = std::max(previousScore - delta,-VALUE_INFINITE);
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 

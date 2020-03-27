6cac2e9788c215d3ab875830d8bd2bd7303d0e1b
xoto10 <buylow001@gmail.com>
2020-03-27 17:55:31 +0000

    Reduce sf for opposite bishops, 1*pawncount(strong). Bench 4706837

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -746,9 +746,9 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 14;
+            sf = 22;
         else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 1 : 7) * pos.count<PAWN>(strongSide));
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }

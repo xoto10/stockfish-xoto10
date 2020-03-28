94d96cdffe5660b284b582d4ff4da757c70e96a2
xoto10 <buylow001@gmail.com>
2020-03-28 23:18:00 +0000

    Eg penalty if our king closer to edge than theirs, 15. Bench 4642256

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -36,6 +36,7 @@ namespace {
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
+  constexpr Score KingCenter    = S( 0, 15);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 
@@ -246,6 +247,9 @@ Score Entry::do_king_safety(const Position& pos) {
       minPawnDist = 1;
   else while (pawns)
       minPawnDist = std::min(minPawnDist, distance(ksq, pop_lsb(&pawns)));
+
+  shelter -= KingCenter * (  edge_distance(file_of(ksq))
+                           < edge_distance(file_of(pos.square<KING>(~Us))));
 
   return shelter - make_score(0, 16 * minPawnDist);
 }

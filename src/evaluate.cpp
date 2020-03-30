ce646ba44f2af6d63e1e847d10018e8502545d86
xoto10 <buylow001@gmail.com>
2020-03-30 23:37:08 +0100

    Initiative penalty for ocb, 20/20. Bench 4919022

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -721,7 +721,7 @@ namespace {
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    - 50 * pos.opposite_bishops()
+                    - 20 * pos.opposite_bishops()
                     -110 ;
 
     Value mg = mg_value(score);
@@ -730,7 +730,8 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
+    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0) - 20 * pos.opposite_bishops(),
+                                             -abs(mg));
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     if (T)

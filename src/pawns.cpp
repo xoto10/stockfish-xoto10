0a40f73941abfb0a1d0c8a400063388a08d9a7d5
xoto10 <buylow001@gmail.com>
2020-03-31 18:53:35 +0100

    Use eg as well for unblockedstorm, mg/2. Bench 4909129

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -210,7 +210,7 @@ Score Entry::evaluate_shelter(const Position& pos, Square ksq) {
       if (ourRank && (ourRank == theirRank - 1))
           bonus -= BlockedStorm * int(theirRank == RANK_3);
       else
-          bonus -= make_score(UnblockedStorm[d][theirRank], UnblockedStorm[d][theirRank]);
+          bonus -= make_score(UnblockedStorm[d][theirRank], UnblockedStorm[d][theirRank] / 2);
   }
 
   return bonus;

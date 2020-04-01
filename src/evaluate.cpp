e35dfadb5aaeed4627dad610afa167a2ee498783
xoto10 <buylow001@gmail.com>
2020-04-01 19:53:13 +0100

    Increase TrappedRook if castling possible at root, 27/5,2*!. Bench 5138277

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -144,7 +144,7 @@ namespace {
   constexpr Score ThreatByKing        = S( 24, 89);
   constexpr Score ThreatByPawnPush    = S( 48, 39);
   constexpr Score ThreatBySafePawn    = S(173, 94);
-  constexpr Score TrappedRook         = S( 30,  6);
+  constexpr Score TrappedRook         = S( 27,  5);
   constexpr Score WeakQueen           = S( 49, 15);
   constexpr Score WeakQueenProtection = S( 14,  0);
 

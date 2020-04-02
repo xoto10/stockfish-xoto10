c2c40aa1a6518539db6d32558914118ca8eee6b3
xoto10 <buylow001@gmail.com>
2020-04-02 21:21:26 +0100

    Use npm for scale factor instead of pawns, *41/32768. Bench 5090464

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -748,8 +748,8 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22;
         else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 1 : 7) * pos.count<PAWN>(strongSide));
-
+            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 1 : 7) * pos.non_pawn_material(strongSide) * 41 / 32768);
+// 1700 - 6700
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
 

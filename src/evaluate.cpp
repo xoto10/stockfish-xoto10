af170f5d87b8f02c927bd30d1604173706ef8c3d
xoto10 <buylow001@gmail.com>
2020-04-03 14:10:56 +0100

    Lower sf if nb vs bb. Bench 5134905

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -752,6 +752,10 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22 ;
+        else if (   pos.count<BISHOP>(strongSide) == 1
+                 && pos.count<KNIGHT>(strongSide) == 1
+                 && pos.count<BISHOP>(~strongSide) == 2)
+            sf = 43;
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 

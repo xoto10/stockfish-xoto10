cab25c07a26564046f635658a13e7ed3de41814d
xoto10 <buylow001@gmail.com>
2020-04-04 00:31:09 +0100

    Small sf adjustment with npm, 39+, /1024. Bench 5205129

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -753,7 +753,8 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22 ;
         else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 39 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide)
+                                 - pos.non_pawn_material(strongSide) / 1024);
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }

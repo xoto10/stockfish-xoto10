dc47ee1999012ecacac7a13f01eea6841cee8965
xoto10 <buylow001@gmail.com>
2020-04-05 00:21:29 +0100

    Lower sf if no queens left and pawn count equal. Bench 4683320

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -747,7 +747,8 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22 ;
-        else if (!pos.count<QUEEN>())
+        else if (   !pos.count<QUEEN>()
+                 && pos.count<PAWN>(strongSide) == pos.count<PAWN>(~strongSide))
             sf = 44;
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));

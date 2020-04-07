bb52d8768339a42f2388006765c1dbc6d01176cf
xoto10 <buylow001@gmail.com>
2020-04-07 17:29:42 +0100

    Scale factor tweaks for ocb. Bench 4215513

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -761,13 +761,17 @@ namespace {
     // If scale is not already specific, scale down the endgame via general heuristics
     if (sf == SCALE_FACTOR_NORMAL)
     {
-        if (   pos.opposite_bishops()
-            && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 22;
+        if (pos.opposite_bishops())
+        {
+            if (pos.non_pawn_material() == 2 * BishopValueMg)
+                sf = 21;
+            else
+                sf = 32 + 2 * pos.count<PAWN>(strongSide);
+        }
         else
-            sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 33 + 6 * pos.count<PAWN>(strongSide));
 
-        sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
+        sf = std::max(0, sf - (pos.rule50_count() - 13) / 4);
     }
 
     return ScaleFactor(sf);

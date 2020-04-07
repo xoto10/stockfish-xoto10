a73d55be2cd24a5e59f98da76f1660eaf8d69a67
xoto10 <buylow001@gmail.com>
2020-04-07 15:21:13 +0100

    Kingdanger changes from tune. Bench 4767841

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -445,20 +445,20 @@ namespace {
     int kingFlankDefense = popcount(b3);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
-                 + 185 * popcount(kingRing[Us] & weak)
-                 + 148 * popcount(unsafeChecks)
-                 +  98 * popcount(pos.blockers_for_king(Us))
-                 +  69 * kingAttacksCount[Them]
+                 + 180 * popcount(kingRing[Us] & weak)
+                 + 146 * popcount(unsafeChecks)
+                 + 104 * popcount(pos.blockers_for_king(Us))
+                 +  74 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
-                 - 873 * !pos.count<QUEEN>(Them)
-                 - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
-                 -   6 * mg_value(score) / 8
+                 - 982 * !pos.count<QUEEN>(Them)
+                 -  84 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
+                 -   5 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 +  37;
+                 +  39;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
-    if (kingDanger > 100)
+    if (kingDanger > 107)
         score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
 
     // Penalty when our king is on a pawnless flank

8b840a00b6372faabc156760fdcb1106e395c9bc
xoto10 <buylow001@gmail.com>
2020-04-09 19:40:09 +0100

    Less Connected bonus if phalanx pawn has leverPusj and no support. Bench 4591433

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -134,7 +134,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed))
+            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) - (leverPush && !support))
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);

8d9603b7dfc99ac82c88bf79239fb4b9e55a3d2e
xoto10 <buylow001@gmail.com>
2020-04-09 19:53:29 +0100

    Less Connected bonus if phalanx pawn has 2 leverPush. Bench 4854176

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -134,7 +134,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) - bool(leverPush))
+            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) - (popcount(leverPush) > 1))
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);

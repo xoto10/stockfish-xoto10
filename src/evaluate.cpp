8f4d10ef37ee638fb755f2a9d2fa6ce2ebcb7ae6
xoto10 <buylow001@gmail.com>
2020-04-09 23:33:06 +0100

    Lower kingprotector for bishop, kp=10/11. Bench 4896420

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -131,7 +131,7 @@ namespace {
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
   constexpr Score Hanging             = S( 69, 36);
-  constexpr Score KingProtector       = S(  7,  8);
+  constexpr Score KingProtector       = S( 10, 11);
   constexpr Score KnightOnQueen       = S( 16, 12);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);

6ed2246d72038966a9a245a90259618e7b522e0e
xoto10 <buylow001@gmail.com>
2020-04-09 14:01:28 +0100

    Simplify backward pawn detection, with reduced Backward penalty, 8/22. Bench 4408749

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,7 +32,7 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
-  constexpr Score Backward      = S( 9, 24);
+  constexpr Score Backward      = S( 8, 22);
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);

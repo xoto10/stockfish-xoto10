bdcc8c5842e5b127c4c264e9d5457339c0c9c44e
xoto10 <buylow001@gmail.com>
2020-04-09 22:23:18 +0100

    Initiative penaltyfor unpushable pawns, >3,14/14. Bench 4922424

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -711,7 +711,7 @@ namespace {
 
     Value eg = eg_value(score);
 
-    int badPawns = std::max(0, (eg > 0) * pe->noMovePawns[WHITE] + (eg < 0) * pe->noMovePawns[BLACK] - 5);
+    int badPawns = std::max(0, (eg > 0) * pe->noMovePawns[WHITE] + (eg < 0) * pe->noMovePawns[BLACK] - 3);
 //if (badPawns)
 //sync_cout << "info string us " << (eg>0 ? "w" : "b") << " badp " << badPawns
 //          << " pos\n" << pos
@@ -724,7 +724,7 @@ namespace {
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
-                    - 21 * badPawns
+                    - 14 * badPawns
                     - 43 * almostUnwinnable
                     -110 ;
 
@@ -733,7 +733,7 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0) - 21 * badPawns, -abs(mg));
+    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0) - 14 * badPawns, -abs(mg));
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     if (T)

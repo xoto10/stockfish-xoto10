67ed66887bdb2a7f9e95c2553cd396e5366e11ee
xoto10 <buylow001@gmail.com>
2020-04-10 13:18:41 +0100

    Quadratics in material.cpp values from 2nd tune. Look bad. Bench 5213672

--- src/material.cpp
+++ src/material.cpp
@@ -33,23 +33,23 @@ namespace {
   constexpr int QuadraticOurs[][PIECE_TYPE_NB] = {
     //            OUR PIECES
     // pair pawn knight bishop rook queen
-    {1438                               }, // Bishop pair
-    {  40,   38                         }, // Pawn
-    {  32,  255, -62                    }, // Knight      OUR PIECES
-    {   0,  104,   4,    0              }, // Bishop
-    { -26,   -2,  47,   105,  -208      }, // Rook
-    {-189,   24, 117,   133,  -134, -6  }  // Queen
+    {1661                               }, // Bishop pair
+    {  37,   45                         }, // Pawn
+    {  31,  247,-124                    }, // Knight      OUR PIECES
+    {   5,  110,   4,    -2             }, // Bishop
+    {   0,   -4,  49,   109,  -416      }, // Rook
+    {-378,   19, 117,   140,  -268, -5  }  // Queen
   };
 
   constexpr int QuadraticTheirs[][PIECE_TYPE_NB] = {
     //           THEIR PIECES
     // pair pawn knight bishop rook queen
     {   0                               }, // Bishop pair
-    {  36,    0                         }, // Pawn
-    {   9,   63,   0                    }, // Knight      OUR PIECES
-    {  59,   65,  42,     0             }, // Bishop
-    {  46,   39,  24,   -24,    0       }, // Rook
-    {  97,  100, -42,   137,  268,    0 }  // Queen
+    {  39,    0                         }, // Pawn
+    {  11,   59,   0                    }, // Knight      OUR PIECES
+    {  59,   82,  38,     0             }, // Bishop
+    {  51,   40,  23,   -48,    0       }, // Rook
+    { 105,  107,   0,   136,  239,    0 }  // Queen
   };
 
   // Endgame evaluation and scaling functions are accessed directly and not through

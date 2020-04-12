537e36dc79eac8be92de10c282267de7429dc794
xoto10 <buylow001@gmail.com>
2020-04-13 00:46:17 +0100

    AlmostUnwinnable, outflanking < -2. Bench 4782548

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -718,7 +718,7 @@ namespace {
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
-    bool almostUnwinnable =   outflanking < 0
+    bool almostUnwinnable =   outflanking < -2
                            && !pawnsOnBothFlanks;
 
     bool infiltration = rank_of(pos.square<KING>(WHITE)) > RANK_4

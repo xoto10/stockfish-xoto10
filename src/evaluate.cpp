ed75fc49c6bfa3bfc2478a17152e3ff8af50558c
xoto10 <buylow001@gmail.com>
2020-04-15 21:16:42 +0100

    Combo; increase some UnblockedStorm values and initiative constant. Bench 4766401

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -732,7 +732,7 @@ namespace {
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    -110 ;
+                    -118 ;
 
     Value mg = mg_value(score);
     Value eg = eg_value(score);

852ddb05a8c5f321f7f1a305b9a97dae5751a1e9
xoto10 <buylow001@gmail.com>
2020-04-16 12:57:25 +0100

    OCB sf, 33 + 4 * count<N+B> (without dbg_mean). Bench 5143062

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -771,7 +771,7 @@ namespace {
         }
         else
             sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide));
-dbg_mean_of(sf);
+
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
 

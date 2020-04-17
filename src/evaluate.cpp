988e8c42221db103378de317e57d9849f8af72e8
xoto10 <buylow001@gmail.com>
2020-04-17 04:02:51 +0100

    Bonus for their pieces on same color sq as our bishop, 3/3. Bench 4759953

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -128,7 +128,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns         = S(  3,  7);
-  constexpr Score BishopSquares       = S(  3,  7);
+  constexpr Score BishopSquares       = S(  3,  3);
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
   constexpr Score Hanging             = S( 69, 36);

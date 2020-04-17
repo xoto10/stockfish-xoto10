207b37af10f55701edeef7d00f423b50b8801349
xoto10 <buylow001@gmail.com>
2020-04-17 17:38:45 +0100

    Symmetrical position more drawish, -5. Bench 4896086

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -772,8 +772,8 @@ namespace {
         else
         {
             sf =  std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide))
-                - 10 * (   pos.non_pawn_material(WHITE) == pos.non_pawn_material(BLACK)
-                        && pe->passed_count() == 0);
+                - 5 * (   pos.non_pawn_material(WHITE) == pos.non_pawn_material(BLACK)
+                       && pe->passed_count() == 0);
         }
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);

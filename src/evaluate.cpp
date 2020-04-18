2fbf48eb6eb2a3f842915c63667b979bedfe9fad
xoto10 <buylow001@gmail.com>
2020-04-18 20:01:40 +0100

    Low material is more drawish, <2r, -4. Bench 4619194

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -770,7 +770,12 @@ namespace {
                 sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
         else
+        {
             sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide));
+
+            if (pos.non_pawn_material(strongSide) < 2 * RookValueMg)
+                sf -= 4;
+        }
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }

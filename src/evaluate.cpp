1515bac315af9079177fbc4609157eb18b9c97d2
xoto10 <buylow001@gmail.com>
2020-04-18 19:24:57 +0100

    Higher sf if we are a piece up or more, +8. Bench 4689037

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -771,7 +771,7 @@ namespace {
         }
         else
             sf =  std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide))
-                + 2 * (pos.non_pawn_material(strongSide) - pos.non_pawn_material(~strongSide)
+                + 8 * (pos.non_pawn_material(strongSide) - pos.non_pawn_material(~strongSide)
                           >= KnightValueMg);
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);

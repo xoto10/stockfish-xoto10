dcea3728596899f3dd1d767abf24f764c7368f88
xoto10 <buylow001@gmail.com>
2020-04-18 21:27:20 +0100

    sf not ocb, 36 + 3*all. Bench 4488852

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -770,12 +770,7 @@ namespace {
                 sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
         else
-        {
-            sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide));
-
-            if (pos.non_pawn_material(strongSide) <= 2 * RookValueMg)
-                sf -= 4;
-        }
+            sf = std::min(sf, 36 + 3 * pos.count<ALL_PIECES>(strongSide));
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }

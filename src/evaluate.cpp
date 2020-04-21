737d675494d4754402577c62c7f75421ea879f71
xoto10 <buylow001@gmail.com>
2020-04-21 12:13:46 +0100

    Bonus if king has moved and no trapped rook, 28/28, 39/9. Bench 5004575

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -132,7 +132,7 @@ namespace {
   constexpr Score FlankAttacks        = S(  8,  0);
   constexpr Score Hanging             = S( 69, 36);
   constexpr Score KingProtector       = S(  7,  8);
-  constexpr Score KingRook            = S( 20, 20);
+  constexpr Score KingRook            = S( 28, 28);
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);

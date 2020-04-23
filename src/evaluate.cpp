656a70d469d3cde28cd37198b915e713b822bec7
xoto10 <buylow001@gmail.com>
2020-04-23 14:01:52 +0100

    LDB vs base, test7, 0/5/25/10. Bench 4808873

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -138,7 +138,7 @@ namespace {
   volatile  Score LongDiagonalBishop  = S(  0,  0);
   volatile  Score LongDiagonalBishopR = S(  5,  0);
   volatile  Score LongDiagonalBishopK = S( 25,  0);
-  volatile  Score LongDiagonalBishopP = S( 18,  0);
+  volatile  Score LongDiagonalBishopP = S( 10,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);
   constexpr Score Outpost             = S( 30, 21);
   constexpr Score PassedFile          = S( 11,  8);

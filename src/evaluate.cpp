90b61377dcaab562ba16fe6194e1c215ed64a206
xoto10 <buylow001@gmail.com>
2020-04-26 22:54:46 +0100

    MinorBehindPawn, 20/1. Bench 4442900

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -134,7 +134,7 @@ namespace {
   constexpr Score KingProtector       = S(  7,  8);
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
-  constexpr Score MinorBehindPawn     = S( 18,  3);
+  constexpr Score MinorBehindPawn     = S( 20,  1);
   constexpr Score Outpost             = S( 30, 21);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);

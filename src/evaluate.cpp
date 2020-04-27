c80f85d2ddd72166e8f8833a8d1fa854e5836c66
xoto10 <buylow001@gmail.com>
2020-04-27 19:50:12 +0100

    closedpos: Eg initiative bonus for pawn attacks towards their king, 16,pawnentry. Bench 4634083

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -738,7 +738,7 @@ namespace {
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
-                    + 20 * pawnAttack
+                    + 16 * pawnAttack
                     - 43 * almostUnwinnable
                     -110 ;
 

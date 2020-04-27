e0eb397d5eb77d79d0af7f0b22ad8d21ff7a2abc
xoto10 <buylow001@gmail.com>
2020-04-27 06:00:39 +0100

    Pawns on same color, 5/12. Bench 4775813

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -127,7 +127,7 @@ namespace {
   };
 
   // Assorted bonuses and penalties
-  constexpr Score BishopPawns         = S(  3, 10);
+  constexpr Score BishopPawns         = S(  5, 12);
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
   constexpr Score Hanging             = S( 69, 36);

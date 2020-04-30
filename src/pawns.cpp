5091108ef3f730c5bb8c22b2d41f8611bf6d88b9
xoto10 <buylow001@gmail.com>
2020-04-30 17:39:50 +0100

    King shelter bonus if >1 blocked files in king flank, 12/12. Bench 4887426

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
-  constexpr Score BlockedKFlank = S(20, 20);
+  constexpr Score BlockedKFlank = S(12, 12);
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);

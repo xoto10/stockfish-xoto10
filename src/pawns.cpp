ce505e1c20e97f5b3f76f0de517f33eaf2b85936
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-02 16:59:22 +0200

    Give 25% more penalty for unopposed doubled pawns. Bench: 4618042

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -152,7 +152,7 @@ namespace {
                      + WeakUnopposed * !opposed;
 
         if (!support)
-            score -=   Doubled * doubled * (2 + !opposed) / 2
+            score -=   Doubled * doubled * (4 + !opposed) / 4
                      + WeakLever * more_than_one(lever);
     }
 

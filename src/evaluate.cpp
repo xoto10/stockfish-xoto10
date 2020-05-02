f255415761655456502707b12a0f3f9d5e8d6564
xoto10 <buylow001@gmail.com>
2020-05-02 12:35:40 +0100

    Lower sf with blocked_count(), 37-bc. Bench 4015919

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -770,7 +770,7 @@ namespace {
                 sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
         else
-            sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 37 + 7 * pos.count<PAWN>(strongSide) - pe->blocked_count());
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }

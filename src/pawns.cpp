34c3b9ae5e14d67c94afd13d98fc0527fea6e293
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-02 17:05:55 +0200

    25% more penalty for isolated doubled pawns. Bench: 4591488

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -152,7 +152,7 @@ namespace {
                      + WeakUnopposed * !opposed;
 
         if (!support)
-            score -=   Doubled * doubled * (2 + !neighbours) / 2
+            score -=   Doubled * doubled * (4 + !neighbours) / 4
                      + WeakLever * more_than_one(lever);
     }
 

514762888af1f3ad38f13219546eb85a3c5a4169
xoto10 <buylow001@gmail.com>
2020-05-04 19:08:04 +0100

    Zero stat_bonus above depth 15. Bench 4247490

--- src/search.cpp
+++ src/search.cpp
@@ -84,7 +84,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return d > 15 ? -700 : 19 * d * d + 155 * d - 132;
+    return d > 15 ? 0 : 19 * d * d + 155 * d - 132;
   }
 
   // Add a small random component to draw evaluations to avoid 3fold-blindness

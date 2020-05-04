3e134bed6433abfb1bd5554c3a16d0e8ec4fa90e
xoto10 <buylow001@gmail.com>
2020-05-05 00:09:27 +0100

    Larger quiet penalty for continuation history. Bench 4143246

--- src/search.cpp
+++ src/search.cpp
@@ -1690,7 +1690,7 @@ moves_loop: // When in check, search starts from here
         for (int i = 0; i < quietCount; ++i)
         {
             thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2;
-            update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2 / 2);
+            update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2 * 3 / 2);
         }
     }
     else

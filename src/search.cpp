25065a8e60e8de44a1cf395dd17794cce1c34076
xoto10 <buylow001@gmail.com>
2020-05-04 23:45:53 +0100

    Smaller quiet penalty for mainHist. Bench 4693556

--- src/search.cpp
+++ src/search.cpp
@@ -1689,7 +1689,7 @@ moves_loop: // When in check, search starts from here
         // Decrease all the non-best quiet moves
         for (int i = 0; i < quietCount; ++i)
         {
-            thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2;
+            thisThread->mainHistory[us][from_to(quietsSearched[i])] << -bonus2 / 2;
             update_continuation_histories(ss, pos.moved_piece(quietsSearched[i]), to_sq(quietsSearched[i]), -bonus2);
         }
     }

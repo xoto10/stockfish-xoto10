6ee8ca6668c453c3f42927eeac14bbe02283cfd9
xoto10 <buylow001@gmail.com>
2020-05-05 03:09:37 +0100

    Penalty for bishop xraying their pawns, pseudo,4/3. Bench 4706520

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -320,7 +320,8 @@ namespace {
                                      * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
 
                 // Penalty for all pawns x-rayed
-                score -= BishopXRayPawns * popcount(attacks_bb<BISHOP>(s, 0) & pos.pieces(Them, PAWN));
+                  score -= BishopXRayPawns * popcount(attacks_bb<BISHOP>(s, 0) & pos.pieces(Them, PAWN));
+//                score -= BishopXRayPawns * popcount(PseudoAttacks[BISHOP][s] & pos.pieces(Them, PAWN));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))

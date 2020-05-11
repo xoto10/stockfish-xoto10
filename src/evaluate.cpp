2d593573bc046c268a112b74b7c24042563961ca
xoto10 <buylow001@gmail.com>
2020-05-11 15:31:39 +0100

    Extra bonus if weak piece holding a defender is on rank 7, 24/0. Bench 4678611

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -149,7 +149,7 @@ namespace {
   constexpr Score ThreatByPawnPush    = S( 48, 39);
   constexpr Score ThreatBySafePawn    = S(173, 94);
   constexpr Score TrappedRook         = S( 55, 13);
-  constexpr Score WeakOnRank7         = S( 16,  0);
+  constexpr Score WeakOnRank7         = S( 24,  0);
   constexpr Score WeakQueen           = S( 51, 14);
   constexpr Score WeakQueenProtection = S( 15,  0);
 

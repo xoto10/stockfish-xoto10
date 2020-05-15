a65d7f53336b7969fcf1e319fa93595a9f97b16c
xoto10 <buylow001@gmail.com>
2020-05-15 04:28:13 +0100

    opt_scale between 1/40 and 1/5 of current time. Bench 4395562

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -80,8 +80,9 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
   // game time for the current move, so also cap to 20% of available game time.
   if (limits.movestogo == 0)
   {
-      opt_scale = std::min(0.007 + std::pow(ply + 3.0, 0.5) / 250.0,
-                           0.2 * limits.time[us] / double(timeLeft));
+      opt_scale = Utility::clamp(0.007 + std::pow(ply + 3.0, 0.5) / 250.0,
+                                 0.025 * limits.time[us] / double(timeLeft),
+                                 0.2 * limits.time[us] / double(timeLeft));
       max_scale = 4 + std::pow(ply + 3, 0.3);
   }
 

43082180f53dfad4982626a0c125606cc1513bc9
xoto10 <buylow001@gmail.com>
2020-05-25 16:05:14 +0100

    Lower draw value (our pov) if they take 3-fold. Bench 4984236

--- src/search.cpp
+++ src/search.cpp
@@ -1442,7 +1442,7 @@ moves_loop: // When in check, search starts from here
     // Check for an immediate draw or maximum ply reached
     if (   pos.is_draw(ss->ply)
         || ss->ply >= MAX_PLY)
-        return (ss->ply >= MAX_PLY && !ss->inCheck) ? evaluate(pos) : VALUE_DRAW;
+        return (ss->ply >= MAX_PLY && !ss->inCheck) ? evaluate(pos) : VALUE_DRAW - 2;
 
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
 

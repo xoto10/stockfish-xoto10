6f6c63c27625f2426c5538876a1bd6f72a74a83e
xoto10 <buylow001@gmail.com>
2020-05-26 10:06:25 +0100

    Combo of BishopPawns and tm changes, 2/8, /280. Bench 4781038

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -127,7 +127,7 @@ namespace {
   };
 
   // Assorted bonuses and penalties
-  constexpr Score BishopPawns         = S(  3,  7);
+  constexpr Score BishopPawns         = S(  2,  8);
   constexpr Score BishopXRayPawns     = S(  4,  5);
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);

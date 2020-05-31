cdcf66dfd4ca4878eef1a2784fce817d212fb6a5
xoto10 <buylow001@gmail.com>
2020-05-31 13:27:48 +0100

    Simplify away nmp verification search. Bench 4741233

--- src/search.cpp
+++ src/search.cpp
@@ -880,20 +880,6 @@ namespace {
 
             if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 13))
                 return nullValue;
-
-            assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
-
-            // Do verification search at high depths, with null move pruning disabled
-            // for us, until ply exceeds nmpMinPly.
-            thisThread->nmpMinPly = ss->ply + 3 * (depth-R) / 4;
-            thisThread->nmpColor = us;
-
-            Value v = search<NonPV>(pos, ss, beta-1, beta, depth-R, false);
-
-            thisThread->nmpMinPly = 0;
-
-            if (v >= beta)
-                return nullValue;
         }
     }
 

9751df8fb9810003ada006ab8d0884a1f01b77bf
xoto10 <buylow001@gmail.com>
2020-06-02 15:27:01 +0100

    Higher sf for >1 strongSide pawns in other half w/ >25 pcs left, +4. Bench 4678253

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -787,7 +787,7 @@ namespace {
             Bitboard theirHalf = (strongSide == WHITE ? Rank5BB | Rank6BB | Rank7BB | Rank8BB
                                                       : Rank1BB | Rank2BB | Rank3BB | Rank4BB);
             sf =  std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide))
-                + (bool(pos.pieces(strongSide, PAWN) & theirHalf) && pos.count<ALL_PIECES>() > 25);
+                + 4 * (pos.count<ALL_PIECES>() > 25 && bool(pos.pieces(strongSide, PAWN) & theirHalf));
         }
     }
 

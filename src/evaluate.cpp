d511928cda41cfd0692d54fc74a2fa9e3a12c63e
xoto10 <buylow001@gmail.com>
2020-06-13 21:28:24 +0100

    Lower general sf if !pobf, 8*. Bench 4872736

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -774,7 +774,7 @@ namespace {
                 sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);
         }
         else
-            sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide)) - 8 * !pawnsOnBothFlanks;
     }
 
     // Interpolate between the middlegame and (scaled by 'sf') endgame score

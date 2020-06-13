863e87b1f6888b0c3fdc363eecba462cb2d4848a
xoto10 <buylow001@gmail.com>
2020-06-13 23:43:53 +0100

    Less futility pruning if alpha == -1, 80*. Bench 4583567

--- src/search.cpp
+++ src/search.cpp
@@ -812,7 +812,7 @@ namespace {
     // Step 8. Futility pruning: child node (~50 Elo)
     if (   !PvNode
         &&  depth < 6
-        &&  eval >= beta + futility_margin(depth, improving) + 40 * (alpha == -1)
+        &&  eval >= beta + futility_margin(depth, improving) + 80 * (alpha == -1)
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
 

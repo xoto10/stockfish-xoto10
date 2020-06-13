336a7738940b2d8258f0bd11fab67afd3a2c6c85
xoto10 <buylow001@gmail.com>
2020-06-13 19:12:31 +0100

    Less futility pruning if losing, 30*(beta<-100). Bench 4930755

--- src/search.cpp
+++ src/search.cpp
@@ -812,7 +812,7 @@ namespace {
     // Step 8. Futility pruning: child node (~50 Elo)
     if (   !PvNode
         &&  depth < 6
-        &&  eval >= beta + futility_margin(depth, improving) + 40 * (beta < -100)
+        &&  eval >= beta + futility_margin(depth, improving) + 30 * (beta < -100)
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
 

094ee58dbb0a03c06fde1123ee77c1c23d18a623
xoto10 <buylow001@gmail.com>
2020-06-14 12:12:46 +0100

    Higher sf if pure OCB and passers on both flanks, +12. Bench 4646993

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -773,7 +773,7 @@ namespace {
                 bool passersOnBothFlanks =   (pe->passed_pawns(strongSide) & QueenSide)
                                           && (pe->passed_pawns(strongSide) & KingSide);
 
-                sf = 18 + 4 * popcount(pe->passed_pawns(strongSide)) + 16 * passersOnBothFlanks;
+                sf = 18 + 4 * popcount(pe->passed_pawns(strongSide)) + 12 * passersOnBothFlanks;
             }
             else
                 sf = 22 + 3 * pos.count<ALL_PIECES>(strongSide);

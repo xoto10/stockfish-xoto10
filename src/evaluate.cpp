9bf94ec38914a5a17cdbbec8337683810f0d7d5d
xoto10 <buylow001@gmail.com>
2020-06-17 15:49:44 +0100

    Extra BXP penalty if >1 xray on our pawns. Bench 4820638

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -327,7 +327,8 @@ namespace {
                                      * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
 
                 // Penalty for all enemy pawns x-rayed
-                score -= BishopXRayPawns * popcount(attacks_bb<BISHOP>(s) & pos.pieces(Them, PAWN));
+                score -= BishopXRayPawns * (  popcount(attacks_bb<BISHOP>(s) & pos.pieces(Them, PAWN))
+                                            + more_than_one(attacks_bb<BISHOP>(s) & pos.pieces(Us, PAWN)));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))

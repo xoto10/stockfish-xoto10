37f7460c0cbd40da72a3f40fd4e54c12d2da5668
xoto10 <buylow001@gmail.com>
2020-07-06 13:13:53 +0100

    Higher kaw if kac>1 but kaw==0, 100. Bench 4560602

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -471,7 +471,7 @@ namespace {
 
     kingDanger +=        kingAttackersCount[Them]
                          * (kingAttackersCount[Them] > 1 && kingAttackersWeight[Them] == 0
-                            ? 50 : kingAttackersWeight[Them])
+                            ? 100 : kingAttackersWeight[Them])
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))

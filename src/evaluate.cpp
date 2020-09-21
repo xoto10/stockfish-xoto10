98fffe1403b3d6fe871186b8fbdc478f8f481ecf
xoto10 <buylow001@gmail.com>
2020-09-21 13:39:26 +0100

    Add tempo when using classical eval (bugfix). Bench 4174320

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1031,12 +1031,13 @@ Value Eval::evaluate(const Position& pos) {
       bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
 
       v = classical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
-      v += Tempo;
 
       // if the classical eval is small and imbalance large, use NNUE nevertheless.
       if (   largePsq
           && abs(v) * 16 < NNUEThreshold2 * r50)
           v = adjusted_NNUE();
+
+      v += Tempo;
   }
 
   // Damp down the evaluation linearly when shuffling

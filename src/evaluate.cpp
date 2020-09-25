2bc58cff719181d457e1c4299335c79ed814c2cc
xoto10 <buylow001@gmail.com>
2020-09-25 20:05:01 +0100

    Don't switch to classical if queen imbalance. Bench 4268727

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1028,7 +1028,8 @@ Value Eval::evaluate(const Position& pos) {
       Value psq = Value(abs(eg_value(pos.psq_score())));
       int   r50 = 16 + pos.rule50_count();
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
-      bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
+      bool  classical =   (largePsq && pos.count<QUEEN>() != 1)
+                       || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
 
       v = classical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
 

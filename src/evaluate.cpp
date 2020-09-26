bb5b67636c94ad893a9afc683e34c874d6084cf4
xoto10 <buylow001@gmail.com>
2020-09-26 10:41:23 +0100

    Use nnue if classical gave small eval and major piece imbalance. Bench 3727309

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1037,7 +1037,7 @@ Value Eval::evaluate(const Position& pos) {
       // small probability if the classical eval is less than the threshold.
       if (   largePsq
           && (abs(v) * 16 < NNUEThreshold2 * r50
-          || (   (pos.opposite_bishops() || pos.count<QUEEN>() == 1)
+          || (   (pos.opposite_bishops() || pos.count<QUEEN>() == 1 || pos.count<ROOK>() == 1 || pos.count<ROOK>() == 3)
               && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
               && !(pos.this_thread()->nodes & 0xB))))
           v = adjusted_NNUE();

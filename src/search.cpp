1059aaed1655e862254b33209df52292cc18b1fc
xoto10 <buylow001@gmail.com>
2020-09-26 10:00:25 +0100

    Use more time if eval falling over multiple moves, 8*. Bench 3865413

--- src/search.cpp
+++ src/search.cpp
@@ -504,7 +504,7 @@ void Thread::search() {
           && !mainThread->stopOnPonderhit)
       {
           double fallingEval = (318 + 6 * (mainThread->bestPreviousScore - bestValue)
-                                    + 6 * (mainThread->iterValue[iterIdx] - bestValue)) / 825.0;
+                                    + 8 * (mainThread->iterValue[iterIdx] - bestValue)) / 825.0;
           fallingEval = std::clamp(fallingEval, 0.5, 1.5);
 
           // If the bestMove is stable over several iterations, reduce time accordingly

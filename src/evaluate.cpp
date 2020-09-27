273555f05f7ac7ea4d0069d770e610f6ee7b1098
xoto10 <buylow001@gmail.com>
2020-09-27 03:38:34 +0100

    Mod snicolet's mega-patch, /64. Bench 4098612

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1024,7 +1024,7 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
          int mat = pos.non_pawn_material();
-         return NNUE::evaluate(pos) * (1024 + mat / 16) / 1024 + Tempo;
+         return NNUE::evaluate(pos) * (1024 + mat / 64) / 1024 + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small

ae63dceb9bddf156a18842c62b4165a6ba0cafbc
xoto10 <buylow001@gmail.com>
2020-09-28 17:15:29 +0100

    Divide by power of 2 when damping with rule50count. Bench 3660830

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1047,7 +1047,7 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (100 - pos.rule50_count()) / 100;
+  v = v * (100 - pos.rule50_count()) * 655 / 65536;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);

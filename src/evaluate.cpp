624ecf08945e5a38510d9d59d576eb02242ba33c
xoto10 <buylow001@gmail.com>
2020-09-30 21:33:34 +0100

    Bonus on nnue eval if material imbalance, +/-16. Bench 3836001

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1023,8 +1023,13 @@ Value Eval::evaluate(const Position& pos) {
   {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
-         int mat = pos.non_pawn_material() + PieceValue[MG][PAWN] * pos.count<PAWN>();
-         return NNUE::evaluate(pos) * (720 + mat / 32) / 1024 + Tempo;
+         Value nnEv = NNUE::evaluate(pos);
+         int   matB = pos.non_pawn_material(BLACK);
+         int   matW = pos.non_pawn_material(WHITE);
+
+         return  nnEv * (720 + (matB + matW) / 32) / 1024
+               + 16 * (matB != matW) * ((nnEv > 0) - (nnEv < 0))
+               + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small

001b38d3d95e612663c699ef624b61fbff6e4ef6
xoto10 <buylow001@gmail.com>
2020-09-30 05:30:28 +0100

    Include infiltration in nnue eval, +24. Bench 3976562

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1023,8 +1023,16 @@ Value Eval::evaluate(const Position& pos) {
   {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
+         Value nnEv = NNUE::evaluate(pos);
+
          int mat = pos.non_pawn_material() + PieceValue[MG][PAWN] * pos.count<PAWN>();
-         return NNUE::evaluate(pos) * (720 + mat / 32) / 1024 + Tempo;
+
+         bool infiltration =   rank_of(pos.square<KING>(WHITE)) > RANK_4
+                            || rank_of(pos.square<KING>(BLACK)) < RANK_5;
+
+         return  nnEv * (720 + mat / 32) / 1024
+               + 24 * infiltration * ((nnEv > 0) - (nnEv < 0))
+               + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small

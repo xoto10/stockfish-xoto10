a9dd2044af6ea86959212c768f92b8248d5dc0d9
xoto10 <buylow001@gmail.com>
2020-10-02 23:16:54 +0100

    Use more time if our 2nd move is unstable, 190,6. Bench 3776081

--- src/search.cpp
+++ src/search.cpp
@@ -519,7 +519,7 @@ void Thread::search() {
               totBestMove2Changes += th->bestMove2Changes;
               th->bestMoveChanges = th->bestMove2Changes = 0;
           }
-          double bestMoveInstability =  1 + (190 * totBestMoveChanges + 8 * totBestMove2Changes)
+          double bestMoveInstability =  1 + (190 * totBestMoveChanges + 6 * totBestMove2Changes)
                                             / (100 * Threads.size());
           double totalTime = rootMoves.size() == 1 ? 0 :
                              Time.optimum() * fallingEval * reduction * bestMoveInstability;

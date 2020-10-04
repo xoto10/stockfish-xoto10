401d7a3b77e3f9e5feb265b9b1dc1f3ba0d49e68
xoto10 <buylow001@gmail.com>
2020-10-04 20:49:26 +0100

    Use classical on every 8th thread. Bench 3776081

--- src/thread.h
+++ src/thread.h
@@ -54,6 +54,7 @@ public:
   void idle_loop();
   void start_searching();
   void wait_for_search_finished();
+  int index() { return idx; }
 
   Pawns::Table pawnsTable;
   Material::Table materialTable;

74bd699430f217d9f93ffa37e3c3fcd281102589
xoto10 <buylow001@gmail.com>
2020-10-06 11:22:48 +0100

    Increase time used if ponder is unstable, 200/10*min(5). Bench 4016121

--- src/search.cpp
+++ src/search.cpp
@@ -519,8 +519,8 @@ void Thread::search() {
               totBestMove2Changes += th->bestMove2Changes;
               th->bestMoveChanges = th->bestMove2Changes = 0;
           }
-          double bestMoveInstability =  1.0 + (  175.0 * totBestMoveChanges
-                                               +  20.0 * std::min(5, totBestMove2Changes))
+          double bestMoveInstability =  1.0 + (  200.0 * totBestMoveChanges
+                                               +  10.0 * std::min(5, totBestMove2Changes))
                                             / (100.0 * Threads.size());
           double totalTime = rootMoves.size() == 1 ? 0 :
                              Time.optimum() * fallingEval * reduction * bestMoveInstability;

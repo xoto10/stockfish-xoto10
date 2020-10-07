198730c7f08debd1f7201ef283d733dbf79d5deb
xoto10 <buylow001@gmail.com>
2020-10-07 14:02:23 +0100

    Only do reduced depth search if <= depth. Bench 3585598

--- src/search.cpp
+++ src/search.cpp
@@ -1218,7 +1218,7 @@ moves_loop: // When in check, search starts from here
 
           Depth d = std::clamp(newDepth - r, 1, newDepth);
 
-          if (d < depth - 1)
+          if (d <= depth)
           {
               value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 

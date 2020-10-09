4ab36f1878afc44750f2b5f2c07e0ea5c9d603d6
xoto10 <buylow001@gmail.com>
2020-10-09 19:46:26 +0100

    nnue: exaggerate mat bonus close to eval 0, <40,*2. Bench 3822944

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1025,7 +1025,7 @@ Value Eval::evaluate(const Position& pos) {
       auto  adjusted_NNUE = [&](){
          int mat = pos.non_pawn_material() + PieceValue[MG][PAWN] * pos.count<PAWN>();
          Value nnEv = NNUE::evaluate(pos);
-         return  (nnEv * 720 + (nnEv > 20 ? nnEv - 20 : nnEv < -20 ? nnEv + 20 : 0) * (mat / 32))
+         return  (nnEv * 720 + (nnEv > 40 ? nnEv + 8 : nnEv < -40 ? nnEv - 8 : nnEv * 2) * (mat / 32))
                  / 1024 + Tempo;
       };
 

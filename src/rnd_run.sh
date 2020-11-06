71b1f08f78edceeb261fb5f3893373d6261aae14
xoto10 <buylow001@gmail.com>
2020-11-06 18:07:10 +0000

    add rnd_run.sh

---
+++ src/rnd_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# rnd_run.sh - add more randomised net args and run (mix of old and new nets)
+
+
+# No jobs running allows wait to be used
+if [[ "$(jobs | wc -l)" != 0 ]] ; then
+   echo "There are already jobs running in this shell"
+   exit 1
+fi
+
+
+python3 rndadj.py
+
+out="none"
+read -p "run cutechess tests? " ans
+case "$ans" in
+   [Yy])
+      out=$(. ./net_run.sh 2>net_run.err)
+      ;;
+   *)
+      exit 2
+esac
+echo "tests started, out=<$out>"
+
+# Wait for tests to finish and then update db
+
+wait
+
+# use $out ...
+#python3 results.py
+
+

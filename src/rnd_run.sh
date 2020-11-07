91c728967b6453ac9f78793ed65de37e2842b594
xoto10 <xoto10@some.place.com>
2020-11-07 20:03:21 +0000

    infinite loop, use 3 threads

--- src/rnd_run.sh
+++ src/rnd_run.sh
@@ -3,31 +3,35 @@
 # rnd_run.sh - add more randomised net args and run (mix of old and new nets)
 
 
-# No jobs running allows wait to be used
-if [[ "$(jobs | wc -l)" != 0 ]] ; then
-   echo "There are already jobs running in this shell"
-   exit 1
-fi
+for i in 1 2 3
+do
+
+   # No jobs running allows wait to be used
+   if [[ "$(jobs | wc -l)" != 0 ]] ; then
+      echo "There are already jobs running in this shell"
+      exit 1
+   fi
 
 
-python3 rndadj.py
+   python3 rndadj.py
 
-out="none"
-read -p "run cutechess tests? " ans
-case "$ans" in
-   [Yy])
-      out=$(. ./net_run.sh 2>net_run.err)
-      ;;
-   *)
-      exit 2
-esac
-echo "tests started, out=<$out>"
+   out="none"
+   read -p "run cutechess tests? " ans
+   case "$ans" in
+      [Yy])
+         out=$(. ./net_run.sh 2>net_run.err)
+         ;;
+      *)
+         exit 2
+   esac
+   echo "tests started, out=<$out>"
 
-# Wait for tests to finish and then update db
+   # Wait for tests to finish and then update db
 
-wait
+   wait
 
-# use $out ...
-#python3 results.py
+   # use $out ...
+   #python3 results.py
 
+done
 

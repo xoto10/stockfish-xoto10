e5ffdf78cc82cc67d9ac426e9deef14aaa7ab3c2
xoto10 <buylow001@gmail.com>
2020-11-15 20:44:16 +0000

    More ttpv reduction if depth < 9. Bench 3739654

--- src/search.cpp
+++ src/search.cpp
@@ -1167,7 +1167,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if position is or has been on the PV (~10 Elo)
           if (ss->ttPv)
-              r -= 2;
+              r -= 1 + (depth > 8);
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if ((rootNode || !PvNode) && depth > 10 && thisThread->bestMoveChanges <= 2)

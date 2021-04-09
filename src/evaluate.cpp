0a94703e5eef9de6690aeb692103517f1319380e
xoto10 <buylow001@gmail.com>
2021-04-09 19:21:20 +0100

    Update nnue scale constant, 592. Bench 4591187

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1092,7 +1092,7 @@ Value Eval::evaluate(const Position& pos) {
       auto  adjusted_NNUE = [&]()
       {
          int material = pos.non_pawn_material() + 4 * PawnValueMg * pos.count<PAWN>();
-         int scale =  604
+         int scale =  592
                     + material / 32
                     - 4 * pos.rule50_count();
 

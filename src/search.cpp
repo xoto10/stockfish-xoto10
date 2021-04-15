c81580c2ff4df1ab345c2ca653c0e525b40a771e
xoto10 <buylow001@gmail.com>
2021-04-15 13:29:17 +0100

    Vary tempoNNUE with recent rootDepth (ema with /2, rrd-3). Bench 4792391

--- src/search.cpp
+++ src/search.cpp
@@ -388,7 +388,7 @@ void Thread::search() {
       if (!Threads.increaseDepth)
          searchAgainCounter++;
 
-      tempoNNUE = std::clamp((recentRootDepth + 1) * 6 / 7, 18, 30);
+      tempoNNUE = std::clamp((recentRootDepth - 3) * 6 / 7, 18, 30);
 
       // MultiPV loop. We perform a full root search for each PV line
       for (pvIdx = 0; pvIdx < multiPV && !Threads.stop; ++pvIdx)

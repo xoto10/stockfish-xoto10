68b9d5caa418522327915f71d684d8052cd7747b
xoto10 <buylow001@gmail.com>
2021-04-15 21:26:50 +0100

    Vary tempoNNUE with recentRootDepth (ema with /2, rrd-3, *47). Bench 4463567

--- src/thread.cpp
+++ src/thread.cpp
@@ -61,6 +61,7 @@ void Thread::clear() {
   mainHistory.fill(0);
   lowPlyHistory.fill(0);
   captureHistory.fill(0);
+  recentRootDepth = 24;
 
   for (bool inCheck : { false, true })
       for (StatsType c : { NoCaptures, Captures })
@@ -204,7 +205,6 @@ void ThreadPool::start_thinking(Position& pos, StateListPtr& states,
   {
       th->nodes = th->tbHits = th->nmpMinPly = th->bestMoveChanges = 0;
       th->rootDepth = th->completedDepth = 0;
-      th->recentRootDepth = 24;
       th->rootMoves = rootMoves;
       th->rootPos.set(pos.fen(), pos.is_chess960(), &th->rootState, th);
       th->rootState = setupStates->back();

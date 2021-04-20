4c89a8068a7f9802134736749c7fcba3eb769125
xoto10 <buylow001@gmail.com>
2021-04-20 17:36:00 +0100

    Less time reduction if losing, >-50. Bench 4503918

--- src/search.cpp
+++ src/search.cpp
@@ -511,7 +511,7 @@ void Thread::search() {
           fallingEval = std::clamp(fallingEval, 0.5, 1.5);
 
           // If the bestMove is stable over several iterations, reduce time accordingly
-          timeReduction = lastBestMoveDepth + 9 < completedDepth ? 1.92 : 0.95;
+          timeReduction = lastBestMoveDepth + 9 < completedDepth && bestValue > -50 ? 1.92 : 0.95;
           double reduction = (1.47 + mainThread->previousTimeReduction) / (2.32 * timeReduction);
 
           // Use part of the gained time from a previous stable move for the current move

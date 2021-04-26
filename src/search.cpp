961aa0718f6aafb28d78aa8ce548492c469719a5
xoto10 <buylow001@gmail.com>
2021-04-26 18:55:11 +0100

    More time if losing, constant +600 in fallingEval if <-100. Bench 4075325

--- src/search.cpp
+++ src/search.cpp
@@ -508,7 +508,7 @@ void Thread::search() {
       {
           double fallingEval = (318 + 6 * (mainThread->bestPreviousScore - bestValue)
                                     + 6 * (mainThread->iterValue[iterIdx] - bestValue)
-                                    + 300 * (bestValue < -100)
+                                    + 600 * (bestValue < -100)
                                ) / 825.0;
           fallingEval = std::clamp(fallingEval, 0.5, 1.5);
 

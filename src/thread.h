047c4c9c683b3dba9e25049f75c5db4afef85804
xoto10 <buylow001@gmail.com>
2021-04-27 14:03:21 +0100

    Prevent large time reduction if losing. Bench 4075325

--- src/thread.h
+++ src/thread.h
@@ -88,7 +88,7 @@ struct MainThread : public Thread {
   void search() override;
   void check_time();
 
-  double previousTimeReduction;
+  double previousStableMove;
   Value bestPreviousScore;
   Value iterValue[4];
   int callsCnt;

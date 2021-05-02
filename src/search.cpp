e7b33729e08dba2c8195437624d7c8fce6b9300c
xoto10 <buylow001@gmail.com>
2021-05-02 16:55:23 +0100

    More time if recent moves were quick, <4.0, *4.0/x, bv<-50. Bench 3993071

--- src/search.cpp
+++ src/search.cpp
@@ -525,7 +525,7 @@ void Thread::search() {
 
           double thinkMore = 1.0;
           mainThread->stableAdjustment[mainThread->moveIdx] = reduction * bestMoveInstability;
-          if (rootDepth > 10 && bestValue < -100)
+          if (rootDepth > 10 && bestValue < -50)
           {
               double stable = (  mainThread->stableAdjustment[0] + mainThread->stableAdjustment[1]
                                + mainThread->stableAdjustment[2] + mainThread->stableAdjustment[3]);
@@ -534,9 +534,9 @@ void Thread::search() {
 //          << " stadj1 " << mainThread->stableAdjustment[1]
 //          << " mvidx " << mainThread->moveIdx
 //          << sync_endl;
-              if (stable < 3.2)
+              if (stable < 4.0)
               {
-                  thinkMore = 3.2 / stable;
+                  thinkMore = 4.0 / stable;
                   mainThread->stableAdjustment[mainThread->moveIdx] *= thinkMore;
               }
           }

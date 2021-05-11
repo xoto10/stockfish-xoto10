cfd1700aa2b6010d1015e36a6acf8b881d138255
xoto10 <buylow001@gmail.com>
2021-05-11 20:47:52 +0100

    Change order of last lmr conditions. Bench 3836428

--- src/search.cpp
+++ src/search.cpp
@@ -1136,7 +1136,7 @@ moves_loop: // When in check, search starts from here
               || cutNode
               || (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 3678)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024)
-          && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
+          && (ss->ply > 1 || !PvNode || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
 

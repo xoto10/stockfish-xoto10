70df561db9f57c11aee50410851602dc8bee8961
xoto10 <buylow001@gmail.com>
2021-05-16 21:13:25 +0100

    Values for more time if losing tune 3 (282k), 267/257.254, 125/162. Also reduced bmi, 1.4*. Bench 4749514

--- src/search.cpp
+++ src/search.cpp
@@ -481,7 +481,7 @@ void Thread::search() {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
           }
-          double bestMoveInstability = 1 + 2 * totBestMoveChanges / Threads.size();
+          double bestMoveInstability = 1 + 1.4 * totBestMoveChanges / Threads.size();
 
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 

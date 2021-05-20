41d421f9164154d814cf7d26e0d5c135b11bdfc3
xoto10 <buylow001@gmail.com>
2021-05-20 12:54:59 +0100

    Simplify lowPieceEndgame, remove single bishop test. Bench 3870673

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1135,8 +1135,7 @@ Value Eval::evaluate(const Position& pos) {
 
       // Use classical evaluation for really low piece endgames.
       // One critical case is the draw for bishop + A/H file pawn vs naked king.
-      bool lowPieceEndgame =   pos.non_pawn_material() == BishopValueMg
-                            || (pos.non_pawn_material() < 2 * RookValueMg && pos.count<PAWN>() < 2);
+      bool lowPieceEndgame = pos.non_pawn_material() < 2 * RookValueMg && pos.count<PAWN>() < 2;
 
       v = largePsq || lowPieceEndgame ? Evaluation<NO_TRACE>(pos).value()  // classical
                                       : adjusted_NNUE();                   // NNUE

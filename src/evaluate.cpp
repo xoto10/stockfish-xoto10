a7c1e4030bbf3535be0b8421072718fee6133d86
xoto10 <buylow001@gmail.com>
2021-05-26 17:35:23 +0100

    reformat (bench change either indicates a rounding difference or an error?!)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -908,7 +908,7 @@ namespace {
     int pc = pos.count<PAWN>();
     int npc = pos.count<ALL_PIECES>() - pos.count<PAWN>();
     int material =  std::max(-11, 13 * pc - 35)
-                  - 1511 * npc * npc / 4096 + 6 * npc + 32
+                  + (-1511 * npc + 24576) * npc / 4096 + 32
                   + 65 * !pos.non_pawn_material();
 
     // Compute the initiative bonus for the attacking side

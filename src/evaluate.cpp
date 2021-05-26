103d6d87f4435ac1da07b51ae0fe9982e32d634d
xoto10 <buylow001@gmail.com>
2021-05-26 23:09:29 +0100

    Adjust sf with almostUnwinnable, 8*. Bench 3575429

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -969,7 +969,7 @@ namespace {
         // Reduce scale factor in case of pawns being on a single flank
         sf -= 4 * !pawnsOnBothFlanks;
 
-        sf -= 4 * almostUnwinnable;
+        sf -= 8 * almostUnwinnable;
     }
 
     // Interpolate between the middlegame and (scaled by 'sf') endgame score

fa219c0eb0096fec55012b13a36f3082c1c2903b
xoto10 <buylow001@gmail.com>
2021-05-26 17:13:55 +0100

    Results from tune, linear, quad, 65. Bench 3604842

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -909,7 +909,7 @@ namespace {
     int npc = pos.count<ALL_PIECES>() - pos.count<PAWN>();
     int material =  std::max(-11, 13 * pc - 35)
                   - 1511 * npc * npc / 4096 + 6 * npc + 32
-                  + 57 * !pos.non_pawn_material();
+                  + 65 * !pos.non_pawn_material();
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()

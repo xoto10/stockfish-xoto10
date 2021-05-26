e9d0d05c8b4c23815f3b56996a456ad4e77d952a
xoto10 <buylow001@gmail.com>
2021-05-26 23:29:25 +0100

    Adjust sf with pawn count, -9. Bench 3587415

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -969,7 +969,7 @@ namespace {
         // Reduce scale factor in case of pawns being on a single flank
         sf -= 4 * !pawnsOnBothFlanks;
 
-        sf += 4 * pe->passed_count();
+        sf += pos.count<PAWN>() - 9;
     }
 
     // Interpolate between the middlegame and (scaled by 'sf') endgame score

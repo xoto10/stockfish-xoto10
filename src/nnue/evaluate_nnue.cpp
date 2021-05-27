1dd8189b11774897d420a64fdf29910814afb249
xoto10 <buylow001@gmail.com>
2021-05-27 23:04:20 +0100

    Adjust mat/pos, mat+500. Bench 3401859

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -161,8 +161,8 @@ namespace Stockfish::Eval::NNUE {
     const auto psqt = featureTransformer->transform(pos, transformedFeatures, bucket);
     const auto output = network[bucket]->propagate(transformedFeatures, buffer);
 
-    int materialist = psqt;
-    int positional  = output[0] + 1000;
+    int materialist = psqt + 500;
+    int positional  = output[0];
 
     int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
     int entertainment = (adjusted && delta_npm <= BishopValueMg - KnightValueMg ? 7 : 0);

7508193e7079d210372061fe529c17474f4d77aa
xoto10 <buylow001@gmail.com>
2021-05-28 14:22:55 +0100

    sn: Take 2, simpler version, comp=10. Bench 3651434

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -158,14 +158,26 @@ namespace Stockfish::Eval::NNUE {
     ASSERT_ALIGNED(buffer, alignment);
 
     const std::size_t bucket = (pos.count<ALL_PIECES>() - 1) / 4;
+
     const auto psqt = featureTransformer->transform(pos, transformedFeatures, bucket);
     const auto output = network[bucket]->propagate(transformedFeatures, buffer);
 
-    int materialist = psqt;
-    int positional  = output[0];
+    int materialist   = psqt;
+    int positional    = output[0];
+    int entertainment = 0;
 
-    int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
-    int entertainment = (adjusted && delta_npm <= BishopValueMg - KnightValueMg ? 7 : 0);
+    if (adjusted)
+    {
+        Color stm     = pos.side_to_move();
+        int delta_npm = pos.non_pawn_material(stm) - pos.non_pawn_material(~stm);
+
+        entertainment = (abs(delta_npm) <= BishopValueMg - KnightValueMg ? 7 : 0);
+
+        if (abs(positional) > 5000)
+           entertainment +=   positional < 0 && materialist > 0            ? 10
+                            : positional > 0 && delta_npm > -KnightValueMg ? 10
+                                                                           : 0  ;
+    }
 
     int A = 128 - entertainment;
     int B = 128 + entertainment;

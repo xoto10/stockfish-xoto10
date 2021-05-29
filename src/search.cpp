102ddbd2b27eb1262b24153b02d17ff73fb9e310
xoto10 <buylow001@gmail.com>
2021-05-29 20:42:40 +0100

    Half value for 1st 1.0 of totBestMoveChanges. Bench 3831936

--- src/search.cpp
+++ src/search.cpp
@@ -477,7 +477,8 @@ void Thread::search() {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
           }
-          double bestMoveInstability = 1 + 2 * totBestMoveChanges / Threads.size();
+          double bestMoveInstability = 1 + 2 * (std::min(1.0, totBestMoveChanges) / 2 + std::max(0.0, totBestMoveChanges - 1))
+                                             / Threads.size();
 
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 

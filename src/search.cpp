1e4fe5cee55b6298531f68d5fd3e8a3327f82bae
xoto10 <buylow001@gmail.com>
2021-05-31 21:41:07 +0100

    Change bmc multiplier with rootdepth, 1.10, 2-10/rd, use double. Bench 4447112

--- src/search.cpp
+++ src/search.cpp
@@ -480,9 +480,8 @@ void Thread::search() {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
           }
-          double bestMoveInstability = 1.14 + (std::max(1.0, 2.0 - 10 / rootDepth))
+          double bestMoveInstability = 1.10 + std::max(1.0, 2.0 - 10.0 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
-
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments

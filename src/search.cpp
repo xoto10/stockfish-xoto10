51b386252d85f95067d4d718722a234218976f56
xoto10 <buylow001@gmail.com>
2021-06-05 16:42:00 +0100

    Use lastBestMoveDepth for bmi, 1.09 + (2.3-8/max(7,lbmd)). Bench 4364128

--- src/search.cpp
+++ src/search.cpp
@@ -146,8 +146,6 @@ namespace {
 
 } // namespace
 
-int A=218, B= 220, C=240, D=240;
-TUNE(A, B, D, SetRange(-240, 720), C);
 
 /// Search::init() is called at startup to initialize various lookup tables
 
@@ -482,8 +480,8 @@ void Thread::search() {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
           }
-          double bestMoveInstability = A/200.0 + (B/100.0 - (C/30.0) / std::max(D/30.0, double(lastBestMoveDepth)))
-                                                 * totBestMoveChanges / Threads.size();
+          double bestMoveInstability = 1.09 + (2.3 - 8.0 / std::max(7, lastBestMoveDepth))
+                                              * totBestMoveChanges / Threads.size();
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments

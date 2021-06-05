23edae2e18c8633fc8215c6bfba93527ecd8d95f
xoto10 <buylow001@gmail.com>
2021-06-05 13:54:42 +0100

    Use lastBestMoveDepth for bmi, 1.09 + (2.2-8/max(10,lbmd)). Bench 4364128

--- src/search.cpp
+++ src/search.cpp
@@ -480,7 +480,7 @@ void Thread::search() {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
           }
-          double bestMoveInstability = 1.09 + (2.2 - 8.0 / std::max(8, lastBestMoveDepth))
+          double bestMoveInstability = 1.09 + (2.2 - 8.0 / std::max(10, lastBestMoveDepth))
                                               * totBestMoveChanges / Threads.size();
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 

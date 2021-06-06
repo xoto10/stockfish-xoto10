1c789f7ef81cf902f0967d5dbceedd7fb61f0b89
xoto10 <buylow001@gmail.com>
2021-06-06 18:03:06 +0100

    Tuned values after 50k games. Bench 4364128

--- src/search.cpp
+++ src/search.cpp
@@ -480,7 +480,7 @@ void Thread::search() {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
           }
-          double bestMoveInstability = 1.12 + (2.18 - 8.50 / std::max(8, lastBestMoveDepth))
+          double bestMoveInstability = 1.12 + (2.17 - 8.80 / std::max(8, lastBestMoveDepth))
                                               * totBestMoveChanges / Threads.size();
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 

f24e5d1b110d74ef2de365721c7755c5aa0b6ac1
xoto10 <buylow001@gmail.com>
2021-06-08 23:13:45 +0100

    How valuable is timeReduction, test fixed 0.90. Bench 5530620

--- src/search.cpp
+++ src/search.cpp
@@ -478,7 +478,7 @@ void Thread::search() {
           }
           double bestMoveInstability = 1.073 + std::max(1.0, 2.25 - 9.9 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
-          double totalTime = Time.optimum() * fallingEval * 0.85 * bestMoveInstability;
+          double totalTime = Time.optimum() * fallingEval * 0.90 * bestMoveInstability;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
           // yielding correct scores and sufficiently fast moves.

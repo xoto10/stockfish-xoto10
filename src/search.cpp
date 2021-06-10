6be6140f5444eef4057eb274ebbb3956ff3465bd
xoto10 <buylow001@gmail.com>
2021-06-10 08:12:46 +0100

    Extra time if stable, +1.50. Bench 5530620

--- src/search.cpp
+++ src/search.cpp
@@ -473,8 +473,8 @@ void Thread::search() {
           // If the bestMove is stable over several iterations, reduce time accordingly
           timeReduction = lastBestMoveDepth + 9 < completedDepth ? 1.92 : 0.95;
           double extra = 0;
-          if (bestValue < -50 && mainThread->previousTimeReduction + timeReduction > 3.5)
-              extra = 2.00;
+          if (mainThread->previousTimeReduction + timeReduction > 3.5)
+              extra = 1.50;
           double reduction = (1.47 + mainThread->previousTimeReduction + extra) / (2.32 * timeReduction);
 
           // Use part of the gained time from a previous stable move for the current move

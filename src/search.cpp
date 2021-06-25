b0d3f23ed7a3aad908bb690903bb5fa5486f5267
xoto10 <buylow001@gmail.com>
2021-06-25 10:47:02 +0100

    More base time for fallingEval, 418, less for bmi, 1.05. Bench 5139233

--- src/search.cpp
+++ src/search.cpp
@@ -454,7 +454,7 @@ void Thread::search() {
           && !Threads.stop
           && !mainThread->stopOnPonderhit)
       {
-          double fallingEval = (518 + 6 * (mainThread->bestPreviousScore - bestValue)
+          double fallingEval = (418 + 6 * (mainThread->bestPreviousScore - bestValue)
                                     + 6 * (mainThread->iterValue[iterIdx] - bestValue)) / 825.0;
           fallingEval = std::clamp(fallingEval, 0.5, 1.5);
 
@@ -468,7 +468,7 @@ void Thread::search() {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
           }
-          double bestMoveInstability = 1.073 + std::max(1.0, 2.25 - 9.9 / rootDepth)
+          double bestMoveInstability = 1.05 + std::max(1.0, 2.25 - 9.9 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
 

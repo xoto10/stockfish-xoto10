c401e589157418a031f26b5e918bfa27b6832486
xoto10 <buylow001@gmail.com>
2021-06-30 12:04:22 +0100

    Scale optimumTime with timeLeft, 1.89/. Bench 4687476

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -81,7 +81,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
   // game time for the current move, so also cap to 20% of available game time.
   if (limits.movestogo == 0)
   {
-      optScale = std::min(0.0084 + std::pow(ply + 3.0, 0.5) * (0.0042 + 3.78 / std::max(9000, int(timeLeft))),
+      optScale = std::min(0.0084 + std::pow(ply + 3.0, 0.5) * (0.0042 + 1.89 / std::max(9000, int(timeLeft))),
                            0.2 * limits.time[us] / double(timeLeft));
       maxScale = std::min(7.0, 4.0 + ply / 12.0);
   }

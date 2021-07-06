6460a9c14781fb9faee3b5045c12d7aafff5354d
xoto10 <buylow001@gmail.com>
2021-07-06 18:47:02 +0100

    Prefer captures that are near their king, <2. Bench 5146694

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -104,7 +104,7 @@ void MovePicker::score() {
       if constexpr (Type == CAPTURES)
           m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))]
-                   + 1000 * (distance<Square>(pos.square<KING>(~pos.side_to_move()), to_sq(m)) < 3);
+                   + 1000 * (distance<Square>(pos.square<KING>(~pos.side_to_move()), to_sq(m)) < 2);
 
       else if constexpr (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]

5c4117a63bf4dc27fe764ed69294e44bc6a893a7
xoto10 <buylow001@gmail.com>
2021-07-10 00:00:15 +0100

    Values from tune (45k). Bench 4687476

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -26,9 +26,6 @@
 
 namespace Stockfish {
 
-int A=1510, B=1842, C=1440;
-TUNE(A,B,C);
-
 TimeManagement Time; // Our global time management object
 
 
@@ -80,7 +77,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
   // game time for the current move, so also cap to 20% of available game time.
   if (limits.movestogo == 0)
   {
-      optScale = std::min( std::max(A/200000.0, B/100000.0 - (C/10.0) / int(timeLeft))
+      optScale = std::min( std::max(0.0075, 0.01803 - 143.0 / int(timeLeft))
                              + std::pow(ply + 3.0, 0.5) * 0.0042,
                            0.2 * limits.time[us] / double(timeLeft) );
       maxScale = std::min(7.0, 4.0 + ply / 12.0);

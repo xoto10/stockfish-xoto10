61723425fe017d2f30ddf11210ccb9a79cd4d69e
xoto10 <buylow001@gmail.com>
2021-07-09 21:08:52 +0100

    Swap bestmove sometimes if matching alpha. Bench 4991927

--- src/search.cpp
+++ src/search.cpp
@@ -1212,7 +1212,8 @@ moves_loop: // When in check, search starts from here
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
-      if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta))))
+      int alphaMatch = !(thisThread->nodes & 127);
+      if (PvNode && (moveCount == 1 || (value > alpha - alphaMatch && (rootNode || value < beta))))
       {
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
@@ -1239,7 +1240,7 @@ moves_loop: // When in check, search starts from here
                                     thisThread->rootMoves.end(), move);
 
           // PV move or new best move?
-          if (moveCount == 1 || value > alpha)
+          if (moveCount == 1 || value > alpha - alphaMatch)
           {
               rm.score = value;
               rm.selDepth = thisThread->selDepth;
@@ -1252,7 +1253,7 @@ moves_loop: // When in check, search starts from here
 
               // We record how often the best move has been changed in each
               // iteration. This information is used for time management and LMR
-              if (moveCount > 1)
+              if (moveCount > 1 && value > alpha)
                   ++thisThread->bestMoveChanges;
           }
           else

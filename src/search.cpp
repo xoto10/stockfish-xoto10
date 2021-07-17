30ae78b213eb003deef0a93e87d53e4573df6529
xoto10 <buylow001@gmail.com>
2021-07-17 09:15:28 +0100

    More time if hitting the cache less, full, max(1). Bench 5505251

--- src/search.cpp
+++ src/search.cpp
@@ -470,8 +470,7 @@ void Thread::search() {
           }
           double bestMoveInstability = 1.073 + std::max(1.0, 2.25 - 9.9 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
-          double cached = std::max(1.0, (double(TtHitAverageResolution * TtHitAverageWindow) / ttHitAverage + 2) * 0.25);
-//sync_cout << "info string ha " << ttHitAverage << " cached " << cached << sync_endl;
+          double cached = std::max(1.0, (0.5 * TtHitAverageResolution * TtHitAverageWindow) / ttHitAverage);
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * cached;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments

ac4577567384883547057d9b1c34561db3602a0b
xoto10 <buylow001@gmail.com>
2021-07-21 12:28:52 +0100

    More positional if >2 blocked pawns, +3. Bench 5217174

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -168,7 +168,9 @@ namespace Stockfish::Eval::NNUE {
     int positional  = output[0];
 
     int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
-    int entertainment = (adjusted && delta_npm <= BishopValueMg - KnightValueMg ? 7 : 0);
+    int entertainment = (adjusted && delta_npm <= BishopValueMg - KnightValueMg
+                        ? 7 + 3 * (popcount(pos.pieces(WHITE, PAWN) & shift<SOUTH>(pos.pieces(BLACK, PAWN))) > 2)
+                        : 0);
 
     int A = 128 - entertainment;
     int B = 128 + entertainment;

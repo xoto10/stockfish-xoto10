b3ba0a4b5073437c8d374235666970c0317a51f6
xoto10 <buylow001@gmail.com>
2021-07-23 12:05:54 +0100

    More time if tthitavg lower than average. Bench 5505251

--- src/thread.h
+++ src/thread.h
@@ -87,7 +87,7 @@ struct MainThread : public Thread {
   void search() override;
   void check_time();
 
-  double previousTimeReduction;
+  double previousTimeReduction, ttHitMoveAverage;
   Value bestPreviousScore;
   Value iterValue[4];
   int callsCnt;

9d8c54e58c5fd8635bed25c6c32c13ff41ad9507
xoto10 <buylow001@gmail.com>
2021-07-24 23:27:17 +0100

    Deter draws where pawn count differs by more than 1. Bench 5223401

--- src/search.cpp
+++ src/search.cpp
@@ -87,8 +87,10 @@ namespace {
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
-  Value value_draw(Thread* thisThread) {
-    return VALUE_DRAW + Value(2 * (thisThread->nodes & 1) - 1);
+  Value value_draw(Position& pos) {
+    return VALUE_DRAW
+         + (abs(pos.count<PAWN>(WHITE) - pos.count<PAWN>(BLACK)) > 1
+              ? -1 : Value(2 * (pos.this_thread()->nodes & 1) - 1));
   }
 
   // Skill structure is used to implement strength limit
@@ -529,7 +531,7 @@ namespace {
         && alpha < VALUE_DRAW
         && pos.has_game_cycle(ss->ply))
     {
-        alpha = value_draw(pos.this_thread());
+        alpha = value_draw(pos);
         if (alpha >= beta)
             return alpha;
     }
@@ -582,7 +584,7 @@ namespace {
             || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
             return (ss->ply >= MAX_PLY && !ss->inCheck) ? evaluate(pos)
-                                                        : value_draw(pos.this_thread());
+                                                        : value_draw(pos);
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply+1), but if alpha is already bigger because
@@ -743,7 +745,7 @@ namespace {
 
         // Randomize draw evaluation
         if (eval == VALUE_DRAW)
-            eval = value_draw(thisThread);
+            eval = value_draw(pos);
 
         // Can ttValue be used as a better position evaluation?
         if (    ttValue != VALUE_NONE

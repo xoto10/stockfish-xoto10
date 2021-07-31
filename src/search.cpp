e89cb9334326f094c45b0090d6de0b9faa781eec
xoto10 <buylow001@gmail.com>
2021-07-31 09:46:59 +0100

    Quiet move bonus for pawns towards king, (r-3)*sb(d+1). Bench 5192664

--- src/search.cpp
+++ src/search.cpp
@@ -1710,9 +1710,8 @@ moves_loop: // When in check, search starts here
     else
     {
         // Bonus for advanced pawns
-        if ( distance<File>(to_sq(move), pos.square<KING>(~us)) < 4 )
-            thisThread->mainHistory[us][from_to(move)] <<
-                (relative_rank(us, to_sq(move)) - RANK_3) * stat_bonus(depth - 1);
+        thisThread->mainHistory[us][from_to(move)] <<
+            (relative_rank(us, to_sq(move)) - RANK_3) * stat_bonus(depth + 1);
     }
 
     // Update countermove history

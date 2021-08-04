3dea425f55287cc79b59ffb10c9ae24db30b286b
xoto10 <buylow001@gmail.com>
2021-08-04 20:27:58 +0100

    Use 5 quiets when skipQuiets=true. Bench 5193057

--- src/search.cpp
+++ src/search.cpp
@@ -1001,7 +1001,7 @@ moves_loop: // When in check, search starts here
       {
           // Only process first quiet move if movecount exceeds our FutilityMoveCount threshold
           moveCountPruning = moveCount >= futility_move_count(improving, depth)
-                            && quiets > 2;
+                            && quiets > 4;
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);

73700ddae9abdab9618af53838173a1d744b2c14
xoto10 <buylow001@gmail.com>
2021-08-08 04:12:15 +0100

    mcpruning, add quiets outside 75-150cp. Bench 5795435

--- src/search.cpp
+++ src/search.cpp
@@ -1001,7 +1001,7 @@ moves_loop: // When in check, search starts here
       {
           // Only process first quiet move if movecount exceeds our FutilityMoveCount threshold
           moveCountPruning = moveCount >= futility_move_count(improving, depth)
-                            && (79*depth + 112*quiets) > 516;
+                            && (abs(bestValue) < 150 || 300 < abs(bestValue) || (79*depth + 112*quiets) > 516);
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);

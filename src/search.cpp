0313c2d38263824c6b1568256f82f76a6b0552e7
xoto10 <buylow001@gmail.com>
2021-08-20 06:27:06 +0100

    less movecountpruning. Bench 5455572

--- src/search.cpp
+++ src/search.cpp
@@ -997,7 +997,8 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth + (PvNode && ttMove));
+
 
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount), 0);

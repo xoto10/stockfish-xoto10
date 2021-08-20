114e7df97c95bbe840cf034699a5653e8acd7bbf
xoto10 <buylow001@gmail.com>
2021-08-20 15:03:01 +0100

    Less movecountpruning, +tt (bugfix). Test on top of passed patch by candirufish. Bench 5318041

--- src/search.cpp
+++ src/search.cpp
@@ -997,7 +997,7 @@ moves_loop: // When in check, search starts here
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-          moveCountPruning = moveCount >= futility_move_count(improving, depth + ttMove);
+          moveCountPruning = moveCount >= futility_move_count(improving, depth + bool(ttMove));
 
 
           // Reduced depth of the next LMR search

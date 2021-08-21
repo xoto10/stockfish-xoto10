afc96585dfef11b62b90d8ad5092fb1f34ba6b22
xoto10 <buylow001@gmail.com>
2021-08-21 19:10:08 +0100

    Higher cap on optimum time use, 0.222. Bench 4844618

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -78,7 +78,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
   if (limits.movestogo == 0)
   {
       optScale = std::min(0.0084 + std::pow(ply + 3.0, 0.5) * 0.0042,
-                           0.25 * limits.time[us] / double(timeLeft));
+                           0.222 * limits.time[us] / double(timeLeft));
       maxScale = std::min(7.0, 4.0 + ply / 12.0);
   }
 

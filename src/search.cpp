9945d6922e2b4161631182ce787257b0357a6b6e
xoto10 <buylow001@gmail.com>
2021-08-22 08:01:40 +0100

    More time if most pieces left and eval high, 0.924, 0.61. Bench 5375286

--- src/search.cpp
+++ src/search.cpp
@@ -470,10 +470,10 @@ void Thread::search() {
           }
           double bestMoveInstability = 1.073 + std::max(1.0, 2.25 - 9.9 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
-          double chances =  0.94
+          double chances =  0.924
                           + std::max(0, rootPos.count<ALL_PIECES>() - 24)
                               * std::min(8, abs(bestValue / 32))
-                              * 0.00625;
+                              * 0.0061;
 
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * chances;
 

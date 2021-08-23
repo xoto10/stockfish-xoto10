f136af74da7cbcf01fc2ef8f4e3a26900aac10ac
xoto10 <buylow001@gmail.com>
2021-08-23 11:21:56 +0100

    Vary eval scaling with score, ltc tune 50k, eval once. Bench 4875333

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1092,13 +1092,14 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         Value ue = Value( std::clamp(abs(NNUE::evaluate(pos, true)), 0, 197) );
+         Value nn = NNUE::evaluate(pos, true);
+         Value ue = Value( std::clamp(abs(nn), 0, 197) );
          int np = pos.count<PAWN>() + pos.non_pawn_material() / 1024;
          int scale =   1046 - ue / 2
                      + (3260 + 160 * ue) * np / 1024
                      + (28796 - 127 * ue) * np * np / 32768;
 
-         Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
+         Value nnue = nn * scale / 1024;
 
          if (pos.is_chess960())
              nnue += fix_FRC(pos);

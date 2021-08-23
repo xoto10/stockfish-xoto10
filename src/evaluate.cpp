13246979797e9eea9b3162492f6cfb0042d1b637
xoto10 <buylow001@gmail.com>
2021-08-23 21:17:17 +0100

    Quad scaling, 993,5,0.8. Bench 5598733

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1090,9 +1090,10 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int scale =   883
-                     + 32 * pos.count<PAWN>()
-                     + 32 * pos.non_pawn_material() / 1024;
+         int mat = pos.count<PAWN>() + pos.non_pawn_material() / 1024;
+         int scale =   993
+                     + 5 * mat
+                     + 8 * mat * mat / 10;
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
 

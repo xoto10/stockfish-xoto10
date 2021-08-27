f598b65393440c3737765a6235f794de88661655
xoto10 <buylow001@gmail.com>
2021-08-27 19:53:20 +0100

    Lower scale with blocked pawns. Bench 5081041

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1092,8 +1092,9 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
+         int bl = popcount( shift<SOUTH>(pos.pieces(BLACK, PAWN)) & pos.pieces(WHITE, PAWN) );
          int scale =   883
-                     + 32 * pos.count<PAWN>()
+                     + 32 * (pos.count<PAWN>() - bl)
                      + 32 * pos.non_pawn_material() / 1024;
 
          Value nnue = NNUE::evaluate(pos, true) * scale / 1024;

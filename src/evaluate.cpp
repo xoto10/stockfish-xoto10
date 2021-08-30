e78f18505fd4ec43cc746a27395db1ebf26fda40
xoto10 <buylow001@gmail.com>
2021-08-30 09:16:43 +0100

    Shift nnue while scaling, 5+0.05 tune, 30k games. Bench 4729691

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,9 +191,6 @@ using namespace Trace;
 
 namespace {
 
-int A=20, B=32;
-TUNE(SetRange(-100, 150), A, B);
-
   // Threshold for lazy and space evaluation
   constexpr Value LazyThreshold1    =  Value(3130);
   constexpr Value LazyThreshold2    =  Value(2204);
@@ -1096,9 +1093,9 @@ Value Eval::evaluate(const Position& pos) {
       auto  adjusted_NNUE = [&]()
       {
          int scale =   883
-                     + B  * pos.count<PAWN>()
-                     + B  * pos.non_pawn_material() / 1024;
-         int shift = A;
+                     + 33 * pos.count<PAWN>()
+                     + 33 * pos.non_pawn_material() / 1024;
+         int shift = 10;
 
          Value nnue = (NNUE::evaluate(pos, true) + shift) * scale / 1024 - shift;
 

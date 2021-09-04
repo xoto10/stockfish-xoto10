d2dd77791cb29668f0997e3498da11fb8925a9e6
xoto10 <buylow001@gmail.com>
2021-09-04 20:33:30 +0100

    Tune tm nn (depth+bmc) at 5+0.05, 1+, scale 1m. Bench 5600615

--- src/search.cpp
+++ src/search.cpp
@@ -22,6 +22,7 @@
 #include <cstring>   // For std::memset
 #include <iostream>
 #include <sstream>
+#include <numeric>
 
 #include "evaluate.h"
 #include "misc.h"
@@ -57,6 +58,32 @@ using Eval::evaluate;
 using namespace Search;
 
 namespace {
+
+  // Net weights and biases of a small neural network for time management
+  int nw[2][2][2] =
+  {
+    {{ 1,  1},{ 1,  1}},
+    {{ 1,  1},{ 1,  1}}
+  };
+  int nb[2][2] =
+  {
+    { 3, 3},
+    { 3, 3}
+  };
+  int nwo[2] = { 2,  2};
+  int nbo = 4;
+
+auto f40 = [](int m){return Range(m - 40, m + 40);};
+auto f90 = [](int m){return Range(m - 90, m + 90);};
+
+TUNE(SetRange(f40), nw);
+TUNE(SetRange(f40), nb);
+TUNE(SetRange(f40), nwo);
+TUNE(SetRange(f90), nbo);
+
+  constexpr int lower_clamp = 30;
+  constexpr int upper_clamp = 300;
+//TUNE(SetDefaultRange, lower_clamp, upper_clamp);
 
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV, Root };
@@ -470,7 +497,24 @@ void Thread::search() {
           }
           double bestMoveInstability = 1.073 + std::max(1.0, 2.25 - 9.9 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
-          double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
+
+          // Inputs of the neural network
+          int ft[2] = { std::clamp(rootDepth, 0, 100),
+                        std::clamp(int(20 * totBestMoveChanges / Threads.size()), 0, 100) };
+          // Matrix multiplication (layers)
+          for (size_t m = 0; m < 2; ++m)
+          {
+              int temp[2] = {0};
+              for (size_t i = 0; i < 2; ++i)
+                  temp[i]= std::max(0, std::inner_product(ft, ft+2, nw[i][m], 0) + nb[m][i]); // ReLU activation function
+              for (size_t n = 0; n < 2; ++n)
+                  ft[n] = temp[n];
+          }
+          double nn = 1.0 + std::clamp((std::inner_product(ft, ft+2, nwo, 0) + nbo) / 1000000.0,
+                                       lower_clamp/100.0, upper_clamp/100.0);
+
+          double totalTime = Time.optimum() * fallingEval * reduction * nn * bestMoveInstability;
+//sync_cout << "info string nntim " << nn << sync_endl;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
           // yielding correct scores and sufficiently fast moves.

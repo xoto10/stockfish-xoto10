b793ddd2b262f852d25deb9a50243d848b451106
xoto10 <buylow001@gmail.com>
2021-09-14 14:40:33 +0100

    Increase optscale by 12% for base+inc tc. Bench 6504816

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -77,8 +77,8 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
   // game time for the current move, so also cap to 20% of available game time.
   if (limits.movestogo == 0)
   {
-      optScale = std::min(0.00924 + std::pow(ply + 3.0, 0.5) * 0.00462,
-                           0.22 * limits.time[us] / double(timeLeft));
+      optScale = std::min(0.00945 + std::pow(ply + 3.0, 0.5) * 0.00472,
+                           0.225 * limits.time[us] / double(timeLeft));
       maxScale = std::min(7.0, 4.0 + ply / 12.0);
   }
 

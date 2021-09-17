3155c957d58a10b21857c6017ab979dac3351432
xoto10 <buylow001@gmail.com>
2021-09-17 12:10:29 +0100

    Use piece count in optExtra calculation, 0.25*. Bench 6658747

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -71,7 +71,8 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, Position& pos) {
       limits.time[us] + limits.inc[us] * (mtg - 1) - moveOverhead * (2 + mtg));
 
   // Use extra time with larger increments
-  double optExtra = std::clamp(1.0 + 0.6 * pos.count<ALL_PIECES>() * limits.inc[us] / limits.time[us], 1.0, 1.075);
+  double optExtra = std::clamp(1.0 + 0.25 * pos.count<ALL_PIECES>() * limits.inc[us] / limits.time[us],
+                               1.0, 1.075);
 //sync_cout << "info string t " << limits.time[us] << " inc " << limits.inc[us]
 //          << " pcs " << pos.count<ALL_PIECES>() << " extra " << optExtra << sync_endl;
 

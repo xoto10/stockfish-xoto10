79c37e6ab79111ff55b8959f3160a1d7df2211c8
xoto10 <buylow001@gmail.com>
2021-09-19 10:16:18 +0100

    More extra time according to increment, 11.0*, 1.0-1.12. Bench 6658747

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -69,7 +69,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
       limits.time[us] + limits.inc[us] * (mtg - 1) - moveOverhead * (2 + mtg));
 
   // Use extra time with larger increments
-  double optExtra = std::clamp(1.0 + 11.8 * limits.inc[us] / limits.time[us], 1.0, 1.118);
+  double optExtra = std::clamp(1.0 + 11.0 * limits.inc[us] / limits.time[us], 1.0, 1.12);
 
   // A user may scale time usage by setting UCI option "Slow Mover"
   // Default is 100 and changing this value will probably lose elo.

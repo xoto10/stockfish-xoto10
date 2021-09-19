6278b5a505f45dcdfe1615432d33fc0c44ad9611
Michael Chaly <Vizvezdenec@gmail.com>
2021-09-20 02:43:48 +0300

    bench 6114419

--- src/search.cpp
+++ src/search.cpp
@@ -1057,7 +1057,7 @@ moves_loop: // When in check, search starts here
           && (tte->bound() & BOUND_LOWER)
           &&  tte->depth() >= depth - 3)
       {
-          Value singularBeta = ttValue - (2 + ttCapture) * depth;
+          Value singularBeta = ttValue - (2 + 2 * ttCapture) * depth;
           Depth singularDepth = (depth - 1) / 2;
 
           ss->excludedMove = move;

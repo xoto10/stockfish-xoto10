12de3678d12e46def1f6564744a206b3a2f7f21c
xoto10 <buylow001@gmail.com>
2021-09-23 12:07:31 +0100

    Entertainment values after 200k games, tune 1. Bench 5568173

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -34,9 +34,6 @@
 
 namespace Stockfish::Eval::NNUE {
 
-int E=7, F=128, G=128;
-TUNE(E, SetRange(64,192), F, G);
-
   // Input feature converter
   LargePagePtr<FeatureTransformer> featureTransformer;
 
@@ -170,10 +167,10 @@ TUNE(E, SetRange(64,192), F, G);
     int materialist = psqt;
     int positional  = output[0];
     int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
-    int entertainment = (adjusted && delta_npm <= RookValueMg - BishopValueMg ? E : 0);
+    int entertainment = (adjusted && delta_npm <= RookValueMg - BishopValueMg ? 5 : 0);
 
-    int A = F   - entertainment;
-    int B = G   + entertainment;
+    int A = 127 - entertainment;
+    int B = 125 + entertainment;
 
     int sum = (A * materialist + B * positional) / 128;
 

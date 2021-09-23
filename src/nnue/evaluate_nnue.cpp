d754074d16b7efe9ce7511da30ddc3996c0b362f
xoto10 <buylow001@gmail.com>
2021-09-23 12:46:08 +0100

    Entertainment value, 5. Bench 6358277

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -169,8 +169,8 @@ namespace Stockfish::Eval::NNUE {
     int delta_npm = abs(pos.non_pawn_material(WHITE) - pos.non_pawn_material(BLACK));
     int entertainment = (adjusted && delta_npm <= RookValueMg - BishopValueMg ? 5 : 0);
 
-    int A = 127 - entertainment;
-    int B = 125 + entertainment;
+    int A = 128 - entertainment;
+    int B = 128 + entertainment;
 
     int sum = (A * materialist + B * positional) / 128;
 

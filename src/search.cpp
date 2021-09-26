c478435c9a88a8d7c7c3fc3f01f8feea90279934
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2021-09-26 12:15:19 +0200

    Take 3. Bench: 6002098

--- src/search.cpp
+++ src/search.cpp
@@ -1192,7 +1192,7 @@ moves_loop: // When in check, search starts here
               r--;
 
           if (ss->nonFirstMoves == 1)
-              r++;
+              r--;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage.is_greater(537, 1024))

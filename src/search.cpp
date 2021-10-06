fe92af6e6d1ad7deb543fb93256ccac0672b5537
xoto10 <buylow001@gmail.com>
2021-10-06 12:12:14 +0100

    Linear sb after d7, no larger than old calculation. Bench 5795161

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((6 * d + 229) * d - 215 , 2000);
+    return d > 7 ? 6 * d + 1953 : 6 * d * d + 229 * d - 215;
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness

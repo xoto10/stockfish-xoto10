92252cea388354a15ecb7465b298df6449e3f230
xoto10 <buylow001@gmail.com>
2021-10-06 14:15:09 +0100

    sb as formula with no branch, 3000-3000/d. Bench 5764812

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,9 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min(3500, d > 7 ? 6 * d + 1953 : 6 * d * d + 229 * d - 215);
+    // assert(d > 0);
+    int bonus = 3000 - 3000 / d;
+    return bonus;
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness

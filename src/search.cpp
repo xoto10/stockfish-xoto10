66c5a9819b30f2a1d91dd6305977ba3ae6504b6b
xoto10 <buylow001@gmail.com>
2021-10-11 21:42:16 +0100

    Linear stat bonus, 230d - 102, 2052. Bench 6118101

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min(253 * d - 209, 2052);
+    return std::min(230 * d - 102, 2052);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness

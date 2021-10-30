7da4e1b16c7b722a331b95e9fec1b9271d31bb4f
xoto10 <buylow001@gmail.com>
2021-10-30 19:03:51 +0100

    Tune classical / nnue test using bestValue. Bench 6203535

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -150,6 +150,10 @@ namespace Eval {
 
 namespace Trace {
 
+auto f1 = [](int m){if (m<30) return Range(m-20,m+20); else return Range(m / 2, m * 3 / 2);};
+int A=826, B=100, C=20;
+TUNE(SetRange(f1), A, B, C);
+
   enum Tracing { NO_TRACE, TRACE };
 
   enum Term { // The first 8 entries are reserved for PieceType
@@ -1091,7 +1095,7 @@ Value Eval::evaluate(const Position& pos) {
 // master 0.171
 
   if (  !useNNUE
-      || abs(eg_value(pos.psq_score()) - bv) * 5 > (826 + pos.non_pawn_material() / 64) * (5 + pos.rule50_count()))
+      || abs(eg_value(pos.psq_score()) - bv) * 100 > (A   + pos.non_pawn_material() / 64) * (B + C * pos.rule50_count()))
       v = Evaluation<NO_TRACE>(pos).value();          // classical
   else
   {

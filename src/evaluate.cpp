74fe974d6fd0c42590f9bcdd1af2670f0bf867bf
xoto10 <buylow001@gmail.com>
2021-11-01 14:09:40 +0000

    No lazy skip until after rootdepth 10. Bench 6784191

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -988,7 +988,8 @@ namespace {
 
     // Early exit if score is high
     auto lazy_skip = [&](Value lazyThreshold) {
-        return abs(mg_value(score) + eg_value(score)) > lazyThreshold + pos.non_pawn_material() / 32;
+        return   pos.this_thread()->completedDepth > 10
+              && abs(mg_value(score) + eg_value(score)) > lazyThreshold + pos.non_pawn_material() / 32;
     };
 
     if (lazy_skip(LazyThreshold1))

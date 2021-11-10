51e17471ac7f2597cefa9b429c9d921e84e9cde0
xoto10 <buylow001@gmail.com>
2021-11-10 11:32:50 +0000

    fallingEval, 334, 5*. Bench 6547978

--- src/search.cpp
+++ src/search.cpp
@@ -477,8 +477,8 @@ void Thread::search() {
           && !Threads.stop
           && !mainThread->stopOnPonderhit)
       {
-          double fallingEval = (318 + 6 * (mainThread->bestPreviousScore - bestValue)
-                                    + 6 * (mainThread->iterValue[iterIdx] - bestValue)) / 825.0;
+          double fallingEval = (334 + 5 * (mainThread->bestPreviousScore - bestValue)
+                                    + 5 * (mainThread->iterValue[iterIdx] - bestValue)) / 825.0;
           fallingEval = std::clamp(fallingEval, 0.5, 1.5);
 
           // If the bestMove is stable over several iterations, reduce time accordingly

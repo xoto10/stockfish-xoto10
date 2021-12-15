03238c97d43aa4bd98816c6380fb7bc14377eb6a
xoto10 <buylow001@gmail.com>
2021-12-15 14:11:40 +0000

    Tuned scaling values (adjusted), 1040/9/22. Bench 4614549

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1091,9 +1091,9 @@ Value Eval::evaluate(const Position& pos) {
       v = Evaluation<NO_TRACE>(pos).value();          // classical
   else
   {
-       int scale = 1045
+       int scale = 1040
                    +  9 * pos.count<PAWN>()
-                   + 21 * pos.non_pawn_material() / 1024;
+                   + 22 * pos.non_pawn_material() / 1024;
 
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
        Color stm      = pos.side_to_move();

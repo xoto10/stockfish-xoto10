8fea528d0feb28094f8541f0185ff2692b212795
Your Name <you@example.com>
2022-03-31 16:45:19 +0100

    Larger complexity, 53+, *41. Bench 7342699

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1102,7 +1102,7 @@ Value Eval::evaluate(const Position& pos) {
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
-       int complexity = 45 + abs(nnue - psq) * 41 / 256;
+       int complexity = 53 + abs(nnue - psq) * 41 / 256;
 
        optimism = optimism * complexity / 32;
        v = (nnue + optimism) * scale / 1024 - optimism;

d9e8194203829fc264eb81fc47219a5219495331
Your Name <you@example.com>
2022-04-07 20:30:52 +0100

    More aggressive optimism, 100. Bench 6780821

--- src/search.cpp
+++ src/search.cpp
@@ -358,7 +358,7 @@ void Thread::search() {
               trend = (us == WHITE ?  make_score(tr, tr / 2)
                                    : -make_score(tr, tr / 2));
 
-              int opt = sigmoid(prev, 8, 17, 144, 13966, 183);
+              int opt = sigmoid(prev, 8, 17, 100, 13966, 183);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }

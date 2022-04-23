d1e3e713488af1e2db88af0c45c78fcd4794483d
xoto10 <you@example.com>
2022-04-23 23:18:57 +0100

    Reduce totbmc when small, -0.4..+0.1. Bench 7729968

--- src/search.cpp
+++ src/search.cpp
@@ -466,6 +466,11 @@ void Thread::search() {
           // If the bestMove is stable over several iterations, reduce time accordingly
           timeReduction = lastBestMoveDepth + 10 < completedDepth ? 1.63 : 0.73;
           double reduction = (1.56 + mainThread->previousTimeReduction) / (2.20 * timeReduction);
+          if (totBestMoveChanges < 0.1)
+          {
+              totBestMoveChanges -= 0.01 / (totBestMoveChanges + 0.0207) + 0.083;
+//sync_cout << "info string bmc " << totBestMoveChanges << " bmc2 " << totBestMoveChanges2 << sync_endl;
+          }
           double bestMoveInstability = 1.073 + std::max(1.0, 2.25 - 9.9 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();

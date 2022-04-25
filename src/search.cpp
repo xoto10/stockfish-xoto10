e7a67e12b40ac00055885f30c0ef623f5576c65f
xoto10 <you@example.com>
2022-04-25 14:07:22 +0100

    Reduce totbmc when small, -0.2..+0.1, .00105, 0.1. Bench 7729968

--- src/search.cpp
+++ src/search.cpp
@@ -467,8 +467,8 @@ void Thread::search() {
           timeReduction = lastBestMoveDepth + 10 < completedDepth ? 1.63 : 0.73;
           double reduction = (1.56 + mainThread->previousTimeReduction) / (2.20 * timeReduction);
           if (totBestMoveChanges < 0.1)
-              totBestMoveChanges -= 0.01 / (totBestMoveChanges + 0.0207) + 0.083;
-          double bestMoveInstability = 1.323 + std::max(1.0, 2.25 - 9.9 / rootDepth)
+              totBestMoveChanges -= 0.00105 / (totBestMoveChanges + 0.005) + 0.01;
+          double bestMoveInstability = 1.173 + std::max(1.0, 2.25 - 9.9 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);

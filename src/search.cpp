63360ed96dd240492228e99be1126e285ac1934c
xoto10 <you@example.com>
2022-04-27 20:31:59 +0100

    sqrt totbmc, 4.0*sq-3.03. Test vs sg. Bench 7729968

--- src/search.cpp
+++ src/search.cpp
@@ -466,7 +466,7 @@ void Thread::search() {
           // If the bestMove is stable over several iterations, reduce time accordingly
           timeReduction = lastBestMoveDepth + 10 < completedDepth ? 1.63 : 0.73;
           double reduction = (1.56 + mainThread->previousTimeReduction) / (2.20 * timeReduction);
-          double bestMoveInstability = 4.0 * std::pow(1.0 + totBestMoveChanges / Threads.size(), 0.5) - 3.2;
+          double bestMoveInstability = 4.0 * std::pow(1.0 + totBestMoveChanges / Threads.size(), 0.5) - 3.03;
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);
 

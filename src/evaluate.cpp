0242e38b460aac5684131ded7db319dd2842d442
xoto10 <you@example.com>
2022-05-10 19:36:40 +0100

    Manual changes from tuning tests. Bench 6731650

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1099,14 +1099,14 @@ Value Eval::evaluate(const Position& pos) {
   if (useNNUE && !useClassical)
   {
        Value nnue     = NNUE::evaluate(pos, true);     // NNUE
-       int scale      = 1044 + 87 * pos.non_pawn_material() / 4096;
+       int scale      = 1048 + 85 * pos.non_pawn_material() / 4096;
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
-       int complexity = 36 * abs(nnue - psq) / 256;
+       int complexity = 150 * abs(nnue - psq) / 1024;
 
-       optimism = optimism * (40 + complexity) / 32;
-       v = (nnue * scale + optimism * (scale - 964)) / 1024;
+       optimism = optimism * (42 + complexity) / 32;
+       v = (nnue * scale + optimism * (scale - 936)) / 1024;
 
        if (pos.is_chess960())
            v += fix_FRC(pos);

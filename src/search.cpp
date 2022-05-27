f66e8b9a3b85289973521d098e6d8d0d84c40965
xoto10 <you@example.com>
2022-05-27 16:41:09 +0100

    More futility pruning, 160. Bench 6395716

--- src/search.cpp
+++ src/search.cpp
@@ -63,7 +63,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool improving) {
-    return Value(168 * (d - improving));
+    return Value(160 * (d - improving));
   }
 
   // Reductions lookup table, initialized at startup

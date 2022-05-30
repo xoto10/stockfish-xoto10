ccef3e272ef93ac1a7cb9856dad11cc787becb10
xoto10 <you@example.com>
2022-05-30 02:25:10 +0100

    Tune values (81k). Bench 6726341

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -192,10 +192,6 @@ using namespace Trace;
 
 namespace {
 
-auto f1 = [](int m){return m < 20 ? Range(m - 20, m + 20) : Range(m / 2, m * 3 / 2);};
-int A=1080, B=110, C=139, D=139, E=251, F=852;
-TUNE(SetRange(f1), A, B, C, D, E, F);
-
   // Threshold for lazy and space evaluation
   constexpr Value LazyThreshold1    =  Value(3631);
   constexpr Value LazyThreshold2    =  Value(2084);
@@ -1104,15 +1100,15 @@ Value Eval::evaluate(const Position& pos) {
   if (useNNUE && !useClassical)
   {
        int complexity;
-       int scale      = A    + B  * pos.non_pawn_material() / 5120;
+       int scale      = 983 + 107 * pos.non_pawn_material() / 5120;
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());
        Value nnue     = NNUE::evaluate(pos, true, &complexity);     // NNUE
 
-       complexity = (C   * complexity + D   * abs(nnue - psq)) / 256;
-       optimism = optimism * (E   + complexity) / 256;
-       v = (nnue * scale + optimism * (scale - F  )) / 1024;
+       complexity = (139 * complexity + 140 * abs(nnue - psq)) / 256;
+       optimism = optimism * (255 + complexity) / 256;
+       v = (nnue * scale + optimism * (scale - 848)) / 1024;
 
        if (pos.is_chess960())
            v += fix_FRC(pos);

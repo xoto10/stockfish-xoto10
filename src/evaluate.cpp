cfc441c1bd17b5eac39a04db18528b7267939f23
xoto10 <you@example.com>
2022-05-31 08:51:14 +0100

    Complexity values from tune, raised scale values (2/3) aiming for better ltc performance. Bench 6291213

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1100,7 +1100,7 @@ Value Eval::evaluate(const Position& pos) {
   if (useNNUE && !useClassical)
   {
        int complexity;
-       int scale      = 1016 + 108 * pos.non_pawn_material() / 5120;
+       int scale      = 1048 + 109 * pos.non_pawn_material() / 5120;
        Color stm      = pos.side_to_move();
        Value optimism = pos.this_thread()->optimism[stm];
        Value psq      = (stm == WHITE ? 1 : -1) * eg_value(pos.psq_score());

881810888dc6ce30d824cb3feb4ffb015d08c67e
xoto10 <you@example.com>
2022-06-02 12:57:19 +0100

    Add some extra, randomised futility pruning. Bench 6139503

--- src/search.cpp
+++ src/search.cpp
@@ -793,7 +793,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 8
-        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 256 >= beta
+        &&  eval >= beta + futility_margin(depth, improving) + (ss-1)->statScore / 256 - int(thisThread->nodes & 63)
         &&  eval >= beta
         &&  eval < 26305) // larger than VALUE_KNOWN_WIN, but smaller than TB wins.
         return eval;

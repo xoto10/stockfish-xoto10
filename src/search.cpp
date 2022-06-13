f04f68543e81df1140fa13d295b103b0af70de45
xoto10 <you@example.com>
2022-06-13 02:57:20 +0100

    Retry futdiv tune. Bench 6009558

--- src/search.cpp
+++ src/search.cpp
@@ -58,12 +58,16 @@ using namespace Search;
 
 namespace {
 
+auto f1 = [](int m){return Range(1, m * 2);};
+int A=256, B=256, C=20;
+TUNE(SetRange(f1), A, B, C);
+
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV, Root };
 
   // Futility margin
   Value futility_margin(Depth d, bool improving) {
-    return Value(3088 - 73735 / (30 + d - improving));
+    return Value(A*20 - (B*400) / (C + d - improving));
   }
 
   // Reductions lookup table, initialized at startup

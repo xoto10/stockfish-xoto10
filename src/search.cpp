a38cc055ccb21adb328f11d833e6b4c4da4709e8
xoto10 <you@example.com>
2022-06-19 08:23:57 +0100

    Tune 2 for shallow pruning. Bench 6193531

--- src/search.cpp
+++ src/search.cpp
@@ -152,6 +152,10 @@ namespace {
 
 } // namespace
 
+auto f1 = [](int m){return m < 20 ? Range(m - 20, m + 20) : Range(m / 2, m * 3 / 2);};
+int A=276, B=218, C=3950, D=131, E=140;
+TUNE(SetRange(f1), A, B, C, D, E);
+
 /// Search::init() is called at startup to initialize various lookup tables
 
 void Search::init() {
@@ -1011,12 +1015,12 @@ moves_loop: // When in check, search starts here
                   && !PvNode
                   && lmrDepth < 6
                   && !ss->inCheck
-                  && ss->staticEval + 269 + 179 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
+                  && ss->staticEval + A   + 179 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 6 < alpha)
                   continue;
 
               // SEE based pruning (~9 Elo)
-              if (!pos.see_ge(move, Value(-221) * depth))
+              if (!pos.see_ge(move, Value(-B  ) * depth))
                   continue;
           }
           else
@@ -1027,7 +1031,7 @@ moves_loop: // When in check, search starts here
 
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 5
-                  && history < -4003 * (depth - 1))
+                  && history < -C    * (depth - 1))
                   continue;
 
               history += thisThread->mainHistory[us][from_to(move)];
@@ -1035,11 +1039,11 @@ moves_loop: // When in check, search starts here
               // Futility pruning: parent node (~9 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 11
-                  && ss->staticEval + 132 + 138 * lmrDepth + history / 60 <= alpha)
+                  && ss->staticEval + D   + 138 * lmrDepth + history / 60 <= alpha)
                   continue;
 
               // Prune moves with negative SEE (~3 Elo)
-              if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 20 * lmrDepth - 12)))
+              if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 20 * lmrDepth + E - 150)))
                   continue;
           }
       }

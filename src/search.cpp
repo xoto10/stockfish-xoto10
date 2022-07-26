ec4064c45b1b81dde8dd787d8cbca8d11e1098b4
xoto10 <you@example.com>
2022-07-26 15:49:44 +0100

    Give opp more choice if we're winning, <-40, d>10. Bench 5494982

--- src/search.cpp
+++ src/search.cpp
@@ -569,7 +569,8 @@ namespace {
     ss->inCheck        = pos.checkers();
     priorCapture       = pos.captured_piece();
     Color us           = pos.side_to_move();
-    moveCount          = captureCount = quietCount = ss->moveCount = oppChoices = 0;
+    moveCount          = captureCount = quietCount = ss->moveCount = 0;
+    oppChoices         = -1;
     bestValue          = -VALUE_INFINITE;
     maxValue           = VALUE_INFINITE;
 

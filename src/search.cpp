4c8075e4674c23961cea1a5b379fe0cd83ef8e62
xoto10 <you@example.com>
2022-07-27 23:09:05 +0100

    Give opp more choice if we're winning, <-40, d>5, (oc+1)/2 - 1. Bench 5268970

--- src/search.cpp
+++ src/search.cpp
@@ -1323,8 +1323,8 @@ moves_loop: // When in check, search starts here
     }
 
     // Prefer positions with multiple choices if opponent is losing
-    if (ss->ply == 3 && depth > 5 && bestValue < VALUE_DRAW - 40)
-        bestValue -= oppChoices / 2;
+    if (ss->ply == 1 && depth > 5 && bestValue < VALUE_DRAW - 40)
+        bestValue -= oppChoices / 2 - 1;
 
     // The following condition would detect a stop only after move loop has been
     // completed. But in this case bestValue is valid because we have fully

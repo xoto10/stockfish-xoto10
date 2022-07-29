2bafa9859f241a8a4c31834cd50df39fc4d40c75
xoto10 <you@example.com>
2022-07-29 01:14:22 +0100

    Give opp more choice if we're winning, linear limit 30-6d, d>5, (oc+1)/2-1. Bench 5012952

--- src/search.cpp
+++ src/search.cpp
@@ -152,9 +152,6 @@ namespace {
 
 } // namespace
 
-int A=80, B=8;
-TUNE(SetRange(0,160), A);
-TUNE(SetRange(0,16), B);
 
 /// Search::init() is called at startup to initialize various lookup tables
 
@@ -1326,7 +1323,7 @@ moves_loop: // When in check, search starts here
     }
 
     // Prefer positions with multiple choices if opponent is losing
-    if (ss->ply == 1 && depth > 5 && bestValue < Value(A) - B * thisThread->rootDepth)
+    if (ss->ply == 1 && depth > 5 && bestValue < 30 - 6 * thisThread->rootDepth)
         bestValue -= oppChoices / 2 - 1;
 
     // The following condition would detect a stop only after move loop has been

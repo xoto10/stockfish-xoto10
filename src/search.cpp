74ef5f8384d161296b3ebd7ea0d10bff4ac4adfd
xoto10 <you@example.com>
2022-07-29 14:51:02 +0100

    Give opp more choice if we're winning, linear 0-4d, d>5, (oc+1)/2 - 1. Bench 5327539

--- src/search.cpp
+++ src/search.cpp
@@ -1323,7 +1323,7 @@ moves_loop: // When in check, search starts here
     }
 
     // Prefer positions with multiple choices if opponent is losing
-    if (ss->ply == 1 && depth > 5 && bestValue < 75 - 9 * thisThread->rootDepth)
+    if (ss->ply == 1 && depth > 5 && bestValue < VALUE_ZERO - 4 * thisThread->rootDepth)
         bestValue -= oppChoices / 2 - 1;
 
     // The following condition would detect a stop only after move loop has been

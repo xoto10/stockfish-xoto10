dba37a88e3dea847be81f8c3c76d587ba0f8c527
xoto10 <you@example.com>
2022-08-01 17:41:37 +0100

    Change order of conditions. Prio -1 to invite comments - does this change do something or does the optimiser re-arrange the order of the tests making this pointless? Bench 5820568

--- src/search.cpp
+++ src/search.cpp
@@ -794,8 +794,8 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 8
-        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 256 >= beta
         &&  eval >= beta
+        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 256 >= beta
         &&  eval < 26305) // larger than VALUE_KNOWN_WIN, but smaller than TB wins.
         return eval;
 

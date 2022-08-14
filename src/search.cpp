d76287dbc189908977f3ab484d0276f68fce46e2
xoto10 <you@example.com>
2022-08-14 18:43:22 +0100

    Adjust razor values with pvnode, 4,180,160. Bench 5940937

--- src/search.cpp
+++ src/search.cpp
@@ -776,8 +776,8 @@ namespace {
     // Step 7. Razoring.
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
-    if (   depth <= 7 - 5 * PvNode
-        && eval < alpha - (348 + 170 * PvNode) - (258 + 130 * PvNode) * depth * depth)
+    if (   depth <= 7 - 4 * PvNode
+        && eval < alpha - (348 + 180 * PvNode) - (258 + 160 * PvNode) * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)

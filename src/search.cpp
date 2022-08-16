d1bb4181181b27a73061fb66926d3958f0189e99
xoto10 <you@example.com>
2022-08-16 06:54:05 +0100

    Skip qs in razor, >1000. Bench 6152857

--- src/search.cpp
+++ src/search.cpp
@@ -780,7 +780,7 @@ namespace {
         && depth <= 7
         && alpha - 348 - 258 * depth * depth - eval > 0)
     {
-        if (alpha - 348 - 258 * depth * depth - eval > 750)
+        if (alpha - 348 - 258 * depth * depth - eval > 1000)
             return eval;
 
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);

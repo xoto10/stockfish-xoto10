aaea98ea42f296aeacdc38a12568fbca4d24192e
xoto10 <you@example.com>
2022-08-19 23:17:21 +0100

    Adjust qs offset, -1. Bench 6079565

--- src/search.cpp
+++ src/search.cpp
@@ -1532,7 +1532,7 @@ moves_loop: // When in check, search starts here
 
       // Do not search moves with negative SEE values (~5 Elo)
       if (    bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && !pos.see_ge(move, VALUE_ZERO + 4))
+          && !pos.see_ge(move, VALUE_ZERO - 1))
           continue;
 
       // Speculative prefetch as early as possible

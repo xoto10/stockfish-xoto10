9281befd0a6a875316b5d961c0339297cf33b6e4
xoto10 <you@example.com>
2022-08-24 23:51:32 +0100

    Randomise step 8, +/-84 * (8-d)/7. Bench 5793473

--- src/search.cpp
+++ src/search.cpp
@@ -789,7 +789,10 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 8
-        &&  eval >= beta + futility_margin(depth, improving) + (ss-1)->statScore / 256 + 24 * int(thisThread->nodes & 7) - 84
+        &&  eval >= beta
+                    + futility_margin(depth, improving)
+                    + (ss-1)->statScore / 256
+                    + (24 * int(thisThread->nodes & 7) - 84) * (8 - depth) / 7
         &&  eval >= beta
         &&  eval < 26305) // larger than VALUE_KNOWN_WIN, but smaller than TB wins.
         return eval;

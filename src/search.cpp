18e59bc74c59c71acfa3b8a4da6431d6e28779b7
xoto10 <you@example.com>
2022-08-26 17:12:17 +0100

    Step 8, beta + 8, 166. Bench 5713171

--- src/search.cpp
+++ src/search.cpp
@@ -63,7 +63,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool improving) {
-    return Value(167 * (d - improving));
+    return Value(166 * (d - improving));
   }
 
   // Reductions lookup table, initialized at startup

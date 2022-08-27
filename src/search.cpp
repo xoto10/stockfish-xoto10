6dc46341f2c3c77d5ef6e3f0c75b3c3e756f99c3
xoto10 <you@example.com>
2022-08-27 22:08:24 +0100

    More see, +40. Bench 5555455

--- src/search.cpp
+++ src/search.cpp
@@ -1014,7 +1014,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning (~9 Elo)
-              if (!pos.see_ge(move, Value(-203) * depth + 120))
+              if (!pos.see_ge(move, Value(-203) * depth + 40))
                   continue;
           }
           else

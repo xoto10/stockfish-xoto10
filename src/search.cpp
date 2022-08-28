01784c6edac22ec8064df82b557f248d48f6f884
xoto10 <you@example.com>
2022-08-28 18:44:34 +0100

    More razor if improving, 100*. Bench 5287116

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
     // return a fail low.
     if (   !PvNode
         && depth <= 7
-        && eval < alpha + 600 * improving - 348 - 258 * depth * depth)
+        && eval < alpha + 100 * improving - 348 - 258 * depth * depth)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha)

373c89e93c987dc62220855160c9a4b52c8b4b16
xoto10 <you@example.com>
2022-09-06 17:01:48 +0100

    120k values + adj, 203,-20,17,-2,2. Bench 5619468

--- src/search.cpp
+++ src/search.cpp
@@ -1014,7 +1014,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning (~9 Elo)
-              if (!pos.see_ge(move, Value(-203) * depth - 20 + 9 * improving))
+              if (!pos.see_ge(move, Value(-203) * depth - 20 + 17 * improving - 2 * moveCount + 2 * delta))
                   continue;
           }
           else

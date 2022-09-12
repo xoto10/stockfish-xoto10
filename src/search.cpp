76e8abba039573b5c78e7edbaf6ea641e27b62d4
xoto10 <you@example.com>
2022-09-12 17:51:44 +0100

    Lower values for eval and staticEval, 83. Bench 4743400

--- src/search.cpp
+++ src/search.cpp
@@ -1098,7 +1098,7 @@ moves_loop: // When in check, search starts here
           // Check extensions (~1 Elo)
           else if (   givesCheck
                    && depth > 9
-                   && (abs(ss->staticEval) > 89 || abs(eval) > 87))
+                   && (abs(ss->staticEval) > 83 || abs(eval) > 83))
               extension = 1;
 
           // Quiet ttMove extensions (~0 Elo)

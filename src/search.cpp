a89d3a647e1425c9f7b7c0e925735f46b05328e8
xoto10 <you@example.com>
2022-09-14 14:03:43 +0100

    Tune eval in reduction adj. Bench 4085130

--- src/search.cpp
+++ src/search.cpp
@@ -152,6 +152,9 @@ namespace {
 
 } // namespace
 
+auto f1 = [](int m){return Range(m / 2, m * 3 / 2);};
+int A=1280, B=14728;
+TUNE(SetRange(f1), A, B);
 
 /// Search::init() is called at startup to initialize various lookup tables
 
@@ -1175,7 +1178,7 @@ moves_loop: // When in check, search starts here
                          - 4433;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= (ss->statScore + 5 * eval ) / 14728;
+          r -= (ss->statScore + A    * eval / 256 ) / B    ;
 
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension

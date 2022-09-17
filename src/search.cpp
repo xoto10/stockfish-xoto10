74ed7b4be857916758db8f408d5950b4e2c5824f
xoto10 <you@example.com>
2022-09-17 06:19:55 +0100

    alpha in reduction adj, 6*a/16148. Bench 4227847

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop: // When in check, search starts here
                          - 4433;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= (ss->statScore + 2 * alpha) / 14468;
+          r -= (ss->statScore + 6 * alpha) / 16148;
 
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension

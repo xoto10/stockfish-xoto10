8775d204d2f3cef2c8d64576a22702d24241d7ca
xoto10 <you@example.com>
2022-09-23 09:17:17 +0100

    Less qsearch in razoring, dep>1. Bench 4512611

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
     if (   depth <= 7
         && eval < alpha - 369 - 254 * depth * depth)
     {
-        if (eval == ttValue && tte->depth() < depth)
+        if (eval == ttValue && depth > 1)
             value = eval;
         else
             value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);

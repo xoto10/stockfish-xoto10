f4c5d2764a748876a8f95432d8317c876bce65b4
xoto10 <you@example.com>
2022-10-02 22:19:12 +0100

    Prefer draws with alpha >= 0. Bench 4122086

--- src/search.cpp
+++ src/search.cpp
@@ -85,8 +85,8 @@ namespace {
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
-  Value value_draw(const Thread* thisThread) {
-    return VALUE_DRAW - 1 + Value(thisThread->nodes & 0x2);
+  Value value_draw(const Value v, const Thread* thisThread) {
+    return VALUE_DRAW - 1 + Value(thisThread->nodes & 0x2) + (v >= VALUE_ZERO);
   }
 
   // Skill structure is used to implement strength limit. If we have an uci_elo then
@@ -534,7 +534,7 @@ namespace {
         && alpha < VALUE_DRAW
         && pos.has_game_cycle(ss->ply))
     {
-        alpha = value_draw(pos.this_thread());
+        alpha = value_draw(alpha, pos.this_thread());
         if (alpha >= beta)
             return alpha;
     }
@@ -586,7 +586,7 @@ namespace {
             || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
             return (ss->ply >= MAX_PLY && !ss->inCheck) ? evaluate(pos)
-                                                        : value_draw(pos.this_thread());
+                                                        : value_draw(alpha, pos.this_thread());
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply+1), but if alpha is already bigger because

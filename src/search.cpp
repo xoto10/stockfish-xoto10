8131ef3c4cb717dda7dac8a4b99bd0461f656cf9
xoto10 <you@example.com>
2022-10-06 12:02:42 +0100

    Mix alpha+beta and statScore for reduction, 3*. Bench 4962195

--- src/search.cpp
+++ src/search.cpp
@@ -1179,7 +1179,7 @@ moves_loop: // When in check, search starts here
                          - 4433;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-          r -= (ss->statScore + 5 * (alpha + beta) / 2) / 15448;
+          r -= (ss->statScore + 3 * (alpha + beta) / 2) / 15448;
 
           // In general we want to cap the LMR depth search at newDepth, but when
           // reduction is negative, we allow this move a limited search extension

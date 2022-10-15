1b6bd9249a13e78316a2377a97340fc9477cac3c
xoto10 <you@example.com>
2022-10-16 00:14:22 +0100

    Tuned +/- optimism values, 60k. Against sn patch. Bench 4392265

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -192,12 +192,6 @@ using namespace Trace;
 
 namespace {
 
-auto f1 = [](int m){return Range(m / 2, m * 3 / 2);};
-auto f2 = [](int m){return Range(m - 100, m + 100);};
-int A=416, B=416, C=424, D=424, E=0, F=0, G=0, H=269, I=754;
-TUNE(SetRange(f1), A, B, C, D, H, I);
-TUNE(SetRange(f2), E, F, G);
-
   // Threshold for lazy and space evaluation
   constexpr Value LazyThreshold1    =  Value(3631);
   constexpr Value LazyThreshold2    =  Value(2084);
@@ -1077,18 +1071,18 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
       // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  (optimism  > 0 ? A * nnueComplexity : B * nnueComplexity)                // nnue complexity
-                        + (optimism  > 0 ? F * int(optimism) : G * int(optimism))                  // optimism
-                        + (optimism  > 0 ? C * abs(psq - nnue) : D * abs(psq - nnue))              // pos OR mat adv
-                        + (optimism  > 0 ? int(optimism) * int(psq - nnue) : E * int(nnue - psq))  // pos if winning
+      nnueComplexity = (  (optimism  > 0 ? 420 * nnueComplexity : 437 * nnueComplexity)                // nnue complexity
+                        + (optimism  > 0 ? 0 : -6 * int(optimism))                  // optimism
+                        + (optimism  > 0 ? 407 * abs(psq - nnue) : 398 * abs(psq - nnue))              // pos OR mat adv
+                        + (optimism  > 0 ? int(optimism) * int(psq - nnue) : 10 * int(nnue - psq))  // pos if winning
                         ) / 1024;
 
       // Return hybrid NNUE complexity to caller
       if (complexity)
           *complexity = nnueComplexity;
 
-      optimism = optimism * (H   + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - I  )) / 1024;
+      optimism = optimism * (259 + nnueComplexity) / 256;
+      v = (nnue * scale + optimism * (scale - 794)) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling

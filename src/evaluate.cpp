732b75e117f32d583d8c231bf4e6d2516c305c09
xoto10 <you@example.com>
2022-10-20 08:32:21 +0100

    60k tune with extra pessimism. Bench 4496424

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1072,9 +1072,9 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
 
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  (optimism  > 0 ? 420 * nnueComplexity : 437 * nnueComplexity)             // nnue complexity
-                        + (optimism  > 0 ? 0 : -6 * int(optimism))                                  // optimism
+                        + (optimism  > 0 ? 0 : -12 * int(optimism))                                 // optimism
                         + (optimism  > 0 ? 407 * abs(psq - nnue) : 398 * abs(psq - nnue))           // pos OR mat adv
-                        + (optimism  > 0 ? int(optimism) * int(psq - nnue) : 20 * int(nnue - psq))  // mat if winning
+                        + (optimism  > 0 ? int(optimism) * int(psq - nnue) : 10 * int(nnue - psq))  // mat if winning
                         ) / 1024;
 
       // Return hybrid NNUE complexity to caller

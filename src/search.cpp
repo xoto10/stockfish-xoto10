8b4f1ed27820a49037226a94ebfe7bd682e76fa5
xoto10 <you@example.com>
2022-11-08 08:36:23 +0000

    Quick tune of optimism shifts. Bench 4239512

--- src/search.cpp
+++ src/search.cpp
@@ -152,6 +152,9 @@ namespace {
 
 } // namespace
 
+auto f1 = [](int m){return Range(m - 200, m + 200);};
+int A=0, B=0;
+TUNE(SetRange(f1), A, B);
 
 /// Search::init() is called at startup to initialize various lookup tables
 
@@ -356,7 +359,7 @@ void Thread::search() {
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
               // Adjust optimism based on root move's previousScore
-              int opt = 118 * prev / (std::abs(prev) + 169);
+              int opt = 118 * (prev + A) / (std::abs(prev + A) + 169) + B;
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }

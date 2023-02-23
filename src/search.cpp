515e63d0ce874f502ca3a68c54d400ff19322cfc
xoto10 <you@example.com>
2023-02-23 12:31:34 +0000

    More optimism, 120. Bench 3905983

--- src/search.cpp
+++ src/search.cpp
@@ -359,7 +359,7 @@ void Thread::search() {
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
               // Adjust optimism based on root move's previousScore
-              int opt = 112 * prev / (std::abs(prev) + 170);
+              int opt = 120 * prev / (std::abs(prev) + 170);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }

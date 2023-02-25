e3d0232f762923f9acbf5770f71631196fbdf2bc
xoto10 <you@example.com>
2023-02-25 09:16:54 +0000

    Randomise optimism, +/-8, 4-6. Bench 4674480

--- src/search.cpp
+++ src/search.cpp
@@ -359,7 +359,7 @@ void Thread::search() {
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
               // Adjust optimism based on root move's previousScore
-              int opt = 120 * prev / (std::abs(prev) + 161);
+              int opt = (112 + int(rootPos.this_thread()->nodes & 120)/8) * prev / (std::abs(prev) + 161);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }

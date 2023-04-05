7b22ba15e891923100152e8dcbff81def99eb96a
xoto10 <you@example.com>
2023-04-05 18:39:01 +0100

    Values from stc tune, 80k, -2,70,115,165. Bench 4832241

--- src/search.cpp
+++ src/search.cpp
@@ -365,7 +365,7 @@ void Thread::search() {
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
               // Adjust optimism based on root move's previousScore
-              int opt = (prev < -21 ? 75 : 126) * prev / (std::abs(prev) + 173);
+              int opt = (prev < -2 ? 70 : 115) * prev / (std::abs(prev) + 165);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }

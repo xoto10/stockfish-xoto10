f0fc67a5e9d82735bb124e7b7b241f9471da6693
xoto10 <you@example.com>
2023-04-06 17:52:27 +0100

    From sg: zero opt if prev < 0. Bench 5194465

--- src/search.cpp
+++ src/search.cpp
@@ -363,7 +363,7 @@ void Thread::search() {
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
               // Adjust optimism based on root move's previousScore
-              int opt = 120 * prev / (std::abs(prev) + 161);
+              int opt = 120 * std::max(0, int(prev)) / (std::abs(prev) + 161);
               optimism[ us] = Value(opt);
               optimism[~us] = -optimism[us];
           }

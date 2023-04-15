1048f812ff1d82e21e0a9e4bc1fbf73d90efd122
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-04-15 20:52:38 +0800

    Simplify away nnueComplexity as well? Bench: 3647711

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1062,20 +1062,14 @@ Value Eval::evaluate(const Position& pos) {
       v = Evaluation<NO_TRACE>(pos).value();
   else
   {
-      int nnueComplexity;
       int scale = 1001 + pos.non_pawn_material() / 64;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
 
-      Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
+      Value nnue = NNUE::evaluate(pos, true);
 
-      // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  406 * nnueComplexity
-                        + (424 + optimism) * abs(psq - nnue)
-                        ) / 1024;
-
-      optimism = optimism * (272 + nnueComplexity) / 256;
+      optimism = optimism * (272 + (424 + optimism) * abs(psq - nnue) / 1024) / 256;
       v = (nnue * scale + optimism * (scale - 748)) / 1024;
   }
 

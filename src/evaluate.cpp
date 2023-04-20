d781ac7febcc8d4902486dc4e2fa0c7a2f933cbe
xoto10 <you@example.com>
2023-04-20 14:01:38 +0100

    Remove all scaling from optimism. Test against PR #4530. Bench 3880520

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1070,13 +1070,7 @@ Value Eval::evaluate(const Position& pos) {
 
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
-      // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  406 * nnueComplexity
-                        + (424 + optimism) * abs(psq - nnue)
-                        ) / 1024;
-
-      optimism = optimism * (272 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 748)) / 1024;
+      v = (nnue + optimism) * scale / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling

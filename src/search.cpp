74249cbb2564092505a795c3e382dceee2a7f334
xoto10 <23479932+xoto10@users.noreply.github.com>
2023-04-26 15:02:40 +0100

    Lower optimism, vs wide+narrow 32k. Bench 3837582

--- src/search.cpp
+++ src/search.cpp
@@ -353,7 +353,7 @@ void Thread::search() {
           beta  = std::min(prev + delta, VALUE_INFINITE);
 
           // Adjust optimism based on root move's previousScore
-          int opt = 120 * prev / (std::abs(prev) + 161);
+          int opt = 100 * prev / (std::abs(prev) + 161);
           optimism[ us] = Value(opt);
           optimism[~us] = -optimism[us];
 

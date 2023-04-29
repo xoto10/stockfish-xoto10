485fe217fa8ec448f3b855a7812d2b56cfd3216a
xoto10 <23479932+xoto10@users.noreply.github.com>
2023-04-29 12:26:28 +0100

    Vary futility_margin across threads. Bench 3714847

--- src/search.cpp
+++ src/search.cpp
@@ -772,7 +772,8 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 9
-        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 280 >= beta
+        &&  eval >= beta + futility_margin(depth, improving) + (ss-1)->statScore / 280
+                    + (int(thisThread->nodes & 0x3) - 2) * int(thisThread->id() & 0x7)
         &&  eval >= beta
         &&  eval < 25128) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;

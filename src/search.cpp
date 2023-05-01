4ee43c1b9b60cfcb6397b584da84d59bff0063fa
xoto10 <23479932+xoto10@users.noreply.github.com>
2023-05-01 15:41:32 +0100

    Less statscore and more fut margin, *204, 170*. Bench 3908833

--- src/search.cpp
+++ src/search.cpp
@@ -64,7 +64,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool improving) {
-    return Value(154 * (d - improving));
+    return Value(170 * (d - improving));
   }
 
   // Reductions lookup table, initialized at startup

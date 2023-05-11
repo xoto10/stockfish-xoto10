49049c1036d8a9f2cf62a233ee2a0fafa4b2a421
xoto10 <23479932+xoto10@users.noreply.github.com>
2023-05-11 21:05:13 +0100

    Retry removal of (psq - nnue) in complexity. Bench 3462311

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1062,21 +1062,15 @@ Value Eval::evaluate(const Position& pos) {
       v = Evaluation<NO_TRACE>(pos).value();
   else
   {
+      Color stm = pos.side_to_move();
       int nnueComplexity;
       int scale = 967 + pos.non_pawn_material() / 64;
 
-      Color stm = pos.side_to_move();
-      Value optimism = pos.this_thread()->optimism[stm];
-
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
+      Value optimism = pos.this_thread()->optimism[stm]
+                       * (1220000 + 11 * nnueComplexity * (scale - 860));
 
-      // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  402 * nnueComplexity
-                        + (454 + optimism) * abs(psq - nnue)
-                        ) / 1024;
-
-      optimism = optimism * (274 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 791)) / 1024;
+      v = nnue * scale / 1024 + optimism / (4096*1024);
   }
 
   // Damp down the evaluation linearly when shuffling

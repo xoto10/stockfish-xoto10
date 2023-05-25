072986bb0706ecef554ad18890bd786d4f2f3e78
xoto10 <23479932+xoto10@users.noreply.github.com>
2023-05-25 15:29:56 +0100

    Larger window increase when failing high, +1. Bench 3030892

--- src/search.cpp
+++ src/search.cpp
@@ -409,7 +409,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 4 + 2 + failedHighCnt;
+              delta += delta / 4 + 2 + !!failedHighCnt;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }

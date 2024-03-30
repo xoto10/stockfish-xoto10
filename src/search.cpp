0a09665edad68f484e674a42f9c91010e02b4bc2
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-03-30 15:53:33 +0000

    Tune with more bands, wider range and other tm vars. Bench 1759189

--- src/search.cpp
+++ src/search.cpp
@@ -55,11 +55,12 @@ namespace {
 
 //static constexpr double EvalLevel[10] = {1.043, 1.017, 0.952, 1.009, 0.971,
 //                                         1.002, 0.992, 0.947, 1.046, 1.001};
-//double evalGroup[10] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
 auto f1 = [](int m){return m < 20 ? Range(m - 20, m + 20) : Range(m / 2, m * 3 / 2);};
-int ET[36] = {1043, 1043, 1043,  1043, 1043, 1034,  1026, 1017,  996,  974,  952,  971,
-               990, 1009,  996,   984,  971,  981,   992, 1002,  999,  995,  992,  977,
-               962,  947,  980,  1013, 1046, 1031,  1016, 1001, 1001, 1001, 1001, 1001};
+int A=1067, B=223, C= 97, D=580, E=1667, F=1495, G=687, H=148, I=217, J=188;
+int ET[36] = {1058, 1040, 1058,  1049, 1021, 1037,  1037, 1024, 1000,  971,  957,  974,
+               991, 1020, 1012,   989,  973,  979,   985, 1013, 1005, 1000, 1010,  962,
+               962,  937,  981,  1027, 1053, 1036,  1018, 1015,  995,  997, 1002, 1002};
+TUNE(SetRange(f1), A, B, C, D, E, F, G, H, I, J);
 TUNE(SetRange(f1), ET);
 
 // Futility margin
@@ -438,15 +439,15 @@ void Search::Worker::iterative_deepening() {
         {
             int nodesEffort = rootMoves[0].effort * 100 / std::max(size_t(1), size_t(nodes));
 
-            double fallingEval = (1067 + 223 * (mainThread->bestPreviousAverageScore - bestValue)
-                                  + 97 * (mainThread->iterValue[iterIdx] - bestValue))
+            double fallingEval = (   A +   B * (mainThread->bestPreviousAverageScore - bestValue)
+                                  +  C * (mainThread->iterValue[iterIdx] - bestValue))
                                / 10000.0;
-            fallingEval = std::clamp(fallingEval, 0.580, 1.667);
+            fallingEval = std::clamp(fallingEval, D/1000.0, E/1000.0);
 
             // If the bestMove is stable over several iterations, reduce time accordingly
-            timeReduction    = lastBestMoveDepth + 8 < completedDepth ? 1.495 : 0.687;
-            double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
-            double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
+            timeReduction    = lastBestMoveDepth + 8 < completedDepth ? F/1000.0 : G/1000.0;
+            double reduction = (H/100.0 + mainThread->previousTimeReduction) / (I/100.0 * timeReduction);
+            double bestMoveInstability = 1 + J/100.0 * totBestMoveChanges / threads.size();
             int    el                  = std::clamp((bestValue + 900) / 50, 0, 35);
 //sync_cout << "info bv " << bestValue << " ET[el] " << ET[el] << sync_endl;
 

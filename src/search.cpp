673d9fc3da8d49e12dfe6a1544317b5d0835ae0c
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-04-20 20:12:01 +0100

    Tune use unsaved time at 30+0.3, from 0.05,0.94. Bench 1479416

--- src/search.cpp
+++ src/search.cpp
@@ -52,6 +52,11 @@ using namespace Search;
 
 namespace {
 
+auto f1 = [](int m){return Range(m / 2, m * 3 / 2);};
+int A=100, B=94;
+TUNE(A);
+TUNE(SetRange(f1), B);
+
 static constexpr double EvalLevel[10] = {1.043, 1.017, 0.952, 1.009, 0.971,
                                          1.002, 0.992, 0.947, 1.046, 1.001};
 
@@ -439,7 +444,7 @@ void Search::Worker::iterative_deepening() {
             double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
             double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
             int    el                  = std::clamp((bestValue + 750) / 150, 0, 9);
-            int    extra               = std::max(0, int(main_manager()->extraTime * 13 / 256));
+            int    extra               = std::max(0, int(main_manager()->extraTime * A / 2000));
             main_manager()->extraTime -= extra;
 
             double totalTime = mainThread->tm.optimum() * fallingEval * reduction
@@ -470,7 +475,7 @@ void Search::Worker::iterative_deepening() {
                 threads.increaseDepth = mainThread->ponder || elapsedTime <= totalTime * 0.506;
 
             if (threads.stop)
-                main_manager()->extraTime += 0.94 * mainThread->tm.optimum()
+                main_manager()->extraTime += B/100.0 * mainThread->tm.optimum()
                                              - mainThread->tm.elapsed(threads.nodes_searched());
         }
 

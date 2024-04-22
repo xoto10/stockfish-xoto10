d0ceb66cb48648cba06d6d82ab6f3db4e3aa7642
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-04-22 19:59:47 +0100

    Avoid reduction quick move if there is extra time. Bench 1479416

--- src/search.cpp
+++ src/search.cpp
@@ -442,13 +442,23 @@ void Search::Worker::iterative_deepening() {
             fallingEval = std::clamp(fallingEval, 0.580, 1.667);
 
             // If the bestMove is stable over several iterations, reduce time accordingly
-            timeReduction    = lastBestMoveDepth + 8 < completedDepth ? 1.495 : 0.687;
-            double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
+            TimePoint optimum = mainThread->tm.optimum();
+            double extra = 1.0;
+
+            timeReduction = lastBestMoveDepth + 8 < completedDepth ? 1.495 : 0.687;
+            // Avoid very quick moves if extraTime has been accumulated
+            if (timeReduction > mainThread->previousTimeReduction && main_manager()->extraTime > 0)
+                // constants copied from timeReduction and reduction assignments above and below
+                extra = (1.48 + 1.495) / (1.48 + 0.687); // ~1.37
+            double reduction  = (1.48 + (extra > 1.0 ? timeReduction : mainThread->previousTimeReduction))
+                                / (2.17 * timeReduction);
             double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
             int    el                  = std::clamp((bestValue + 750) / 150, 0, 9);
 
-            double totalTime = mainThread->tm.optimum() * fallingEval * reduction
-                             * bestMoveInstability * EvalLevel[el];
+            double totalTime = optimum * fallingEval * reduction * bestMoveInstability * EvalLevel[el];
+
+            main_manager()->extraTime -= (extra - 1.0) * totalTime;
+            totalTime *= extra;
 
             // Cap used time in case of a single legal move for a better viewer experience
             if (rootMoves.size() == 1)
@@ -472,6 +482,9 @@ void Search::Worker::iterative_deepening() {
             }
             else
                 threads.increaseDepth = mainThread->ponder || elapsedTime <= totalTime * 0.506;
+
+            if (threads.stop)
+                main_manager()->extraTime += 0.96 * optimum - elapsedTime;
         }
 
         mainThread->iterValue[iterIdx] = bestValue;

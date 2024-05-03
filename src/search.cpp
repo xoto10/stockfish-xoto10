4e248fd8fa8324bbb6582f3e53c38dde5d6f063f
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-03 17:55:11 +0100

    More time if avg low, faster bugfix, bv>0,<0.7,*1.1,4/1. Bench 1480008

--- src/search.cpp
+++ src/search.cpp
@@ -484,7 +484,7 @@ void Search::Worker::iterative_deepening() {
         return;
 
     mainThread->previousTimeReduction = timeReduction;
-    mainThread->timeMultAvg           = (40 * mainThread->timeMultAvg + 10 * timeMult) / 100.0;
+    mainThread->timeMultAvg           = (40 * mainThread->timeMultAvg + 10 * timeMult) / 50.0;
 //sync_cout << "info tmu " << timeMult << sync_endl;
 //sync_cout << "info tma " << mainThread->timeMultAvg << sync_endl;
 

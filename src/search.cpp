d097d57f08e7d31270569d160c0cf62b1ca77229
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-04 14:34:37 +0100

    Tuned values at 60+0.6, 192k games, removed recapture code. Bench 1480008

--- src/search.cpp
+++ src/search.cpp
@@ -443,10 +443,9 @@ void Search::Worker::iterative_deepening() {
             double reduction = (1.49 + mainThread->previousTimeReduction) / (2.03 * timeReduction);
             double bestMoveInstability = 1 + 1.97 * totBestMoveChanges / threads.size();
             int    el                  = std::clamp((bestValue + 750) / 150, 0, 9);
-//          double recapture           = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.972 : 0.962;
 
             double totalTime = mainThread->tm.optimum() * fallingEval * reduction
-                             * bestMoveInstability * EvalLevel[el];// * recapture;
+                             * bestMoveInstability * EvalLevel[el];
 
             // Cap used time in case of a single legal move for a better viewer experience
             if (rootMoves.size() == 1)

db3c95e6e672041ce9269b6fc20814ab669e02ed
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-06 15:05:01 +0100

    Use 50% originalTime, bugfix, /2, assign and simplify. Bench 2180675

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -101,9 +101,8 @@ void TimeManagement::init(Search::LimitsType& limits,
         double optExtra = limits.inc[us] < 500 ? 1.0 : 1.13;
 
         // Calculate time constants based on current time left.
-        double optConstant =
-          std::min(0.00225 + 0.000811 * std::log10((originalTime + limits.time[us]) / 4000.0), 0.00506);
-        double maxConstant = std::max(3.39 + 3.01 * std::log10(limits.time[us] / 1000.0), 2.93);
+        double optConstant = std::min(0.000811 * std::log10(0.1487 * (originalTime + limits.time[us])), 0.00506);
+        double maxConstant = std::max(3.01 * std::log10(13.37 * limits.time[us]), 2.93);
 
         optScale = std::min(0.0122 + std::pow(ply + 2.95, 0.462) * optConstant,
                             0.213 * limits.time[us] / double(timeLeft))

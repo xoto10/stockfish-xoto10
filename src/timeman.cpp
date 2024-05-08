1ca85b4c3bf75a41cedb94c9bc3085fc391c23de
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-09 00:02:40 +0100

    Try some last things, 280,548k. Bench 2180675

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -104,8 +104,9 @@ void TimeManagement::init(Search::LimitsType& limits,
         double optExtra = limits.inc[us] < 500 ? 1.0 : 1.13;
 
         // Calculate time constants based on current time left.
-        double optConstant = std::min(0.000960 * std::log10(0.07700 * (originalTime + limits.time[us])), 0.00506);
+        double optConstant = std::min(0.000280 * std::log10(548000.0 * (originalTime + limits.time[us])), 0.00506);
         double maxConstant = std::max(3.01 * std::log10(13.37 * limits.time[us]), 2.93);
+sync_cout << "info optConstant " << optConstant << " origTime " << originalTime << sync_endl;
 
         optScale = std::min(0.0122 + std::pow(ply + 2.95, 0.462) * optConstant,
                             0.213 * limits.time[us] / double(timeLeft))

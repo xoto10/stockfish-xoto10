f24d2a8c43ca514738e1ca2a09cedf7c88e06cd4
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-09 19:07:34 +0100

    More time on first move, 1.3. Bench 2353057

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -28,7 +28,7 @@
 
 namespace Stockfish {
 
-const static double plyExtra[] = { 5.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+const static double plyExtra[] = { 1.3, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
 
 TimePoint TimeManagement::optimum() const { return optimumTime; }
 TimePoint TimeManagement::maximum() const { return maximumTime; }

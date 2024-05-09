ad86473dbef031d9fb06979222328fe195ecdf75
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-09 01:02:05 +0100

    Some last things, 525,11.08. Bench 2180675

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -104,7 +104,7 @@ void TimeManagement::init(Search::LimitsType& limits,
         double optExtra = limits.inc[us] < 500 ? 1.0 : 1.13;
 
         // Calculate time constants based on current time left.
-        double optConstant = std::min(0.001540 * std::log10(0.00334 * (originalTime + limits.time[us])), 0.00506);
+        double optConstant = std::min(0.000525 * std::log10(11.08 * (originalTime + limits.time[us])), 0.00506);
         double maxConstant = std::max(3.01 * std::log10(13.37 * limits.time[us]), 2.93);
 
         optScale = std::min(0.0122 + std::pow(ply + 2.95, 0.462) * optConstant,

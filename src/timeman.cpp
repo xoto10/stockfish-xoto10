7b10c93c14cc66ea1e2e593b6e6f678083f73b40
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-10 10:57:30 +0100

    Tuned time variations, stc, 21k. Bench 2353057

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -28,7 +28,7 @@
 
 namespace Stockfish {
 
-const static double plyExtra[] = { 0.9, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+const static double plyExtra[] = { 0.981, 0.990, 0.974, 0.991, 1.022, 1.009, 0.995, 1.001, 1.034, 1.024 };
 
 TimePoint TimeManagement::optimum() const { return optimumTime; }
 TimePoint TimeManagement::maximum() const { return maximumTime; }

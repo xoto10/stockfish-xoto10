3fc722c84c719066d57d1fd2022996299faabc6a
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-11 12:53:44 +0100

    Reduce time boost above 100% on first move, 1/6. Bench 2353057

--- src/search.cpp
+++ src/search.cpp
@@ -454,7 +454,7 @@ void Search::Worker::iterative_deepening() {
 
             double timeMultiplier = fallingEval * reduction * bestMoveInstability * EvalLevel[el] * recapture;
             if (rootPos.game_ply() - mainThread->originalPly == 0)
-                timeMultiplier = (1 + 9 * timeMultiplier) / 10.0;
+                timeMultiplier = (2 + 5 * timeMultiplier) / 6.0;
 
             double totalTime = mainThread->tm.optimum() * timeMultiplier;
 

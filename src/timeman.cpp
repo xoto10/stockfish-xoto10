1b818330ad5f088cca7bee345e2ddb0c47ee6393
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-12 13:29:36 +0100

    Adding formula, 2.10*,255, 301,1,0.462. Bench 2353057

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -113,7 +113,7 @@ void TimeManagement::init(
         // Calculate time constants based on current time left.
         double logTimeInSec = std::log10(scaledTime / 1000.0);
 
-        double optConstant  = 2 * std::min(0.000255 * logTimeInSec, 0.00198);
+        double optConstant  = 2.10 * std::min(0.000255 * logTimeInSec, 0.00198);
         double optPly       = 0.00301 * std::pow(ply + 1.0, 0.462);
 
         optScale = std::min(0.01452 + optConstant + optPly, 0.213 * limits.time[us] / timeLeft)

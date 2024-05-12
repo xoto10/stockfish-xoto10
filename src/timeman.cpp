153d2264edee0298fdece3360e7c59a102945ce9
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-12 22:43:57 +0100

    Test early tune values, 24k, 2.07,264,304,492,1555. Bench 2353057

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -113,16 +113,14 @@ void TimeManagement::init(
         // Calculate time constants based on current time left.
         double logTimeInSec = std::log10(scaledTime / 1000.0);
 
-        double optConstant  = 2.20 * std::min(0.000255 * logTimeInSec, 0.00198);
-        double optPly       = 0.00281 * std::pow(ply + 1.0, 0.430);
+        double optConstant  = 2.07 * std::min(0.000264 * logTimeInSec, 0.00198);
+        double optPly       = 0.00304 * std::pow(ply + 1.0, 0.492);
 
-        optScale = std::min(0.01452 + optConstant + optPly, 0.213 * limits.time[us] / timeLeft)
+        optScale = std::min(0.01555 + optConstant + optPly, 0.213 * limits.time[us] / timeLeft)
                  * optExtra;
 
         double maxConstant  = std::max(3.39 + 3.01 * logTimeInSec, 2.93);
         maxScale = std::min(6.64, maxConstant + ply / 12.0);
-//sync_cout << "info optConstantExp " << optConstant << " optPly " << optPly
-//          << " optScale " << optScale << sync_endl;
     }
 
     // x moves in y seconds (+ z increment)

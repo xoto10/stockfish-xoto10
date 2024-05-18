232d71d57566d913253bace8409d206cd886cc62
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-18 08:27:07 +0100

    Values from tune 3 at mtc, 100k, 384,267,385,1095. Bench 1991750

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -113,9 +113,9 @@ void TimeManagement::init(
         // Calculate time constants based on current time left.
         double logTimeInSec = std::log10(scaledTime / 1000.0);
 
-        double optTime = std::min(0.00591 * logTimeInSec, 0.01187);
-        double optPly  = 0.00286 * std::pow(ply + 0.368, 0.496);
-        optScale = std::min(0.00821 + optTime + optPly, 0.213 * limits.time[us] / timeLeft)
+        double optTime = std::min(0.00384 * logTimeInSec, 0.01187);
+        double optPly  = 0.00267 * std::pow(ply + 0.385, 0.496);
+        optScale = std::min(0.01095 + optTime + optPly, 0.213 * limits.time[us] / timeLeft)
                  * optExtra;
 
         double maxConstant  = std::max(3.39 + 3.01 * logTimeInSec, 2.93);

7834d70cc378b87d1cb994ba4781417a42fb5b8d
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-29 00:02:48 +0100

    Bonus/penalty for ocb, 96,96. Bench 1385328

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -85,7 +85,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
       / 35967;
 
     if (pos.opposite_bishops())
-        v += 64 * (v < 0) - 64 * (v > 0);
+        v += 96 * (v < 0) - 96 * (v > 0);
 
     // Damp down the evaluation linearly when shuffling
     v = v * (204 - pos.rule50_count()) / 208;

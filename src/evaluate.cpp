ceadaaced1a7da3627022e0cbcc4981d3c0bdf13
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-29 02:22:32 +0100

    Powers of 2, also shuffling. Bench 1582049

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -85,7 +85,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
       / 32768;
 
     // Damp down the evaluation linearly when shuffling
-    v = v * (204 - pos.rule50_count()) / 208;
+    v = v * (204 - pos.rule50_count()) * 315 / 65536;
 
     // Guarantee evaluation does not hit the tablebase range
     v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);

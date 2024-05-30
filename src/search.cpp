7ed995b33a5531ddebd03859300206fec919f0d6
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-30 16:03:31 +0100

    Randomise 1/32 on ply 3 if equal, bv, incs. Bench 1301136

--- src/search.cpp
+++ src/search.cpp
@@ -1276,7 +1276,7 @@ moves_loop:  // When in check, search starts here
                 rm.score = -VALUE_INFINITE;
         }
 
-        int inc = (ss->ply == 1 && value == bestValue && (int(nodes) & 15) == 0);
+        int inc = (ss->ply == 3 && value == bestValue && (int(nodes) & 31) == 0);
 
         if (value + inc > bestValue)
         {

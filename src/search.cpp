45b30eddd8b3284d84b944f28f4a089c299c0a63
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-05-31 23:15:06 +0100

    Values from tune with fewer vars, 63k. Bench 1484840

--- src/search.cpp
+++ src/search.cpp
@@ -440,17 +440,17 @@ void Search::Worker::iterative_deepening() {
         {
             int nodesEffort = rootMoves[0].effort * 100 / std::max(size_t(1), size_t(nodes));
 
-            double fallingEval = (1127 + 222 * (mainThread->bestPreviousAverageScore - bestValue)
-                                  + 98 * (mainThread->iterValue[iterIdx] - bestValue))
+            double fallingEval = (1067 + 198 * (mainThread->bestPreviousAverageScore - bestValue)
+                                  + 91 * (mainThread->iterValue[iterIdx] - bestValue))
                                / 10000.0;
-            fallingEval = std::clamp(fallingEval, 0.580, 1.667);
+            fallingEval = std::clamp(fallingEval, 0.524, 1.585);
 
             // If the bestMove is stable over several iterations, reduce time accordingly
-            timeReduction    = lastBestMoveDepth + 8 < completedDepth ? 1.495 : 0.687;
+            timeReduction    = lastBestMoveDepth + 8 < completedDepth ? 1.441 : 0.661;
             double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
-            double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
+            double bestMoveInstability = 1 + 1.81 * totBestMoveChanges / threads.size();
             int    el                  = std::clamp((bestValue + 750) / 150, 0, 9);
-            double recapture           = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.955 : 1.005;
+            double recapture           = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.960 : 1.090;
 
             double totalTime = mainThread->tm.optimum() * fallingEval * reduction
                              * bestMoveInstability * EvalLevel[el] * recapture;

741c8d1b4781ce92e0e5721de335658385908501
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-06-04 22:02:09 +0100

    3. Tuned values D/E/F/G. Bench 1378596

--- src/search.cpp
+++ src/search.cpp
@@ -442,9 +442,9 @@ void Search::Worker::iterative_deepening() {
             int nodesEffort = rootMoves[0].effort * 100 / std::max(size_t(1), size_t(nodes));
 
             double fallingEval = (1067 + 223 * (mainThread->bestPreviousAverageScore - bestValue)
-                                  + 100 * (mainThread->iterValue[iterIdx] - bestValue))
+                                  + 97 * (mainThread->iterValue[iterIdx] - bestValue))
                                / 10000.0;
-            fallingEval = std::clamp(fallingEval, 0.591, 1.643);
+            fallingEval = std::clamp(fallingEval, 0.580, 1.643);
 
             // If the bestMove is stable over several iterations, reduce time accordingly
             timeReduction    = lastBestMoveDepth + 8 < completedDepth ? 1.475 : 0.747;

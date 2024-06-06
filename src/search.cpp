96aba7fd64475d6786705b60a2142a275a11e5b7
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-06-06 18:31:40 +0100

    Cap main time multipliers, 8.0. Bench 961982

--- src/search.cpp
+++ src/search.cpp
@@ -453,8 +453,8 @@ void Search::Worker::iterative_deepening() {
             int    el                  = std::clamp((bestValue + 750) / 150, 0, 9);
             double recapture           = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.955 : 1.005;
 
-            double totalTime = mainThread->tm.optimum() * fallingEval * reduction
-                             * bestMoveInstability * EvalLevel[el] * recapture;
+            double totalTime = mainThread->tm.optimum() * std::min(8.0, fallingEval * reduction * bestMoveInstability)
+                             * EvalLevel[el] * recapture;
 
             // Cap used time in case of a single legal move for a better viewer experience
             if (rootMoves.size() == 1)

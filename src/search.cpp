62ef41fa3ad4d6478c8785c67fe09848a819d28d
xoto10 <23479932+xoto10@users.noreply.github.com>
2024-06-30 18:03:18 +0100

    Randomise move choice in last 2 ply, 0-6(fix), &63. Bench 1563371

--- src/search.cpp
+++ src/search.cpp
@@ -1287,7 +1287,7 @@ moves_loop:  // When in check, search starts here
 
         // In case we have an alternative move close in eval to the current bestmove,
         // promote it to bestmove by pretending it just exceeds alpha (but not beta).
-        int inc = std::clamp(2 * (ss->ply + 2 - thisThread->rootDepth), 0, 4)
+        int inc = std::clamp(3 * (ss->ply + 2 - thisThread->rootDepth), 0, 6)
                   * (   (int(nodes) & 63) == 0
                      && ss->ply + ss->ply / 32 >= thisThread->rootDepth - 3
                      && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY - 100);

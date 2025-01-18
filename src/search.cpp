446e8f77a8936d775d9166eaf9173058bfe800c7
xoto10 <23479932+xoto10@users.noreply.github.com>
2025-01-18 22:25:59 +0000

    Futility margin and nodes, *15-12. Bench 1560658

--- src/search.cpp
+++ src/search.cpp
@@ -795,7 +795,7 @@ Value Search::Worker::search(
     if (!ss->ttPv && depth < 14
         && eval - futility_margin(depth, cutNode && !ss->ttHit, improving, opponentWorsening)
                - (ss - 1)->statScore / 310 + 40 - std::abs(correctionValue) / 131072
-               + (depth < 2) * (((int)thisThread->nodes & 15) - 4)
+               + (depth < 2) * (((int)thisThread->nodes & 15) - 12)
              >= beta
         && eval >= beta && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
         return beta + (eval - beta) / 3;

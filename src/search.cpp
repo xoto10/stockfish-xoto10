d56ff60d1a06f3f581c163a2b4ec726af759c4ec
xoto10 <23479932+xoto10@users.noreply.github.com>
2025-08-11 17:33:37 +0100

    undo format change

--- src/search.cpp
+++ src/search.cpp
@@ -471,10 +471,12 @@ void Search::Worker::iterative_deepening() {
             double k      = 0.527;
             double center = lastBestMoveDepth + 11;
             timeReduction = 0.8 + 0.84 / (1.077 + std::exp(-k * (completedDepth - center)));
-            double reduction = (1.4540 + mainThread->previousTimeReduction) / (2.1593 * timeReduction);
+            double reduction =
+              (1.4540 + mainThread->previousTimeReduction) / (2.1593 * timeReduction);
             double bestMoveInstability = 0.9929 + 1.8519 * totBestMoveChanges / threads.size();
 
-            double totalTime = mainThread->tm.optimum() * fallingEval * reduction * bestMoveInstability;
+            double totalTime =
+              mainThread->tm.optimum() * fallingEval * reduction * bestMoveInstability;
 
             // Cap used time in case of a single legal move for a better viewer experience
             if (rootMoves.size() == 1)

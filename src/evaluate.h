fd58c4bddcf56e70defda37021d43a7ed9514531
xoto10 <23479932+xoto10@users.noreply.github.com>
2025-09-03 18:22:41 +0100

    Tune ideal pawn number for material/optimism calc. Bench 2560569

--- src/evaluate.h
+++ src/evaluate.h
@@ -50,6 +50,7 @@ Value evaluate(const NNUE::Networks&          networks,
                const Position&                pos,
                Eval::NNUE::AccumulatorStack&  accumulators,
                Eval::NNUE::AccumulatorCaches& caches,
+               uint64_t                       nodes,
                int                            optimism);
 }  // namespace Eval
 

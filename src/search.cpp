b616fa24dfd466892b80c2198846c33c24c549cc
xoto10 <23479932+xoto10@users.noreply.github.com>
2025-09-07 01:52:06 +0100

    Vary razoring with w/l, +/-400. Bench 2649371

--- src/search.cpp
+++ src/search.cpp
@@ -850,7 +850,7 @@ Value Search::Worker::search(
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 514 - 294 * depth * depth + (eval > 0) - (eval < 0) * 400)
+    if (!PvNode && eval < alpha - 514 - 294 * depth * depth + ((eval > 0) - (eval < 0)) * 400)
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 8. Futility pruning: child node

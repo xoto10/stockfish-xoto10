fdd9c3e32a593d67a7b42a038d449e5f7443bdf6
xoto10 <23479932+xoto10@users.noreply.github.com>
2025-09-10 22:03:46 +0100

    Vary optimism with thread idx (fix). Bench 2785713

--- src/search.cpp
+++ src/search.cpp
@@ -331,7 +331,7 @@ void Search::Worker::iterative_deepening() {
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
             // Adjust optimism based on root move's averageScore
-            optimism[us]  = (137 - threadIdx % 8) * avg / (std::abs(avg) + 91);
+            optimism[us]  = int(137 - threadIdx % 8) * avg / (std::abs(avg) + 91);
             optimism[~us] = -optimism[us];
 
             // Start with a small aspiration window and, in the case of a fail

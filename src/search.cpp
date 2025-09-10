ee63e46711e108149eaf83e6933a11702e1b9cbc
xoto10 <23479932+xoto10@users.noreply.github.com>
2025-09-10 22:39:34 +0100

    Vary razoring with thread idx. Bench 2785713

--- src/search.cpp
+++ src/search.cpp
@@ -850,7 +850,7 @@ Value Search::Worker::search(
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 514 - 294 * depth * depth)
+    if (!PvNode && eval < alpha - 514 - 294 * depth * depth - int(threadIdx % 8))
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 8. Futility pruning: child node

1c2339be86aaa2dc56ba80da8035908c0caf2958
xoto10 <23479932+xoto10@users.noreply.github.com>
2025-09-15 22:01:10 +0100

    Value changes, 1+v-a, min5, 0.110*, *7/8. Bench 2493363

--- src/search.cpp
+++ src/search.cpp
@@ -297,7 +297,7 @@ void Search::Worker::iterative_deepening() {
     {
         // Age out PV variability metric
         if (mainThread)
-            totBestMoveChanges /= 4;
+            totBestMoveChanges = totBestMoveChanges * 7 / 8;
 
         // Save the last iteration's scores before the first PV line is searched and
         // all the move scores except the (new) PV are set to -VALUE_INFINITE.

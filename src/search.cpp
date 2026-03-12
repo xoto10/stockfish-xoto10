a5c59b389eb3a5e7a984dd77a2eeb9bf0852bf31
xoto10 <23479932+xoto10@users.noreply.github.com>
2026-03-12 10:40:24 +0000

    Even more time if reply chnages a lot, 0.94. Bench 2288704

--- src/search.cpp
+++ src/search.cpp
@@ -152,10 +152,6 @@ bool is_shuffling(Move move, Stack* const ss, const Position& pos) {
 
 }  // namespace
 
-auto f1 = [](int m){return Range(m / 2, m * 3 / 2);};
-int A=970;
-TUNE(SetRange(f1), A);
-
 Search::Worker::Worker(SharedState&                    sharedState,
                        std::unique_ptr<ISearchManager> sm,
                        size_t                          threadId,
@@ -504,7 +500,7 @@ void Search::Worker::iterative_deepening() {
 
             double reduction = (1.43 + mainThread->previousTimeReduction) / (2.28 * timeReduction);
 
-            double bestMoveInstability = 1.02 + (A/1000.0) * totBestMoveChanges / threads.size();
+            double bestMoveInstability = 1.02 + 0.94 * totBestMoveChanges / threads.size();
 
             double highBestMoveEffort = nodesEffort >= 93340 ? 0.76 : 1.0;
 

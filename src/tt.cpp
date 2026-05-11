d643e2d2909fdd3cab412b4f46a96c74951f7eac
xoto10 <23479932+xoto10@users.noreply.github.com>
2026-05-11 20:29:43 +0100

    always overwrite. 20,0. Bench 3196493

--- src/tt.cpp
+++ src/tt.cpp
@@ -93,23 +93,16 @@ void TTEntry::save(
     if (m || uint16_t(k) != key16)
         move16 = m;
 
-    // Overwrite less valuable entries (cheapest checks first)
-    if (   b == BOUND_EXACT
-        || uint16_t(k) != key16
-        || Bound((genBound8 & BOUND_MASK) >> BOUND_SHIFT) != BOUND_EXACT
-        || d - DEPTH_NONE + 2 * pv > depth8 - 4
-        || relative_age(curr_generation))
-    {
-        assert(d > DEPTH_NONE);
-        assert(d - DEPTH_NONE < 256);
-        assert(curr_generation <= GENERATION_MASK);  // TT::new_search() plays nice
+    // Overwrite previous entries
+    assert(d > DEPTH_NONE);
+    assert(d - DEPTH_NONE < 256);
+    assert(curr_generation <= GENERATION_MASK);  // TT::new_search() plays nice
 
-        key16     = uint16_t(k);
-        depth8    = uint8_t(d - DEPTH_NONE);
-        genBound8 = uint8_t(curr_generation | b << BOUND_SHIFT | uint8_t(pv) << PV_SHIFT);
-        value16   = int16_t(v);
-        eval16    = int16_t(ev);
-    }
+    key16     = uint16_t(k);
+    depth8    = uint8_t(d - DEPTH_NONE);
+    genBound8 = uint8_t(curr_generation | b << BOUND_SHIFT | uint8_t(pv) << PV_SHIFT);
+    value16   = int16_t(v);
+    eval16    = int16_t(ev);
 }
 
 
